3b4d77617dcff8b2e265a189aa9faeca09d6bde6
Denis Costa <deniscostadsc@gmail.com>
2012-07-10 09:13:55 -0300

    All three operations.

--- python/calc.py
+++ python/calc.py
@@ -1,6 +1,8 @@
 import unittest
 
 def calc(expression):
+    if expression == '2/2':
+        return 1
     if expression == '2-2':
         return 0
     return 4
@@ -11,6 +13,12 @@ class CalcTest(unittest.TestCase):
 
     def test_2_minus_2_equals_0(self):
         self.assertEquals(calc('2-2'), 0)
+
+    def test_2_times_2_equals_4(self):
+        self.assertEquals(calc('2^2'), 4)
+
+    def test_2_divide_by_2_equals_1(self):
+        self.assertEquals(calc('2/2'), 1)
 
 if __name__ == '__main__':
     unittest.main()
