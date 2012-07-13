5d0041c94b0c217b2a27f3873d40cfb15cee98cb
Denis Costa <deniscostadsc@gmail.com>
2012-07-13 10:48:43 -0300

    Improving tests.

--- python/calc.py
+++ python/calc.py
@@ -8,16 +8,10 @@ def calc(expression):
     return 4
 
 class CalcTest(unittest.TestCase):
-    def test_2_plus_2_equals_4(self):
+    def test_simple_calcs(self):
         self.assertEquals(calc('2+2'), 4)
-
-    def test_2_minus_2_equals_0(self):
         self.assertEquals(calc('2-2'), 0)
-
-    def test_2_times_2_equals_4(self):
         self.assertEquals(calc('2*2'), 4)
-
-    def test_2_divide_by_2_equals_1(self):
         self.assertEquals(calc('2/2'), 1)
 
 if __name__ == '__main__':
