10c2ecb98f02170b5717183ab7b497d162c73fa5
Denis Costa <deniscostadsc@gmail.com>
2012-07-13 11:11:33 -0300

    Adding more tests and pow operation.

--- python/calc.py
+++ python/calc.py
@@ -1,11 +1,22 @@
 import unittest
 
 def calc(expression):
-    if expression == '2/2':
-        return 1
-    if expression == '2-2':
-        return 0
-    return 4
+    if '+' in expression:
+        a, b = expression.split('+')
+        expression = '%s' % str(calc(a) + calc(b))
+    if '-' in expression:
+        a, b = expression.split('-')
+        expression = '%s' % str(calc(a) - calc(b))
+    if '*' in expression:
+        a, b = expression.split('*')
+        expression = '%s' % str(calc(a) * calc(b))
+    if '/' in expression:
+        a, b = expression.split('/')
+        expression = '%s' % str(calc(a) / calc(b))
+    if '^' in expression:
+        a, b = expression.split('^')
+        expression = '%s' % str(calc(a) ** calc(b))
+    return int(expression)
 
 class CalcTest(unittest.TestCase):
     def test_simple_calcs(self):
@@ -13,6 +24,14 @@ class CalcTest(unittest.TestCase):
         self.assertEquals(calc('2-2'), 0)
         self.assertEquals(calc('2*2'), 4)
         self.assertEquals(calc('2/2'), 1)
+        self.assertEquals(calc('9^2'), 81)
+
+    def test_two_operations_in_an_expression(self):
+        self.assertEquals(calc('2+2*3'), 8)
+        self.assertEquals(calc('3+3*4'), 15)
+        self.assertEquals(calc('3*4-3'), 9)
+        self.assertEquals(calc('3^4-3'), 78)
+        self.assertEquals(calc('8/4-3'), -1)
 
 if __name__ == '__main__':
     unittest.main()
