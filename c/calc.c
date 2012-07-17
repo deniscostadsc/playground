69eb317072689c5f772dd4085616eb2853548667
Denis Costa <deniscostadsc@gmail.com>
2012-07-17 10:15:14 -0300

    Adding one more test

--- c/calc.c
+++ c/calc.c
@@ -2,10 +2,14 @@
 #include <assert.h>
 
 int calc(char expression[]) {
+    if (expression == "2-2")
+        return 0;
     return 4;
 }
 
 int main (int argc, char const* argv[]) {
+    // simple tests
     assert(calc("2+2") == 4);
+    assert(calc("2-2") == 0);
     return 0;
 }
