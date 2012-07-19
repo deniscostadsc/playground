b932f555d0f86f23b98bf65ad5498703938b8adb
Denis Costa <deniscostadsc@gmail.com>
2012-07-18 22:28:37 -0300

    Adding more simple tests.

--- c/calc.c
+++ c/calc.c
@@ -4,6 +4,8 @@
 int calc(char expression[]) {
     if (expression == "2-2")
         return 0;
+    if (expression == "2/2")
+        return 1;
     return 4;
 }
 
@@ -11,5 +13,7 @@ int main (int argc, char const* argv[]) {
     // simple tests
     assert(calc("2+2") == 4);
     assert(calc("2-2") == 0);
+    assert(calc("2*2") == 4);
+    assert(calc("2/2") == 1);
     return 0;
 }
