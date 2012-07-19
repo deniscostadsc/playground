51c8719d239b219c538744932e4ef7dabb0b6a54
Denis Costa <deniscostadsc@gmail.com>
2012-07-18 22:31:58 -0300

    Adding a test with a pow expression.

--- c/calc.c
+++ c/calc.c
@@ -6,6 +6,8 @@ int calc(char expression[]) {
         return 0;
     if (expression == "2/2")
         return 1;
+    if (expression == "9^2")
+        return 81;
     return 4;
 }
 
@@ -15,5 +17,6 @@ int main (int argc, char const* argv[]) {
     assert(calc("2-2") == 0);
     assert(calc("2*2") == 4);
     assert(calc("2/2") == 1);
+    assert(calc("9^2") == 81);
     return 0;
 }
