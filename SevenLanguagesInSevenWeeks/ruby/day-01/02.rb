73b0514abe633ad2dfdfb3a23090f5b44ba080b0
Denis Costa <deniscostadsc@gmail.com>
2012-11-27 19:33:30 -0200

    Adding more exercises

---
+++ SevenLanguagesInSevenWeeks/ruby/day-01/02.rb
@@ -0,0 +1,2 @@
+puts('Hello, Ruby'.index('Ruby'))
+
---
+++ SevenLanguagesInSevenWeeks/ruby/day-01/06.rb
@@ -0,0 +1,13 @@
+number = rand(100)
+
+guess = gets.to_i
+
+while guess != number
+    if guess < number
+        puts('Too low')
+    else
+        puts('Too high')
+    end
+    guess= gets.to_i
+end
+puts('Nice try')
