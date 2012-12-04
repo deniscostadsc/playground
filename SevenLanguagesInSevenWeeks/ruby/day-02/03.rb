f536dcfd6231579b4391032069d95e8939ebfc5a
Denis Costa <deniscostadsc@gmail.com>
2012-12-03 22:20:31 -0200

    Stacks e queues.

---
+++ SevenLanguagesInSevenWeeks/ruby/day-02/03.rb
@@ -0,0 +1,40 @@
+
+$stack = Array.new
+
+def put_in_stack item
+    $stack.push item
+end
+
+def remove_from_stack
+    $stack.pop
+end
+
+put_in_stack 1
+put_in_stack 2
+put_in_stack 3
+remove_from_stack
+put_in_stack 4
+put_in_stack 5
+
+puts $stack
+
+puts
+
+$queue = Array.new
+
+def put_in_queue item
+    $queue.push item
+end
+
+def remove_from_queue
+    $queue.shift
+end
+
+put_in_queue 1
+put_in_queue 2
+put_in_queue 3
+remove_from_queue
+put_in_queue 4
+put_in_queue 5
+
+puts $queue
