ac27f281a2128d8e4fd37795db04b8489f4ae29f
Denis Costa <deniscostadsc@gmail.com>
2012-12-30 12:12:25 -0200

    Refactoring for beauty code.

--- SevenLanguagesInSevenWeeks/ruby/day-02/06.rb
+++ SevenLanguagesInSevenWeeks/ruby/day-02/06.rb
@@ -1,10 +1,4 @@
 pattern = ARGV[0]
 file = ARGV[1]
 
-File.open(file, 'r') do |f|
-    while line = f.gets
-        if line.match pattern
-            puts line
-        end
-    end
-end
+File.open(file, 'r').each {|line| puts line if line.match pattern}
