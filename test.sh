7fb6b786a8cd686fa7a459ee6af2e5db3eb647a7
Denis Costa <deniscostadsc@gmail.com>
2013-08-18 13:08:13 -0300

    Adicionando projecteuler.

--- test.sh
+++ test.sh
@@ -1,26 +1,38 @@
 #!/bin/bash
 
+# set -xv
+
 cd $(dirname $0)
 
-# cleaning
-find . -regex '.*\.\(py[co]\|out\)' -delete
+clean (){
+    # cleaning by extension
+    find . -regex '.*\.\(py[co]\|out\)' -delete
+
+    # cleaning results
+    find . -name 'out2.txt' -delete
+}
+
+clean
 
 exit=0
 
-for folder in $(ls -F |grep '\/$'); do
-    cd $folder
-    for folder in $(ls -F |grep '\/$'); do
-        cd $folder
-        gcc *.c
-        ./a.out < in.txt > out2.txt
-        if [ ! -z "$(diff out2.txt out.txt)" ]; then
-            echo "Error: $folder"
-            exit=1
-        fi
-        rm *.out out2.txt
+for project_folder in $(ls -F | grep '\/$'); do
+    if [ $project_folder != "projecteuler/" ]; then
+        cd $project_folder
+        for folder in $(ls -F | grep '\/$'); do
+            cd $folder
+            gcc *.c
+            ./a.out < in.txt > out2.txt
+            if [ ! -z "$(diff out2.txt out.txt)" ]; then
+                echo "Error: $project_folder/$folder"
+                exit=1
+            fi
+            cd ..
+        done
         cd ..
-    done
-    cd ..
+    fi
 done
+
+clean
 
 exit $exit
