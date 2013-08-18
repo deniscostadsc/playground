4151ec18b64f4d665f43788cab2add91bb49cbdb
Denis Costa <deniscostadsc@gmail.com>
2013-08-18 13:13:12 -0300

    Removendo chaves desnecessárias.

--- projecteuler/1/1.c
+++ projecteuler/1/1.c
@@ -8,10 +8,9 @@ of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is
 
 int main(){
     int i, sum = 0;
-    for (i = 0; i < 1000; ++i){
+    for (i = 0; i < 1000; ++i)
         if (i % 3 == 0 || i % 5 == 0)
             sum += i;
-    }
     printf("%d\n", sum);
     return 0;
 }
