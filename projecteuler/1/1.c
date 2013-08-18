a372f74bab987f1a08c885e54f9a46052066bfb4
Denis Costa <deniscostadsc@gmail.com>
2013-08-18 13:15:18 -0300

    Removendo enunciado para arquivos separados.

--- projecteuler/1/1.c
+++ projecteuler/1/1.c
@@ -1,11 +1,5 @@
 #include <stdio.h>
 
-/*
-If we list all the natural numbers below 10 that are multiples
-of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is
-23. Find the sum of all the multiples of 3 or 5 below 1000.
-*/
-
 int main(){
     int i, sum = 0;
     for (i = 0; i < 1000; ++i)
