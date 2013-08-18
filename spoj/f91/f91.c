2216ba943991c50fb622757b587f4baf0a654d61
Denis Costa <deniscostadsc@gmail.com>
2013-08-18 12:12:38 -0300

    Diminuindo código do f91

--- spoj/f91/f91.c
+++ spoj/f91/f91.c
@@ -44,10 +44,8 @@ int f91(int n){
 
 int main(){
     int n;
-    scanf("%d", &n);
-    while (n != 0) {
+    while (scanf("%d", &n) && n != 0) {
         printf("f91(%d) = %d\n", n, f91(n));
-        scanf("%d", &n);        
     };
     return 0;
 }
