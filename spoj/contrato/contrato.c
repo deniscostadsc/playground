b18523b23770855226fbc656d09f2dfba4e095ad
Denis Costa <deniscostadsc@gmail.com>
2013-08-19 20:24:07 -0300

    Resolvendo contrato do Spoj.

---
+++ spoj/contrato/contrato.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+main (){
+    int n, s, t;
+    while((t = getchar()) != '0'){
+        getchar();
+        s = 0;
+        while((n = getchar()) != '\n'){
+            if (n != t){
+                s += n - '0';
+                if (s != 0)
+                    printf("%c", n);
+            }
+        }
+        if (s == 0)
+            printf("%d", 0);
+        printf("\n");
+    }
+    return 0;
+}
