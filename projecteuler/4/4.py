9e6f036634fa03f4fec3a2fc39f1c7d2ad21e4fc
Denis Costa <deniscostadsc@gmail.com>
2013-08-18 23:17:46 -0300

    linha vazia nos arquivos python.

--- projecteuler/4/4.py
+++ projecteuler/4/4.py
@@ -1,10 +1,10 @@
-print(max([ i * j for i in xrange(999, 1, -1) for j in xrange(999, 1, -1) if str(i * j) == str(i * j)[::-1] ]))
+print(max([ i * j for i in xrange(100, 999) for j in xrange(100,999) if str(i * j) == str(i * j)[::-1] ]))
 
 # or 
 
 # greatest = 1
-# for i in xrange(999, 1, -1):
-#     for j in xrange(999, 1, -1):
+# for i in xrange(100, 999):
+#     for j in xrange(100, 999):
 #         if str(i * j) == str(i * j)[::-1] and i * j > greatest:
 #             greatest = i * j
 # print(greatest)
