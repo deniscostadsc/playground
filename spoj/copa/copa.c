29a299e485be6c15f1d91d52b83da6435abcdb23
Denis Costa <deniscostadsc@gmail.com>
2013-08-19 11:07:34 -0300

    fix teste e refactoring do código.

--- spoj/copa/copa.c
+++ spoj/copa/copa.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 int main(){
-    int t, j, n, s = 0;
+    int t, j, n, s;
     char x[10];
     while(scanf("%d %d", &t, &j) && t != 0){
+        s = 0;
         while (t--){
             scanf("%s %d", x, &n);
             s += n;
         }
         printf("%d\n", j * 3 - s);
-        s = 0;
     }
     return 0;
 }
