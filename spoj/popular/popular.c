5ccead0974c869a5664502b58e19575da5e86197
Denis Costa <deniscostadsc@gmail.com>
2013-08-20 21:42:34 -0300

    Renomeando variável.

--- spoj/popular/popular.c
+++ spoj/popular/popular.c
@@ -4,15 +4,15 @@
 int main (){
     int i, n, k, y, maior;
     while (scanf("%d", &i) && i != 0){
-        int votos[i];
-        memset(votos, 0, sizeof(votos));
-        maior = votos[0];
+        int v[i];
+        memset(v, 0, sizeof(v));
+        maior = v[0];
         for (k = 0; k < i; ++k){
             for (y = 0; y < i; ++y){
                 scanf("%d", &n);
-                votos[y] += n;
-                if (votos[y] > maior){
-                    maior = votos[y];
+                v[y] += n;
+                if (v[y] > maior){
+                    maior = v[y];
                 }
             }
         }
