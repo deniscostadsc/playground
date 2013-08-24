a2dd54e5b46451ac26f37023383205fb2a979c15
Denis Costa <deniscostadsc@gmail.com>
2013-08-24 20:07:49 -0300

    Mais parecido com C++ de verdade. Eu acho... 8D

--- uri/1001/1001.cpp
+++ uri/1001/1001.cpp
@@ -1,12 +1,9 @@
-#include <stdio.h>
-#include <iostream>
-using std::cout;
-using std::endl;
+#include <cstdio>
 
 int main(){
     int a, b;
     scanf("%d", &a);
     scanf("%d", &b);
-    cout << "X = " << a + b << endl;
+    printf("X = %d\n", a + b);
     return 0;
 }
