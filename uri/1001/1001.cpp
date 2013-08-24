e9b7be504b051c708dc60a92e78ee1ec31399dbc
Denis Costa <deniscostadsc@gmail.com>
2013-08-24 19:54:42 -0300

    Começando a brincar com C++.

---
+++ uri/1001/1001.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main(){
+    int a, b;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    cout << "X = " << a + b << endl;
+    return 0;
+}
