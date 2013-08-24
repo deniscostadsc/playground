7574dea8b4d32cdfb4dc7e4ca0e96050f3d7c916
Denis Costa <deniscostadsc@gmail.com>
2013-08-24 19:15:53 -0300

    Resolvendo o encotel do spoj. Solução feia.

---
+++ spoj/encotel/encotel.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+    char c;
+    while(scanf("%c", &c) != EOF){
+        if (65 <= c && c <= 67)
+            printf("%d", 2);
+        else if (68 <= c && c <= 70)
+            printf("%d", 3);
+        else if (71 <= c && c <= 73)
+            printf("%d", 4);
+        else if (74 <= c && c <= 76)
+            printf("%d", 5);
+        else if (77 <= c && c <= 79)
+            printf("%d", 6);
+        else if (80 <= c && c <= 83)
+            printf("%d", 7);
+        else if (84 <= c && c <= 86)
+            printf("%d", 8);
+        else if (87 <= c && c <= 90)
+            printf("%d", 9);
+        else
+            printf("%c", c);
+    }
+    return 0;
+}
