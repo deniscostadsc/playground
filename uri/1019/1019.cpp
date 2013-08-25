ee6b4eb0e71cde10b5d751492f050c76951b370c
Denis Costa <deniscostadsc@gmail.com>
2013-08-25 15:45:57 -0300

    Resolvendo 1019 do uri.

---
+++ uri/1019/1019.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+
+int main(){
+    int t, h = 0, m = 0, s = 0;
+    scanf("%d", &t);
+    if (t >= 60 * 60){
+        h = t / (60 * 60);
+        t %= (60 * 60);
+    }
+    if (t >= 60){
+        m = t / 60;
+        t %= 60;
+    }
+    s = t;
+    printf("%d:%d:%d\n", h, m, s);
+    return 0;
+}
