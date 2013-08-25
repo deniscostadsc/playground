f405aedf95e35db356099bc1dd4b2a88b4609e6b
Denis Costa <deniscostadsc@gmail.com>
2013-08-25 11:02:37 -0300

    Resolvendo 1022 do uri.

---
+++ uri/1022/1022.cpp
@@ -0,0 +1,46 @@
+#include <cstdio>
+#include <cmath>
+
+int main(){
+    int n, a, b, c, d, r, num, den, num_r, den_r, j;
+    char o, i;
+
+    scanf("%d", &n);
+    while(n--){
+        scanf("%d %c %d %c %d %c %d", &a, &i, &b, &o, &c, &i, &d);
+        if (o == '+'){
+            num = (b * d) * a / b + (b * d) * c / d;
+            den = b * d;
+        } else if (o == '-'){
+            num = (b * d) * a / b - (b * d) * c / d;
+            den = b * d;
+        } else if (o == '*'){
+            num = a * c;
+            den = b * d;
+        } else {
+            num = a * d;
+            den = b * c;
+        }
+
+        if (std::abs(num) < std::abs(den))
+            r = std::abs(num);
+        else
+            r = std::abs(den);
+
+        num_r = num;
+        den_r = den;
+
+        j = 2;
+        while (j <= r){
+            if (num_r % j == 0 && den_r % j == 0){
+                num_r = num_r / j;
+                den_r = den_r / j;
+            } else {
+                j++;
+            }
+        }
+
+        printf("%d/%d = %d/%d\n", num, den, num_r, den_r);
+    }
+    return 0;
+}
