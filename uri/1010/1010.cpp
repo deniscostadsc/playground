62359aca1db64171d658946f70d3cfc56c9a7e7b
Denis Costa <deniscostadsc@gmail.com>
2013-08-25 07:05:15 -0300

    Resolvendo 1010 do URI.

---
+++ uri/1010/1010.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+#include <cmath>
+
+int main(){
+    int a, b;
+    float c, s = 0.0;
+    while(scanf("%d %d %f", &a, &b, &c) != EOF){
+        s += c * b;
+    }
+    printf("VALOR A PAGAR: R$ %.2f\n", s);
+}
