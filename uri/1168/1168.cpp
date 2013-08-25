ca218c26bf483e1cb15c6e9d22746a4daecb92cb
Denis Costa <deniscostadsc@gmail.com>
2013-08-25 16:40:18 -0300

    Resolvendo 1168 do uri.

---
+++ uri/1168/1168.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+
+int main(){
+    int n, s, l[10] = {6, 2, 5, 5, 4, 5, 6, 3, 7, 6};
+    char d;
+    scanf("%d", &n);
+    getchar();
+    while(n--){
+        s = 0;
+        while((d = getchar()) != '\n'){
+            s += l[d - '0'];
+        }
+        printf("%d leds\n", s);
+    }
+    return 0;
+}
