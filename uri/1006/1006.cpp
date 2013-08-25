9287a1b3a1d6e55e3de0ba2f18261d0d304b5def
Denis Costa <deniscostadsc@gmail.com>
2013-08-24 22:06:45 -0300

    Resolvendo 1006

---
+++ uri/1006/1006.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+
+int main(){
+    float a, b, c;
+    scanf("%f", &a);
+    scanf("%f", &b);
+    scanf("%f", &c);
+    printf("MEDIA = %.1f\n", (a * 2.0 + b * 3.0 + c * 5.0) / 10.0);
+    return 0;
+}
