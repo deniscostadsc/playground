69ef7278199f683c22f9bdd8607c24a1c7cde2ba
Denis Costa <deniscostadsc@gmail.com>
2013-08-27 09:58:37 -0300

    Resolvendo 1020 do uri.

---
+++ uri/1020/1020.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+
+int main(){
+    int t, a = 0, m = 0, d = 0;
+    scanf("%d", &t);
+    if (t >= 365){
+        a = t / 365;
+        t %= 365;
+    }
+    if (t >= 30){
+        m = t / 30;
+        t %= 30;
+    }
+    d = t;
+    printf("%d ano(s)\n", a);
+    printf("%d mes(es)\n", m);
+    printf("%d dia(s)\n", d);
+    return 0;
+}
