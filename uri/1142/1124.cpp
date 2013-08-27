58dbbbf236f3f9f8f5dd8dfa1fb247942cb6f3f1
Denis Costa <deniscostadsc@gmail.com>
2013-08-27 11:33:16 -0300

    Resolvendo 1142.

---
+++ uri/1142/1124.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+
+int main(){
+    int a, b = 1;
+    scanf("%d", &a);
+    while(a--){
+        printf("%d ", b++);
+        printf("%d ", b++);
+        printf("%d ", b++);
+        b++;
+        printf("PUM\n");
+    }
+    return 0;
+}
