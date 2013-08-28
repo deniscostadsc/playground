4da30722041a17f7ce6732c61df95a36abcd680e
Denis Costa <deniscostadsc@gmail.com>
2013-08-28 17:17:05 -0300

    Resolvendo 1095.

---
+++ uri/1095/1095.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+
+int main(){
+    int i = 1, j = 60;
+    while (j != 0){
+        printf("I=%d J=%d\n", i, j);
+        i += 3;
+        j -= 5;
+    }
+    printf("I=%d J=%d\n", i, j);
+    return 0;
+}
