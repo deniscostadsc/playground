f32d35bad2612921ec653a26c2e64f8d5dcaa79f
Denis Costa <deniscostadsc@gmail.com>
2013-08-28 13:51:06 -0300

    Resolvendo 1132

---
+++ uri/1132/1132.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+
+int main(){
+    int d, t, i, s = 0;
+    scanf("%d", &d);
+    scanf("%d", &t);
+
+    if (d < t){
+        for (i = d; i <= t; i++){
+            if (i % 13 != 0)
+                s += i;
+        }
+    } else {
+        for (i = t; i <= d; i++){
+            if (i % 13 != 0)
+                s += i;
+        }
+    }
+    printf("%d\n", s);
+}
