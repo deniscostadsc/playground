4b4e7284f898ce6cb7af2e0dda04137132790c5e
Denis Costa <deniscostadsc@gmail.com>
2013-08-28 20:41:29 -0300

    Resolvendo 1037.

---
+++ uri/1037/1037.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+
+int main(){
+    double n;
+    
+    scanf("%lf", &n);
+    if (n >= 0.0 && n <= 25.0)
+        printf("Intervalo [0,25]\n");
+    else if (n > 25.0 && n <= 50.0)
+        printf("Intervalo (25,50]\n");
+    else if (n > 50.0 && n <= 75.0)
+        printf("Intervalo (50,75]\n");
+    else if (n > 75.0 && n <= 100.0)
+        printf("Intervalo (75,100]\n");
+    else
+        printf("Fora de intervalo\n");
+    return 0;
+}
