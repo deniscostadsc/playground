bbdc136e90d318367d7d970690a0a6a652746fd9
Denis Costa <deniscostadsc@gmail.com>
2013-08-28 13:33:29 -0300

    Retirando comentário.

--- uri/1050/1050.cpp
+++ uri/1050/1050.cpp
@@ -1,18 +1,5 @@
 #include <cstdio>
 
-/*
-
-61 - Brasilia
-71 - Salvador
-11 - Sao Paulo
-21 - Rio de Janeiro
-32 - Juiz de Fora
-19 - Campinas
-27 - Vitoria
-31 - Belo Horizonte
-
-*/
-
 int main(){
     int d;
     scanf("%d", &d);
