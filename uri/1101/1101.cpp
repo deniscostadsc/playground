ac3755434837e6feeb2f94fa12b162613273980d
Denis Costa <deniscostadsc@gmail.com>
2013-08-28 20:26:45 -0300

    Resolvendo 1101.

---
+++ uri/1101/1101.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+
+int main(){
+    int i, j, x, z, s;
+     while (scanf("%d %d", &i, &j) && i > 0 && j > 0) {
+        s = 0;
+        if (j < i){
+            x = i; i = j; j = x;
+        }
+        for (z = i; z <= j; z++){
+            printf("%d ", z);
+            s += z;
+        }
+        printf("Sum=%d\n", s);
+    }
+    return 0;
+}
