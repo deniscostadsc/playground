37cb3da11f72249ac87a2715ccb972a49708b872
Denis Costa <deniscostadsc@gmail.com>
2013-08-29 10:12:33 -0300

    Refatorando 1021.

--- uri/1021/1021.cpp
+++ uri/1021/1021.cpp
@@ -1,27 +1,26 @@
 #include <cstdio>
 
 int main(){
-    double n,  d[] = {100.0, 50.0, 20.0, 10.0, 5.0, 2.0}, m[] = {1.0, 0.5, 0.25, 0.10, 0.05, 0.01};
+    double n,  d[] = {100.0, 50.0, 20.0, 10.0, 5.0, 2.0, 1.0, 0.5, 0.25, 0.10, 0.05, 0.01};
     int t = 0, c;
 
     scanf("%lf", &n);
     printf("NOTAS:\n");
-    do {
-        printf("%d nota(s) de R$ %.2f\n", (int)n / (int)d[t], d[t]);
-        if (n >= d[t])
-            n -= d[t] * (int)(n / d[t]);
-    } while(d[t++] != 2.0);
 
     t = 0;
-    printf("MOEDAS:\n");
-    do {
+    while (n >= 0.01){
         c = 0;
-        while(n >= m[t]){
-            n -= m[t];
+        while (n >= d[t]){
+            n -= d[t];
             c++;
         }
-        printf("%d moeda(s) de R$ %.2f\n", c, m[t]);
+        if (d[t] == 1.0)
+            printf("MOEDAS:\n");
+        if (d[t] >= 2.0 )
+            printf("%d nota(s) de R$ %.2f\n", c, d[t]);
+        else
+            printf("%d moeda(s) de R$ %.2f\n", c, d[t]);
         t++;
-    } while(n >= 0.01);
+    }
     return 0;
 }
