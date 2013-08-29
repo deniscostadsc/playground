2d64c95bdb3a6f2f92b151c29e4090fef836dd25
Denis Costa <deniscostadsc@gmail.com>
2013-08-29 09:52:01 -0300

    Resolvendo 1157.

---
+++ uri/1157/1157.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+
+int main(){
+    int n, i;
+    scanf("%d", &n);
+    for (i = 1; i <= n / 2; i++){
+        if (n % i == 0)
+            printf("%d\n", i);
+    }
+    printf("%d\n", n);
+    return 0;
+}
