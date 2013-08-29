147b1c121e4bf8e96c364b2332a44b2a90d3e596
Denis Costa <deniscostadsc@gmail.com>
2013-08-28 21:24:54 -0300

    Resolvendo 1096.

---
+++ uri/1096/1096.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+
+int main(){
+    for (int i = 1; i <= 9; i += 2){
+        printf("I=%d J=7\n", i);
+        printf("I=%d J=6\n", i);
+        printf("I=%d J=5\n", i);
+    }
+    return 0;
+}
