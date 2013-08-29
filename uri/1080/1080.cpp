9a300f57970c5c100b1ab9e1039b337e7baee26a
Denis Costa <deniscostadsc@gmail.com>
2013-08-28 21:38:25 -0300

    Resolvendo 1080.

---
+++ uri/1080/1080.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+
+int main(){
+    int n, q = 0, g = -1, i = -1;
+    while(++q && q <= 100){
+        scanf("%d", &n);
+        if (n > g){
+            g = n; i = q;
+        }
+
+    }
+    printf("%d\n", g);
+    printf("%d\n", i);
+    return 0;
+}
