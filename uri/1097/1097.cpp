c3645ab729abb304795d1438051184a4c609dc35
Denis Costa <deniscostadsc@gmail.com>
2013-08-29 09:38:24 -0300

    Resolvendo 1097.

---
+++ uri/1097/1097.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+
+int main(){
+    int i = 1, j = 7;
+    while (i <= 9){
+        printf("I=%d J=%d\n", i, j--);
+        printf("I=%d J=%d\n", i, j--);
+        printf("I=%d J=%d\n", i, j--);
+        i += 2;
+        j += 5;
+    }
+    return 0;
+}
