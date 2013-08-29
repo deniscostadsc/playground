2823e9e9721bb542ad28e530eb9c324ed0a406e1
Denis Costa <deniscostadsc@gmail.com>
2013-08-29 10:45:01 -0300

    Resolvendo 1041.

---
+++ uri/1041/1041.cpp
@@ -0,0 +1,25 @@
+#include <cstdio>
+
+int main(){
+    float x, y;
+    scanf("%f %f", &x, &y);
+
+    if (x == 0.0 && y == 0.0)
+        printf("Origem\n");
+    else if (x == 0.0 && y != 0.0)
+        printf("Eixo Y\n");
+    else if (y == 0.0 && x != 0.0)
+        printf("Eixo X\n");
+    else if (x > 0.0)
+        if (y > 0.0)
+            printf("Q1\n");
+        else
+            printf("Q4\n");
+    else
+        if (y > 0.0)
+            printf("Q2\n");
+        else
+            printf("Q3\n");
+
+    return 0;
+}
