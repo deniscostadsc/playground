0034727f1cb1e267d7d9d9bd28d5d630daa79f42
Denis Costa <deniscostadsc@gmail.com>
2013-08-31 12:05:07 -0300

    Resolvendo 1116.

---
+++ uri/1116/1116.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+
+int main(){
+    float x, y;
+    int n;
+    scanf("%d", &n);
+    while(n--){
+        scanf("%f %f", &x, &y);
+        if (y == 0.0)
+            printf("divisao impossivel\n");
+        else
+            printf("%.1f\n", x / y);
+    }
+    return 0;
+}
