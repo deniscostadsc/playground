cc6cee52cfd0e500b49abc98581821bc67dab108
Denis Costa <deniscostadsc@gmail.com>
2013-08-31 00:25:17 -0300

    Resolvendo 1047.

---
+++ uri/1047/1047.cpp
@@ -0,0 +1,22 @@
+#include <cstdio>
+
+int main(){
+    int hi, mi, hf, mf, mr, hr;
+    scanf("%d %d %d %d", &hi, &mi, &hf, &mf);
+
+    hr = hf - hi;
+    if (hf - hi < 0)
+        hr = 24 + hf - hi;
+
+    mr = mf - mi;
+    if (mf - mi < 0){
+        mr = 60 + mf - mi;
+        hr--;
+    }
+
+    if (hf == hi && mf == mi)
+        printf("O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)\n");
+    else
+        printf("O JOGO DUROU %d HORA(S) E %d MINUTO(S)\n", hr, mr);
+    return 0;
+}
