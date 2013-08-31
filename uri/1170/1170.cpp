8f6d2dc3cec63451cc2b7029040026f8eb6ac1cf
Denis Costa <deniscostadsc@gmail.com>
2013-08-31 15:43:14 -0300

    Resolvendo 1170.

---
+++ uri/1170/1170.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+
+int main(){
+    int n, d;
+    float i;
+    scanf("%d", &n);
+    while (n--){
+        scanf("%f", &i);
+        d = 0;
+        while (i > 1.0){
+            d++;
+            i /= 2;
+        }
+        printf("%d dias\n", d);
+    }
+}
