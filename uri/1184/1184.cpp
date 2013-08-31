a7ce105e77fbfe5010e6c892f5f67aab8ba5c1c6
Denis Costa <deniscostadsc@gmail.com>
2013-08-31 19:44:12 -0300

    Resolvendo 1184.

---
+++ uri/1184/1184.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+
+int main(){
+    char o;
+    int i, j;
+    float s, n;
+    scanf("%c", &o);
+    for (i = 0; i <= 11; i++){
+        for (j = 0; j <= 11; j++){
+            scanf("%f", &n);
+            if (j < i)
+                s += n;
+        }
+    }
+    if (o == 'S')
+        printf("%.1f\n", s);
+    else
+        printf("%.1f\n", s / 66);
+    return 0;
+}
