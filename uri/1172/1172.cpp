6ce876d8e22c13fb98d01b52597fbd471498a541
Denis Costa <deniscostadsc@gmail.com>
2013-08-31 18:50:57 -0300

    Resolvendo 1172

---
+++ uri/1172/1172.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+
+int main(){
+    int n, i;
+    for (n = 0; n <= 9; n++){
+        scanf("%d", &i);
+        if (i > 0)
+            printf("X[%d] = %d\n", n, i);
+        else
+            printf("X[%d] = 1\n", n);
+    }
+    return 0;
+}
