34a786f1f63c355edc2de8f4c80961cbe07e80e6
Denis Costa <deniscostadsc@gmail.com>
2013-08-31 12:49:34 -0300

    Resolvendo 1146.

---
+++ uri/1146/1146.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+
+int main(){
+    int n, i;
+    while(scanf("%d", &n) && n != 0){
+        printf("1");
+        for (i = 2; i <= n; i++){
+            printf(" %d", i);
+        }
+        printf("\n");
+    }
+    return 0;
+}
