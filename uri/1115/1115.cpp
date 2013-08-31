1086455fcc4636b4cc49b5fd9f72892592ca289c
Denis Costa <deniscostadsc@gmail.com>
2013-08-31 20:06:18 -0300

    Resolvendo 1115.

---
+++ uri/1115/1115.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+
+int main(){
+    int x, y;
+    while(scanf("%d %d", &x, &y) != EOF && x != 0 && y != 0){
+        if (x > 0 && y > 0)
+            printf("primeiro\n");
+        else if (x < 0 && y < 0)
+            printf("terceiro\n");
+        else if (x < 0)
+            printf("segundo\n");
+        else
+            printf("quarto\n");
+    }
+    return 0;
+}
