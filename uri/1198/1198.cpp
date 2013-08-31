b6d12be968fea4807e1165f920b3da10f8704b74
Denis Costa <deniscostadsc@gmail.com>
2013-08-31 01:00:18 -0300

    Resolvendo 1198.

---
+++ uri/1198/1198.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+
+int main(){
+    long long int a, b;
+    while(scanf("%lld %lld", &a, &b) != EOF){
+        if (a > b)
+            printf("%lld\n", a - b);
+        else
+            printf("%lld\n", b - a);
+    }
+    return 0;
+}
