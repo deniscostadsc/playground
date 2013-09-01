28761fc5cdaa5c389e8d2962d4cd9014287c3c4d
Denis Costa <deniscostadsc@gmail.com>
2013-08-31 23:49:34 -0300

    Resolvendo 1052

---
+++ uri/1052/1052.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+#include <cstring>
+
+int main(){
+    char m[12][10] = {
+        "January", "February", "March", "April",
+        "May", "June", "July", "August",
+        "September", "October", "November", "December"
+    };
+    int n;
+
+    scanf("%d", &n);
+    for (int i = 0; i < strlen(m[n - 1]); i++){
+        printf("%c", m[n - 1][i]);
+    }
+    printf("\n");
+    return 0;
+}
