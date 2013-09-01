5dbb38666b67997e9ca4c5dd2c75b9639aad4616
Denis Costa <deniscostadsc@gmail.com>
2013-09-01 12:18:21 -0300

    Resolvendo 1117.

---
+++ uri/1117/1117.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+
+int main(){
+    float n, s = 0.0;
+    int c = 0;
+    while(c < 2){
+        scanf("%f", &n);
+        if (n >= 0.0 && n <= 10.0){
+            c++;
+            s += n;
+        } else {
+            printf("nota invalida\n");
+        }
+    }
+    printf("media = %.2f\n", s / 2.0);
+    return 0;
+}
