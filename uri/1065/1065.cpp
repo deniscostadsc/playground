18f6b61e7b036952c47883289b73a5c4dc4a113d
Denis Costa <deniscostadsc@gmail.com>
2013-09-01 00:10:26 -0300

    Resolvendo 1065.

---
+++ uri/1065/1065.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+
+int main(){
+    int n, c = 0, i = 5;
+    while(i--){
+        scanf("%d", &n);
+        if (n % 2 == 0)
+            c++;
+    }
+    printf("%d valores pares\n", c);
+
+    return 0;
+}
