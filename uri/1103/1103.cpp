e9de2189db626de59b2aa20fa8a812bb836b7d8d
Denis Costa <deniscostadsc@gmail.com>
2013-09-02 12:08:09 -0300

    Resolvendo 1103.

---
+++ uri/1103/1103.cpp
@@ -0,0 +1,26 @@
+#include <cstdio>
+
+int main(){
+    int hi, hf, mi, mf, m;
+     while (1) {
+        scanf("%d %d %d %d", &hi, &mi, &hf, &mf);
+        if (hi == 0 && hf == 0 && mi == 0 && mf == 0)
+            break;
+
+        if (hi <= hf)
+            m = (hf - hi) * 60;
+        else
+            m = (24 - (hi - hf)) * 60;
+
+        if (mi <= mf)
+            m += mf - mi;
+        else
+            m += 60 - 60 - mi + mf;
+
+        if (hi == hf &&  mi > mf)
+            m = 23 * 60 + (60 - mi + mf);
+
+        printf("%d\n", m);
+    }
+    return 0;
+}
