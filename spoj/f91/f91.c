bca8be7cf73d475c6d70f3e9b2521405efb96537
Denis Costa <deniscostadsc@gmail.com>
2013-09-03 20:41:59 -0300

    MElhorando o tempo do f91

--- spoj/f91/f91.c
+++ spoj/f91/f91.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
+#include <string.h>
+
+int r[1000000];
 
 int f91(int n){
+    if (r[n] != 0)
+        return r[n];
     if (n >= 101)
-        return n - 10;
-    return f91(f91(n + 11));
+        r[n] = n - 10;
+    else
+        r[n] = f91(f91(n + 11));
+    return r[n];
 }
 
 int main(){
     int n;
+    memset(r, 0, sizeof(r));
     while (scanf("%d", &n) && n != 0) {
         printf("f91(%d) = %d\n", n, f91(n));
     };
