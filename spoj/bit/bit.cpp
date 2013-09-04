76cd10b72f269bfc7949ff20f250e0305690d408
Denis Costa <deniscostadsc@gmail.com>
2013-09-03 22:11:48 -0300

    Resolvendo bit.

---
+++ spoj/bit/bit.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+
+int main(){
+    int n, c = 0, b[] = {50, 10, 5, 1};
+    while(scanf("%d", &n) && n != 0){
+        printf("Teste %d\n", ++c);
+        for (int i = 0; i <= 3; i++){
+            printf("%d", n / b[i]);
+            n %= b[i];
+            if (i < 3)
+                printf(" ");
+        }
+        printf("\n\n");
+    }
+    return 0;
+}
