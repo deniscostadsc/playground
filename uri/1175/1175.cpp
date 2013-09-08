e886780343b9758027128330210596418aed09ed
Denis Costa <deniscostadsc@gmail.com>
2013-09-08 19:00:18 -0300

    Resolvendo 1175.

---
+++ uri/1175/1175.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main(){
+    int x, i, a[20];
+    for (i = 0; i <= 19; i++) scanf("%d", &a[i]);
+
+    for (int i = 0; i <= 9; i++){
+        x = a[i];
+        a[i] = a[19 - i];
+        a[19 - i] = x;
+    }
+
+    for (i = 0; i <= 19; i++) printf("N[%d] = %d\n", i, a[i]);
+
+    return 0;
+}
