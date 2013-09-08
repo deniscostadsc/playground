37823149f6dbc6707d2f3cdbdad26d04c9714e1b
Denis Costa <deniscostadsc@gmail.com>
2013-09-08 20:44:12 -0300

    Resolvendo 1075.

---
+++ uri/1075/1075.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdio>
+
+int main(){
+    int n;
+    std::cin >> n;
+    for (int i = 1; i <= 10000; i++){
+        if (i % n == 2)
+            printf("%d\n", i);
+    }
+    return 0;
+}
