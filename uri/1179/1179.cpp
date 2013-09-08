669150e20ed0b11508ace99ab8c4efb0eca8cb52
Denis Costa <deniscostadsc@gmail.com>
2013-09-08 14:07:41 -0300

    Resolvendo 1179.

---
+++ uri/1179/1179.cpp
@@ -0,0 +1,36 @@
+#include <cstdio>
+
+int main(){
+    int j, n, p[5], i[5], pi = 0, ii = 0;
+    while(scanf("%d", &n) != EOF){
+        if (n % 2 == 0){
+            p[pi] = n;
+            pi++;
+        } else {
+            i[ii] = n;
+            ii++;
+        }
+
+        if (pi == 5){
+            for (j = 0; j <= 4; j++) printf("par[%d] = %d\n", j, p[j]);
+            for (j = 0; j <= 4; j++) p[j] = NULL;
+            pi = 0;
+        }
+        if (ii == 5){
+            for (j = 0; j <= 4; j++) printf("impar[%d] = %d\n", j, i[j]);
+            for (j = 0; j <= 4; j++) i[j] = NULL;
+            ii = 0;
+        }
+    }
+    for (j = 0; j <= 4; j++){
+        if (i[j] == NULL)
+            break;
+        printf("impar[%d] = %d\n", j, i[j]);
+    }
+    for (j = 0; j <= 4; j++){
+        if (p[j] == NULL)
+            break;
+        printf("par[%d] = %d\n", j, p[j]);
+    }
+    return 0;
+}
