5cd2db37a9c0285ae47f41c74c558a6f544c5bf0
Denis Costa <deniscostadsc@gmail.com>
2013-09-07 23:32:21 -0300

    Resolvendo 1046.

---
+++ uri/1046/1046.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+
+int main(){
+    int hi, hf, hr;
+    scanf("%d %d", &hi, &hf);
+
+    hr = hf - hi;
+    if (hf - hi < 0)
+        hr = 24 + hf - hi;
+
+    if (hf == hi)
+        printf("O JOGO DUROU 24 HORA(S)\n");
+    else
+        printf("O JOGO DUROU %d HORA(S)\n", hr);
+    return 0;
+}
