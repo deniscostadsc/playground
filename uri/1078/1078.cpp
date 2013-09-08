7063aae6fccda600569ebc0e8ca8ef23a52e4144
Denis Costa <deniscostadsc@gmail.com>
2013-09-08 20:52:11 -0300

    Resolvendo 1078.

---
+++ uri/1078/1078.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n, i;
+    cin >> n;
+    for (i = 1; i <= 10; i++)
+        cout << i << " x " << n << " = " << i * n << endl;
+    return 0;
+}
