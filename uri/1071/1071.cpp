d4d3b34ccd86ffbc5d339c0af8011deb04ec0653
Denis Costa <deniscostadsc@gmail.com>
2013-09-08 20:00:40 -0300

    Resolvendo 1071.

---
+++ uri/1071/1071.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main(){
+    int i, x, y, s = 0;
+
+    std::cin >> x;
+    std::cin >> y;
+
+    if (x < y){
+        for (i = x + 1; i < y; i++){
+            if (i % 2 != 0) s += i;
+        }
+    } else {
+        for (i = y + 1; i < x; i++){
+            if (i % 2 != 0) s += i;
+        }
+    }
+
+    std::cout << s << std::endl;
+
+    return 0;
+}
