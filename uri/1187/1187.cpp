472ed8ec174961e6d8006dec9d1b98c503632c2e
Denis Costa <deniscostadsc@gmail.com>
2013-09-07 23:03:41 -0300

    Resolvendo 1187.

---
+++ uri/1187/1187.cpp
@@ -0,0 +1,21 @@
+#include <cstdio>
+
+int main(){
+    char o;
+    int i, j, l = 1;
+    double s = 0, n;
+    scanf("%c", &o);
+    for (i = 0; i <= 4; i++){
+        for (j = 0; j <= 11; j++){
+            scanf("%lf", &n);
+            if (j >= l && j <= 11 - l)
+                s += n;
+        }
+        l++;
+    }
+    if (o == 'S')
+        printf("%.1f\n", s);
+    else
+        printf("%.1f\n", s / 30);
+    return 0;
+}
