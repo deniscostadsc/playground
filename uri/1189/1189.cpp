02436c2843c21867b7fcffd1035a3af1478509d7
Denis Costa <deniscostadsc@gmail.com>
2013-09-08 18:04:14 -0300

    Refatorando 1189.

---
+++ uri/1189/1189.cpp
@@ -0,0 +1,24 @@
+#include <cstdio>
+
+int main(){
+    char o;
+    int i, j, l = 0;
+    double s = 0, n;
+    scanf("%c", &o);
+    for (i = 0; i <= 11; i++){
+        for (j = 0; j <= 11; j++){
+            scanf("%lf", &n);
+            if (j < l)
+                s += n;
+        }
+        if (i < 5)
+            l++;
+        else if (i >= 6)
+            l--;
+    }
+    if (o == 'S')
+        printf("%.1f\n", s);
+    else
+        printf("%.1f\n", s / 30);
+    return 0;
+}
