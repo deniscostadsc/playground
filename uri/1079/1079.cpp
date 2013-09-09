a16d7bfdc3b97d48e69ed9eba138ea1c5452322a
Denis Costa <deniscostadsc@gmail.com>
2013-09-08 21:09:25 -0300

    Resolvendo 1079.

---
+++ uri/1079/1079.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdio>
+
+int main(){
+    int n;
+    float a, b, c;
+
+    scanf("%d", &n);
+    while (n--){
+        scanf("%f %f %f", &a, &b, &c);
+        printf("%.1f\n", (a * 2.0 + b * 3.0 + c * 5.0) / 10.0);
+    }
+    
+    return 0;
+}
