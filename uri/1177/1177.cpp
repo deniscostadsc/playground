47e5223b819411f47be5106c91b669add4c6e11c
Denis Costa <deniscostadsc@gmail.com>
2013-09-08 22:43:35 -0300

    Resolvendo 1177.

---
+++ uri/1177/1177.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdio>
+
+int main(){
+    int n, i, j = 0;
+    std::cin >> n;
+    for (i = 0; i < 1000; i++){
+        if (j >= n)
+            j = 0;
+        printf("N[%d] = %d\n", i, j++);
+    }
+    return 0;
+}
