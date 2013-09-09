b17cc955a1abf32eb599718bdbaf5144a2c4a084
Denis Costa <deniscostadsc@gmail.com>
2013-09-09 10:02:51 -0300

    Resolvendo 1029.

---
+++ uri/1029/1029.cpp
@@ -0,0 +1,35 @@
+#include <cstdio>
+
+long int f[39];
+long int r[39];
+
+long int fib(long int n){
+    if (f[n] != -1)
+        return f[n];
+    if (n == 0)
+        f[n] = 0;
+    else if (n == 1)
+        f[n] = 1;
+    else {
+        f[n] = fib(n - 1) + fib(n - 2);
+        if (n >= 3)
+            r[n] = r[n - 1] + r[n - 2] + 2;
+    }
+    return f[n];
+}
+
+int main(){
+    int i, j;
+    long int n;
+    for (j = 0; j <= 60; j++) { f[j] = -1; r[j] = -1; }
+    r[0] = 0;
+    r[1] = 0;
+    r[2] = 2;
+
+    scanf("%d", &i);
+    while(i--){
+        scanf("%ld", &n);
+        printf("fib(%ld) = %ld calls = %ld\n", n, r[n], fib(n));
+    }
+    return 0;
+}
