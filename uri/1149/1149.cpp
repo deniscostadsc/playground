de1c3b79b6308d3532a267a401c6e45ad60329e9
Denis Costa <deniscostadsc@gmail.com>
2013-09-08 22:29:46 -0300

    Resolvendo 1149.

---
+++ uri/1149/1149.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+    int a, b, s;
+    std::cin >> a;
+    s = a;
+    std::cin >> b;
+    while (b <= 0)
+        std::cin >> b;
+    for (int i = 1; i < b; i++)
+        s += ++a;
+    std::cout << s << std::endl;
+    return 0;
+}
