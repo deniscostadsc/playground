521f14e270ec526f9ffb78bcc700b667299d926a
Denis Costa <deniscostadsc@gmail.com>
2013-09-08 22:30:28 -0300

    Refatorando 1149.

--- uri/1149/1149.cpp
+++ uri/1149/1149.cpp
@@ -2,13 +2,15 @@
 
 int main(){
     int a, b, s;
+
     std::cin >> a;
     s = a;
     std::cin >> b;
-    while (b <= 0)
-        std::cin >> b;
+
+    while (b <= 0) std::cin >> b;
     for (int i = 1; i < b; i++)
         s += ++a;
+
     std::cout << s << std::endl;
     return 0;
 }
