ec69e59dc6a9303405ce9748f1858d87c99b88f5
Denis Costa <deniscostadsc@gmail.com>
2013-09-10 09:25:42 -0300

    Refatorando 1174.

--- uri/1174/1174.cpp
+++ uri/1174/1174.cpp
@@ -6,9 +6,7 @@ using namespace std;
 int main(){
     int n = -1;
     float i;
-    while (1){
-        n++;
-        if (n > 99) break;
+    while (++n <= 99){
         cin >> i;
         if (i <= 10){
             cout << "A[" << n << "] = " << i << endl;
