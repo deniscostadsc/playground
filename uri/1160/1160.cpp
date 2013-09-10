8a390b0ee0548c8c4a9a55d9dadb65044beb2688
Denis Costa <deniscostadsc@gmail.com>
2013-09-09 21:12:35 -0300

    Resolvendo 1160.

---
+++ uri/1160/1160.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdio>
+
+int main(){
+    int n, c;
+    int a, b;
+    float ac, bc;
+
+    std::cin >> n;
+    while (n--){
+        c = 0;
+        scanf("%d %d %f %f", &a, &b, &ac, &bc);
+        while(a <= b){
+            a *= (ac / 100.0) + 1.0;
+            b *= (bc / 100.0) + 1.0;
+            c++;
+            if (c > 100){
+                printf("Mais de 1 seculo.\n");
+                break;
+            }
+        }
+        if (c <= 100)
+            printf("%d anos.\n", c);
+    }
+    return 0;
+}
