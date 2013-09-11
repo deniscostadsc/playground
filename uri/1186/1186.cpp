a8635a819009239c7c06333d811b6e7135be15ad
Denis Costa <deniscostadsc@gmail.com>
2013-09-10 22:10:16 -0300

    Resolvendo 1186.

---
+++ uri/1186/1186.cpp
@@ -0,0 +1,21 @@
+#include <cstdio>
+
+int main(){
+    char o;
+    int i, j, x = 11;
+    float s = 0, n;
+    scanf("%c", &o);
+    for (i = 0; i <= 11; i++){
+        for (j = 0; j <= 11; j++){
+            scanf("%f", &n);
+            if (x < j)
+                s += n;
+        }
+        x--;
+    }
+    if (o == 'S')
+        printf("%.1f\n", s);
+    else
+        printf("%.1f\n", s / 66);
+    return 0;
+}
