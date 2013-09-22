d134cd2acffe4f031e95093a2a437ff7f155b3d0
Denis Costa <deniscostadsc@gmail.com>
2013-09-22 20:53:04 -0300

    s/tab/space/

--- spoj/test/test.cpp
+++ spoj/test/test.cpp
@@ -3,11 +3,11 @@
 using namespace std;
 
 int main(){
-	string n;
-	while (cin >> n){
-		if (n == "42")
-			break;
-		cout << n << endl;
-	}
+    string n;
+    while (cin >> n){
+        if (n == "42")
+            break;
+        cout << n << endl;
+    }
     return 0;
 }
