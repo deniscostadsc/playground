baa75648a57ac77a9014737b4f73cbafe4c3b653
Denis Costa <deniscostadsc@gmail.com>
2013-10-06 14:26:41 -0300

    Solving dbpr.

---
+++ rosalind/dbpr/dbpr.py
@@ -0,0 +1,15 @@
+import urllib
+import sys
+
+protein_id = sys.stdin.read().replace('\n', '')
+
+base_url = 'http://www.uniprot.org/uniprot/%s.txt'
+
+protein_description = urllib.urlopen(base_url % protein_id).read()
+
+for line in protein_description.split('\n'):
+    line = line[5:]
+    if line.startswith('GO'):
+        bio_process = line.split(';')[2]
+        if bio_process.startswith(' P:'):
+            print bio_process[3:]
