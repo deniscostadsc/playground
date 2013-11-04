be885db76bd7a536a12ef7742033ca9d3e6d9211
Denis Costa <deniscostadsc@gmail.com>
2013-11-04 15:13:44 -0200

    Refatorando 1029.

--- uri/1029/1029.cpp
+++ uri/1029/1029.cpp
@@ -6,11 +6,9 @@ long int r[39];
 long int fib(long int n){
     if (f[n] != -1)
         return f[n];
-    if (n == 0)
-        f[n] = 0;
-    else if (n == 1)
-        f[n] = 1;
-    else {
+    if (n <= 1) {
+        f[n] = n;
+    } else {
         f[n] = fib(n - 1) + fib(n - 2);
         if (n >= 3)
             r[n] = r[n - 1] + r[n - 2] + 2;
@@ -21,7 +19,11 @@ long int fib(long int n){
 int main(){
     int i, j;
     long int n;
-    for (j = 0; j <= 60; j++) { f[j] = -1; r[j] = -1; }
+    for (j = 0; j <= 39; j++) {
+        f[j] = -1;
+        r[j] = -1;
+    }
+
     r[0] = 0;
     r[1] = 0;
     r[2] = 2;
