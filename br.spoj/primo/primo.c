1a9b143ff3e4416b1a1768336b6855c0399d2a6e
Denis Costa <deniscostadsc@gmail.com>
2013-11-15 18:31:29 -0200

    Refatorando primo.

--- br.spoj/primo/primo.c
+++ br.spoj/primo/primo.c
@@ -2,19 +2,13 @@
 #include <math.h>
 
 int is_prime (long n){
+    int i;
+
     if (n < 2) return 0;
     if (n == 2) return 1;
-    if (n == 3) return 1;
-    if (n == 5) return 1;
-    if (n == 7) return 1;
     if (n % 2 == 0) return 0;
-    if (n % 3 == 0) return 0;
-    if (n % 5 == 0) return 0;
-    if (n % 7 == 0) return 0;
 
-    int i;
-    int k = (int) sqrt(n) * 2;
-    for (i = 11; i < k; i++){
+    for (i = 11; i <= sqrt(n); i++){
         if (n % i == 0)
             return 0;
     }
@@ -23,11 +17,10 @@ int is_prime (long n){
 
 int main(){
     long n;
+
     scanf("%ld", &n);
-    if (is_prime(n)){
-        printf("sim");
-    } else {
-        printf("nao");
-    }
+    if (is_prime(n)) printf("sim");
+    else printf("nao");
+    
     return 0;
 }
