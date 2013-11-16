9044353811808d72c5e6314313612f12bf7f84d4
Denis Costa <deniscostadsc@gmail.com>
2013-11-16 17:47:50 -0200

    Refatorando função dos primos.

--- projecteuler/3/3.c
+++ projecteuler/3/3.c
@@ -3,17 +3,12 @@
 
 int is_prime (int n){
     int i;
+
     if (n < 2) return 0;
     if (n == 2) return 1;
-    if (n == 3) return 1;
-    if (n == 5) return 1;
-    if (n == 7) return 1;
     if (n % 2 == 0) return 0;
-    if (n % 3 == 0) return 0;
-    if (n % 5 == 0) return 0;
-    if (n % 7 == 0) return 0;
 
-    for (i = 11; i < n / 2; i++){
+    for (i = 2; i <= sqrt(n); i++){
         if (n % i == 0)
             return 0;
     }
@@ -24,10 +19,13 @@ int main(){
     int i;
     long long n = 600851475143;
     long long r;
-    for (i = 2; i < sqrt(n); i++)
+
+    for (i = 2; i <= sqrt(n); i++){
         if (n % i == 0 && is_prime(i)){
             r = i;
         }
+    }
     printf("%lld\n", r);
+
     return 0;
 }
