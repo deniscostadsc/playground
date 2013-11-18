3c700610b3f9363b7a6e4cc535bae7e751120137
Denis Costa <deniscostadsc@gmail.com>
2013-11-18 01:47:45 -0200

    Resolvendo 10.

---
+++ projecteuler/10/10.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+bool is_prime(unsigned long n){
+    if (n == 2) return true;
+    if (n % 2 == 0 || n < 2) return false;
+
+    for (unsigned long i = 3; i <= sqrt(n); i += 2){
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
+int main(){
+    unsigned int i;
+    unsigned long long sum = 2;
+
+    for (i = 3; i < 2000000; i += 2){
+        if (is_prime(i)) sum += i;
+    }
+
+    cout << sum << endl;
+
+    return 0;
+}
