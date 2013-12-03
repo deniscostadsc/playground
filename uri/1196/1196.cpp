690ecca02a8028f179a8e308a344f9ea62a8f8ac
Denis Costa <deniscostadsc@gmail.com>
2013-12-03 15:53:40 -0200

    Resolvendo 1196.

---
+++ uri/1196/1196.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <cstdio>
+#include <map>
+
+using namespace std;
+
+int main(){
+    char c;
+    map<char, char> keyboard;
+
+    keyboard['1'] = '`';
+    keyboard['2'] = '1';
+    keyboard['3'] = '2';
+    keyboard['4'] = '3';
+    keyboard['5'] = '4';
+    keyboard['6'] = '5';
+    keyboard['7'] = '6';
+    keyboard['8'] = '7';
+    keyboard['9'] = '8';
+    keyboard['0'] = '9';
+    keyboard['-'] = '0';
+    keyboard['='] = '-';    
+
+    keyboard['W'] = 'Q';
+    keyboard['E'] = 'W';
+    keyboard['R'] = 'E';
+    keyboard['T'] = 'R';
+    keyboard['Y'] = 'T';
+    keyboard['U'] = 'Y';
+    keyboard['I'] = 'U';
+    keyboard['O'] = 'I';
+    keyboard['P'] = 'O';
+    keyboard['['] = 'P';
+    keyboard[']'] = '[';
+    keyboard['\\'] = ']';
+
+    keyboard['S'] = 'A';
+    keyboard['D'] = 'S';
+    keyboard['F'] = 'D';
+    keyboard['G'] = 'F';
+    keyboard['H'] = 'G';
+    keyboard['J'] = 'H';
+    keyboard['K'] = 'J';
+    keyboard['L'] = 'K';
+    keyboard[';'] = 'L';
+    keyboard['\''] = ';';
+
+    keyboard['X'] = 'Z';
+    keyboard['C'] = 'X';
+    keyboard['V'] = 'C';
+    keyboard['B'] = 'V';
+    keyboard['N'] = 'B';
+    keyboard['M'] = 'N';
+    keyboard[','] = 'M';
+    keyboard['.'] = ',';
+    keyboard['/'] = '.';
+
+    while((c = getchar()) != EOF){
+        if (keyboard.find(c) != keyboard.end()) cout << keyboard[c];
+        else cout << c;
+    }
+
+    return 0;
+}
---
+++ uri/1196/generate_in.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+alphabet=(" " B C D E F G H I J K L M N O P R S T U V W X Y \] \[ \\ \; \' , . / - = 1 2 3 4 5 6 7 8 9 0 )
+
+for (( i = 0; i < 1000; i++ )); do
+    n=$(($RANDOM % 100 + 10))
+    for (( j = 0; j < $n; j++ )); do
+        echo -n "${alphabet[$(($RANDOM % 45))]}"
+    done
+    echo ""
+done
+
+
+    
