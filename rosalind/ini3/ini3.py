310dab2c2c35d208752a92fef8dc858d4982bb25
Denis Costa <deniscostadsc@gmail.com>
2014-02-02 19:38:21 -0200

    Resolvendo ini3.

---
+++ rosalind/ini3/ini3.py
@@ -0,0 +1,7 @@
+import sys
+
+stdin = sys.stdin.readlines()
+
+_string = stdin[0].replace('\n', '')
+numbers = map(lambda x: int(x), stdin[1].split())
+print('%s %s' % (_string[numbers[0]:numbers[1] + 1], _string[numbers[2]:numbers[3] + 1]))
