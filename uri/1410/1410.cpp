f07533c816dc1c6b671fffc66d8edc6d3fb4dc56
Denis Costa <deniscostadsc@gmail.com>
2014-08-08 07:53:29 +0200

    Resolvendo 1410.

---
+++ uri/1410/1410.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a, d, i, j, nearest_attack_player, defense_distance, count, temp;
+
+    while (cin >> a >> d && a != 0 && d != 0){
+        cin >> nearest_attack_player;
+
+        for (i = 1; i < a; i++){
+            cin >> temp;
+            if (temp < nearest_attack_player) nearest_attack_player = temp;
+        }
+
+        count = 0;
+
+        for (j = 0; j < d; j++){
+            cin >> defense_distance;
+            if (defense_distance <= nearest_attack_player) count++;
+        }
+
+        if (count >= 2) cout << "N" << endl;
+        else cout << "Y" << endl;
+    }
+
+    return 0;
+}
