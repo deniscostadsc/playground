50d011473d2a0e8103243d4e3855c6fd1757413f
Denis Costa <deniscostadsc@gmail.com>
2014-11-03 00:38:34 -0200

    Corrige problema.

--- uri/1002/1002.cpp
+++ uri/1002/1002.cpp
@@ -1,11 +1,9 @@
 #include <cstdio>
 
-#define pi 3.14159
-
 int main(){
-    int n;
-    while (scanf("%d", &n) != EOF){
-        printf("A=%.4f\n", n * n * pi);
-    }
+    double n;
+    scanf("%lf", &n);
+    printf("A=%.4f\n", n * n * 3.14159);
+
     return 0;
 }
