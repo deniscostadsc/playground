628292e039493a2960b9a8bf2fa6671eb8f40f4f
Denis Costa <deniscostadsc@gmail.com>
2014-12-31 12:57:41 -0200

    Corrige e reformata.

--- uri/1045/1045.cpp
+++ uri/1045/1045.cpp
@@ -8,18 +8,13 @@ int main(){
     if (b < c){ x = b; b = c; c = x; }
     if (a < b){ x = a; a = b; b = x; }
 
-    if (a > b + c)
-        printf("NAO FORMA TRIANGULO\n");
-    else if (a * a == b * b + c * c)
-        printf("TRIANGULO RETANGULO\n");
-    else if (a * a > b * b + c * c)
-        printf("TRIANGULO OBTUSANGULO\n");
-    else if (a * a < b * b + c * c)
-        printf("TRIANGULO ACUTANGULO\n");
-    if (a == b && b == c)
-        printf("TRIANGULO EQUILATERO\n");
-    else if (a == b || b == c)
-        printf("TRIANGULO ISOSCELES\n");
+    if (a >= b + c) printf("NAO FORMA TRIANGULO\n");
+    else if (a * a == b * b + c * c) printf("TRIANGULO RETANGULO\n");
+    else if (a * a > b * b + c * c) printf("TRIANGULO OBTUSANGULO\n");
+    else if (a * a < b * b + c * c) printf("TRIANGULO ACUTANGULO\n");
+
+    if (a == b && b == c) printf("TRIANGULO EQUILATERO\n");
+    else if (a == b || b == c) printf("TRIANGULO ISOSCELES\n");
 
     return 0;
 }
