53ec3c339b335e26173a89e91692c21c92058df9
Denis Costa <deniscostadsc@gmail.com>
2014-12-31 16:01:50 -0200

    Resolve 1536.

---
+++ uri/1536/1536.cpp
@@ -0,0 +1,26 @@
+#include <cstdio>
+
+int main() {
+    int n, team_1_1, team_2_1, team_1_2, team_2_2;
+    
+    scanf("%d", &n);
+
+    while (n--) {
+        scanf("%d x %d", &team_1_1, &team_2_1);
+        scanf("%d x %d", &team_2_2, &team_1_2);
+
+        if (team_1_1 + team_1_2 > team_2_1 + team_2_2) {
+            puts("Time 1");
+        } else if (team_1_1 + team_1_2 < team_2_1 + team_2_2) {
+            puts("Time 2");
+        } else if (team_1_2 > team_2_1) {
+            puts("Time 1");
+        } else if (team_1_2 < team_2_1) {
+            puts("Time 2");
+        } else {
+            puts("Penaltis");
+        }
+    }
+
+    return 0;
+}
