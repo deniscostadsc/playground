f285a5975584d0d98b11e2ab924441a80568e62c
Denis Costa <deniscostadsc@gmail.com>
2015-01-02 10:02:54 -0200

    Corrige 1036.

--- uri/1036/1036.cpp
+++ uri/1036/1036.cpp
@@ -1,15 +1,18 @@
 #include <cstdio>
 #include <cmath>
 
-int main(){
-    float a, b, c, d;
-    scanf("%f %f %f", &a, &b, &c);
-    d = (b * b) - 4 * a * c;
-    if (d > 0 || 2 * a == 0){
-        printf("R1 = %.5f\n", ((b * -1) + sqrt(d)) / (2 * a));
-        printf("R2 = %.5f\n", ((b * -1) - sqrt(d)) / (2 * a));
+int main() {
+    double a, b, c, delta;
+
+    scanf("%lf %lf %lf", &a, &b, &c);
+
+    delta = (b * b) - 4 * a * c;
+    if (delta >= 0 && a != 0) {
+        printf("R1 = %.5lf\n", ((b * -1) + sqrt(delta)) / (2 * a));
+        printf("R2 = %.5lf\n", ((b * -1) - sqrt(delta)) / (2 * a));
     } else {
         printf("Impossivel calcular\n");
     }
+
     return 0;
 }
