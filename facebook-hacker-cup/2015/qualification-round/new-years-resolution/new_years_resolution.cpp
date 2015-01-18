78d9aa27fd4b2a72ed1c08c84779f016552b9503
Denis Costa <deniscostadsc@gmail.com>
2015-01-18 20:14:52 -0200

    Resolvendo o primeiro do Facebook hacker cup.

---
+++ facebook-hacker-cup/2015/qualification-round/new-years-resolution/new_years_resolution.cpp
@@ -0,0 +1,74 @@
+#include <cstdio>
+#include <vector>
+
+using namespace std;
+
+int gp, gc, gf;
+vector< vector<int> > foods;
+
+bool still_valid(int p, int c, int f) {
+    return p <= gp && c <= gc && f <= gf;
+}
+
+bool is_ok(int p, int c, int f) {
+    return p == gp && c == gc && f == gf;
+}
+
+bool backtrack(int p, int c, int f, vector<int> used_food) {
+    bool ok = false;
+    int next;
+
+    if (!still_valid(p, c, f)) return false;
+    if (is_ok(p, c, f)) return true;
+
+    if ((int)used_food.size() == 0) next = 0;
+    else next = used_food[used_food.size() - 1] + 1;
+
+    while (next < (int)foods.size()) {
+        vector<int> copy_used_food(used_food);
+        copy_used_food.insert(copy_used_food.end(), next);
+        ok = backtrack(p + foods[next][0],
+                       c + foods[next][1],
+                       f + foods[next][2],
+                       copy_used_food) || ok;
+        next++;
+    }
+
+    return ok;
+}
+
+bool can_eat_something() {
+    vector<int> e;
+    return backtrack(0, 0, 0, e);
+}
+
+int main() {
+    int t, n, p, c, f, i = 1;
+
+    scanf("%d", &t);
+    while (i <= t) {
+        scanf("%d %d %d ", &gp, &gc, &gf);
+        scanf("%d", &n);
+
+        while (n--) {
+            vector<int> food;
+            
+            scanf("%d %d %d ", &p, &c, &f);
+            
+            food.insert(food.end(), p);
+            food.insert(food.end(), c);
+            food.insert(food.end(), f);
+            foods.insert(foods.end(), food);
+        }
+
+        printf("Case #%d: ", i);
+        if (can_eat_something()) puts("yes");
+        else puts("no");
+
+        foods.clear();
+
+        i++;
+    }
+
+    return 0;
+}
