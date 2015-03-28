8f63ee0522c71605ba2315abeab7b4bdef6e10a4
Denis Costa <deniscostadsc@gmail.com>
2015-03-28 08:36:40 -0300

    Corrige 1021.

--- uri/1021/1021.cpp
+++ uri/1021/1021.cpp
@@ -2,13 +2,13 @@
 
 int main(){
     double n,  d[] = {100.0, 50.0, 20.0, 10.0, 5.0, 2.0, 1.0, 0.5, 0.25, 0.10, 0.05, 0.01};
-    int t = 0, c;
+    int t = 0, c, i;
 
     scanf("%lf", &n);
     printf("NOTAS:\n");
 
     t = 0;
-    while (n >= 0.01){
+    for (i = 0; i < 12; i++) {
         c = 0;
         while (n >= d[t]){
             n -= d[t];
