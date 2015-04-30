96d8e2dfb4f0f861f7ba8d0fb96e5ce4a1cb7485
Denis Costa <deniscostadsc@gmail.com>
2015-04-30 10:08:16 -0300

    Renomeia função de contrução da árvore.

--- uri/1191/1191.cpp
+++ uri/1191/1191.cpp
@@ -2,16 +2,16 @@
 
 using namespace std;
 
-void build_tree_from_preorder_inorder(string pre, string in) {
+void postorder_print_from_preorder_inorder(string pre, string in) {
     int in_root_index = in.find(pre[0]);
 
     if (in.substr(0, in_root_index).length() > 0) {
-        build_tree_from_preorder_inorder(
+        postorder_print_from_preorder_inorder(
             pre.substr(1),
             in.substr(0, in_root_index));
     }
     if (in.substr(in_root_index + 1).length() > 0) {
-        build_tree_from_preorder_inorder(
+        postorder_print_from_preorder_inorder(
             pre.substr(in_root_index + 1),
             in.substr(in_root_index + 1));
     }
@@ -23,7 +23,7 @@ int main() {
     string pre, in;
 
     while (cin >> pre >> in) {
-        build_tree_from_preorder_inorder(pre, in);
+        postorder_print_from_preorder_inorder(pre, in);
         cout << endl;
     }
 
