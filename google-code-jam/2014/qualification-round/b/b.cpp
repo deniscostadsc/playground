e3d8d4aa7096bdefef8ac19a1e232d9676f3194c
Denis Costa <deniscostadsc@gmail.com>
2015-05-02 18:41:20 -0300

    Organiza mas não resolve.

--- google-code-jam/2014/qualification-round/b/b.cpp
+++ google-code-jam/2014/qualification-round/b/b.cpp
@@ -12,56 +12,27 @@ int main(){
         total_seconds = 0.0;
         cookies = 0.0;
 
-        printf("%f\n", c);
-        printf("%f\n", c / cookies_per_second);
+        while (cookies < x){
+            if (c / cookies_per_second){
+                ;
+            }
 
-        printf("%f\n", x);
-        printf("%f\n", x / cookies_per_second);
+            if (c / cookies_per_second < seconds + x / (cookies_per_second + f) && cookies >= c){
 
-        printf("%f\n", x / (cookies_per_second + f));
+                seconds = c / cookies_per_second;
 
-        // while (cookies < x){
-            // if (c / cookies_per_second){
-                // ;
-            // }
+                total_seconds += seconds;
+                cookies_per_second += f;
+                cookies -= c;
+            } else {
+                seconds =   x / cookies_per_second;
+                cookies += seconds * cookies_per_second;
+                total_seconds += seconds;
+                break;
+            }
+        }
 
-            // printf("c f x cookies_per_second: %f %f %f %f\n", c, f, x, cookies_per_second);
-
-            // printf("c / cookies_per_second %f\n", c / cookies_per_second);
-            // printf("seconds + x / (cookies_per_second + f) %f\n", seconds + x / (cookies_per_second + f));
-
-            // if (c / cookies_per_second < seconds + x / (cookies_per_second + f) && cookies >= c){
-            //     printf("1 +C: %f\n", c);
-            //     printf("1 +Seconds: %f\n", seconds);
-
-            //     seconds = c / cookies_per_second;
-
-            //     printf("+Seconds: %f\n", seconds);
-
-            //     total_seconds += seconds;
-            //     cookies_per_second += f;
-            //     cookies -= c;
-
-            //     printf("+Cookies: %f\n", cookies);
-
-            // } else {
-
-
-            //     seconds =   x / cookies_per_second;
-
-            //     printf("-Seconds: %f\n", seconds);
-
-            //     cookies += seconds * cookies_per_second;
-
-            //     printf("-Cookies: %f\n", cookies);
-
-            //     total_seconds += seconds;
-            //     // break;
-            // }
-        // }
-
-        // printf("Case #%d: %f\n", ++case_n, total_seconds);
-
+        printf("Case #%d: %f\n", ++case_n, total_seconds);
     }
 
     return 0;
