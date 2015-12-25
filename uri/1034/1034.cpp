0102a369922881ab5555c8f7e84acb879d1281cb
Denis Costa <deniscostadsc@gmail.com>
2015-12-25 15:40:31 -0200

    Not done yet! :rage:

--- uri/1034/1034.cpp
+++ uri/1034/1034.cpp
@@ -1,36 +1,52 @@
+#include <algorithm>
 #include <iostream>
 #include <set>
+#include <vector>
 
 using namespace std;
 
+vector<int> solutions;
+
+int change(int m, set<int>::iterator it_a_begin,
+           set<int>::iterator it_a_end, int n) {
+
+    cout << *it_a_begin << endl;
+
+    if (m < 0 || it_a_begin == it_a_end) return 0;
+    if (m == 0) {
+        solutions.push_back(n);
+        return 1;
+    }
+
+    int r1 = change(m - *it_a_begin, it_a_begin, it_a_end, ++n);
+    int r2 = change(m, ++it_a_begin, it_a_end, --n);
+
+    return r1 + r2;
+}
+
 int main() {
-    int t, n, m, z, count;
+    int t, n, m, z;
     set<int> a;
-    set<int>::reverse_iterator ri;
     
     cin >> t;
 
     while (t--) {
-        count = 0;
         cin >> n >> m;
+
         while (n--) {
             cin >> z;
             a.insert(z);
         }
 
-        while (m) {
-            for (ri = a.rbegin(); ri != a.rend(); ++ri) {
-                if (m >= *ri){
-                    count += m / *ri;
-                    m -= *ri * (m / *ri);
-                }
-            }
-        }
-        
-        a.clear();
+        change(m, a.begin(), a.end(), 0);
 
-        cout << count << '\n';
+        cout << *min_element(solutions.begin(), solutions.end()) << endl;
+
+        a.clear();
+        solutions.clear();
     }
 
     return 0;
 }
+
+//for (set<int>::iterator it = a.begin(); it != a.end(); it++) { cout << *it << endl; }
--- uri/1034/generate_in.sh
+++ uri/1034/generate_in.sh
@@ -6,14 +6,14 @@ echo $TESTCASES
 
 for i in $(seq $TESTCASES); do
     n=$(($RANDOM % 24 + 2))
-    m=$(($RANDOM % 1000 + 10))
+    m=$(($RANDOM % 100000 + 10))
 
     echo "$n $m"
 
     echo -n "1 "
     a=0
     for j in $(seq $(($n - 1))); do
-        a=$(($a + 5))
+        a=$(($a + $(($RANDOM % 9 + 1))))
         echo -n "$a "
     done
     echo
