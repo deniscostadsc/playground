1524c623b819953086c9bb7a25677e26a48e0f06
Denis Costa <deniscostadsc@gmail.com>
2015-12-26 01:20:53 -0200

    Formata script de provisionamento.

--- provisioner.sh
+++ provisioner.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 apt-get update && sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y
+apt-get install -y nodejs g++
 
 cp /vagrant/marathon.sh /home/vagrant/
 
-apt-get install -y nodejs g++
 echo '. /home/vagrant/marathon.sh' >> /home/vagrant/.bashrc
 echo 'cd /vagrant' >> /home/vagrant/.bashrc
