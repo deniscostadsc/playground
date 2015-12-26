038cbadf26b59f379045fd40135ab2b33fdb2b5c
Denis Costa <deniscostadsc@gmail.com>
2015-12-26 01:44:20 -0200

    Oculta script com funções auxiliares.

--- provisioner.sh
+++ provisioner.sh
@@ -3,7 +3,7 @@
 apt-get update && sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y
 apt-get install -y nodejs g++
 
-cp /vagrant/marathon.sh /home/vagrant/
+cp /vagrant/.marathon.sh /home/vagrant/
 
-echo '. /home/vagrant/marathon.sh' >> /home/vagrant/.bashrc
+echo '. /home/vagrant/.marathon.sh' >> /home/vagrant/.bashrc
 echo 'cd /vagrant' >> /home/vagrant/.bashrc
