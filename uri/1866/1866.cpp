287c553d96ea0dd58c0c424a5ad82d0cfdbbed3a
Denis Costa <deniscostadsc@gmail.com>
2015-12-25 23:48:58 -0200

    Refatora 1866.

--- uri/1866/1866.cpp
+++ uri/1866/1866.cpp
@@ -3,20 +3,15 @@
 using namespace std;
 
 int main() {
-    int c, n, i, sum;
+    int c, n;
 
     scanf("%d", &c);
     
     while (c--) {
-        sum = 0;
         scanf("%d", &n);
 
-        for (i = 0; i < n; i++) {
-            if (i % 2 == 0) sum++;
-            else sum--;
-        }
-        
-        printf("%d\n", sum);
+        if (n % 2 == 0) puts("0");
+        else puts("1");
     }
     return 0;
 }
