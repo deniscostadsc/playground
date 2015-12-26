aa31a3a90e48fa1549ddc8c0e7a8f67326eedde6
Denis Costa <deniscostadsc@gmail.com>
2015-12-26 12:35:10 -0200

    Melhora solução do 001.

--- projecteuler/001/001.cpp
+++ projecteuler/001/001.cpp
@@ -2,14 +2,15 @@
 
 using namespace std;
 
+int sum_all_divisible_by_n(int n, int target) {
+    int p = target / n;
+    return n * (p * (p + 1)) / 2;
+}
+
 int main(){
-    int i, s = 0;
-
-    for (i = 0; i < 1000; ++i){
-        if (i % 3 == 0 || i % 5 == 0) s += i;
-    }
-
-    cout << s << endl;
+    cout << sum_all_divisible_by_n(3, 999) +
+        sum_all_divisible_by_n(5, 999) -
+        sum_all_divisible_by_n(15, 999) << endl;
 
     return 0;
 }
