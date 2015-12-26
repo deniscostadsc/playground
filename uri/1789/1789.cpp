7c11f19b0b7e6d5a995cddaff834240247401365
Denis Costa <deniscostadsc@gmail.com>
2015-12-25 23:21:28 -0200

    Resolve 1789.

---
+++ uri/1789/1789.cpp
@@ -0,0 +1,22 @@
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+    int l, v, fastest;
+
+    while (scanf("%d", &l) != EOF) {
+        fastest = 0;
+
+        while (l--) {
+            scanf("%d", &v);
+
+            if (v > fastest) fastest = v;
+        }
+
+        if (fastest < 10) puts("1");
+        else if (fastest < 20) puts("2");
+        else puts("3");
+    }
+    return 0;
+}
