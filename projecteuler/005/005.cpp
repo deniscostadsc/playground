f179dea63a05ac9cb0fea1b9084984eff5ad794e
Denis Costa <deniscostadsc@gmail.com>
2015-12-28 16:17:54 -0200

    Adiciona código pimpão e rápido.

---
+++ projecteuler/005/005.cpp
@@ -0,0 +1,34 @@
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+int gcd(int x, int y) {
+    if (y == 0) return x;
+    return gcd(y, x % y);
+}
+
+long lcm(int x, int y) {
+    return abs(x) / gcd(x, y) * abs(y);
+}
+
+long lcm_many(int numbers[], int n) {
+    int i;
+    long result = numbers[0];
+
+    for (i = 1; i < n; i++) {
+        result = lcm(result, numbers[i]);
+    }
+
+    return result;
+}
+
+
+int main() {
+    int numbers[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
+                     12, 13, 14, 15, 16, 17, 18, 19, 20};
+
+    printf("%ld\n", lcm_many(numbers, 20));
+
+    return 0;
+}
--- projecteuler/005/005.py
+++ projecteuler/005/005.py
@@ -1,11 +1,19 @@
-number = 1
-while True:
-    check = True
-    for i in xrange(1, 21):
-        if (number % i != 0):
-            check = False
-            break
-    if check:
-        print(number)
-        break
-    number += 1
+def gcd(x, y):
+    if y == 0:
+        return x
+    return gcd(y, x % y);
+
+
+def lcm(x, y):
+    return abs(x) / gcd(x, y) * abs(y)
+
+
+def lcm_many(numbers):
+    result = numbers[0]
+
+    for n in numbers:
+        result = lcm(result, n)
+    return result
+
+
+print(lcm_many(xrange(1, 21)))
