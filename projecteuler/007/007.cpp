2982fb795974d4534fc411bc7a16d073cece9d0b
Denis Costa <deniscostadsc@gmail.com>
2015-12-28 17:08:29 -0200

    Formata código.

--- projecteuler/007/007.cpp
+++ projecteuler/007/007.cpp
@@ -3,23 +3,24 @@
 
 using namespace std;
 
-bool is_prime(unsigned long long n){
+bool is_prime(unsigned long long n) {
     if (n == 2) return true;
     if (n % 2 == 0 || n < 2) return false;
 
-    for (unsigned long i = 3; i <= sqrt(n); i += 2){
+    for (unsigned long i = 3; i <= sqrt(n); i += 2) {
         if (n % i == 0) return false;
     }
+
     return true;
 }
 
 int main(){
     int c = 0;
     unsigned long long n = 1;
-    while(n++){
-        if (is_prime(n))
-            c++;
-        if (c == 10001){
+
+    while(n++) {
+        if (is_prime(n)) c++;
+        if (c == 10001) {
             cout << n << endl;
             break;
         }
