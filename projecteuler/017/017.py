9250a11ea1168e984449fa01f3c99380ae6b79f5
Denis Costa <deniscostadsc@gmail.com>
2015-12-29 18:34:47 -0200

    Resolve 017.

---
+++ projecteuler/017/017.py
@@ -0,0 +1,52 @@
+base_numbers = {
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+    7: 'seven',
+    8: 'eight',
+    9: 'nine',
+    10: 'ten',
+    11: 'eleven',
+    12: 'twelve',
+    13: 'thirteen',
+    14: 'fourteen',
+    15: 'fifteen',
+    16: 'sixteen',
+    17: 'seventeen',
+    18: 'eighteen',
+    19: 'nineteen',
+    20: 'twenty',
+    30: 'thirty',
+    40: 'forty',
+    50: 'fifty',
+    60: 'sixty',
+    70: 'seventy',
+    80: 'eighty',
+    90: 'ninety'
+}
+
+def number_in_words(number):
+    if number == 1000:
+        return 'one thousand'
+
+    written_number = []
+    while number:
+        if number >= 100:
+            written_number.append(base_numbers[number / 100])
+            written_number.append('hundred')
+            number -= number / 100 * 100
+            if number:
+                written_number.append('and')
+
+        for basic_number, basic_name in sorted(base_numbers.items(), reverse=True):
+            if basic_number <= number:
+                written_number.append(basic_name) 
+                number %= basic_number
+    return ' '.join(written_number)
+
+written_numbers_until_1000 = [number_in_words(i) for i in xrange(1001)]
+
+print(len(''.join(written_numbers_until_1000).replace(' ', '')))
---
+++ projecteuler/017/get_numbers.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+function get_numbers {
+    base_url='https://www.ego4u.com/en/cram-up/vocabulary/numbers/generator?param='
+    numbers=$1
+
+    number_in_words=$(curl -s ${base_url}${number} | \
+        grep '<h3>cardinal' | \
+        sed 's/^<h3>[^<]*<\/h3><ul><li>//g' | \
+        sed 's/<.*//g')
+    echo "${number}:${number_in_words}" >> numbers.txt
+}
+
+for number in $(seq 1 1000); do
+    get_numbers $number &
+
+    if [ $((number % 50)) == 0 ]; then
+        sleep 3
+    fi
+done
