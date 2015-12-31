f13b5a5fe54d1c92e7b5061aa477889b43163fbc
Denis Costa <deniscostadsc@gmail.com>
2015-12-31 14:14:18 -0200

    Adiciona o vimrc para poder editar código de dentro da VM.

--- provisioner.sh
+++ provisioner.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 apt-get update && sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y
-apt-get install -y nodejs g++
+apt-get install -y nodejs g++ git
+
+wget -q https://raw.githubusercontent.com/deniscostadsc/dotfiles/master/.vimrc
+chown vagrant:vagrant .vimrc
 
 cp /vagrant/.marathon.sh /home/vagrant/
 
