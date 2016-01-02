70289b7a57c90bba406ca4d643e5dc87aba2a4c9
Denis Costa <deniscostadsc@gmail.com>
2016-01-02 20:55:27 +0000

    Resolve 045.

---
+++ projecteuler/045/045.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+long triangle(long n) {
+    return n * (n + 1) / 2;
+}
+
+long pentagonal(long n) {
+    return n * (3 * n - 1) / 2;
+}
+
+long hexagonal(long n) {
+    return n * (2 * n - 1);
+}
+
+int main() {
+    bool got_pentagonal = false, got_hexagonal = false;
+    long tn , pn = 0, hn = 0, t = 285 + 1, p = 165, h = 143;
+
+    while (!got_pentagonal && !got_hexagonal) {
+
+        tn = triangle(t++);
+
+        while (pn < tn) pn = pentagonal(p++);
+
+        if (pn == tn) got_pentagonal = true;
+
+        while (got_pentagonal && hn < pn) hn = hexagonal(h++);
+
+        if (hn == pn) got_hexagonal = true;
+        else got_pentagonal = got_hexagonal = false;
+    }
+
+    cout << tn << endl;
+
+    return 0;
+}
