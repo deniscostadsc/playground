c20b3148a99753e5180260a2e134705bf20c75e8
Denis Costa <deniscostadsc@gmail.com>
2016-01-03 21:00:02 +0000

    Corrige lint.

--- projecteuler/001/001.js
+++ projecteuler/001/001.js
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
+'use strict';
 
 var s = 0;
 
-for (var i = 0; i < 1000; i++){
-    if (i % 3 === 0 || i % 5 === 0) s += i;
+for (var i = 0; i < 1000; i++) {
+  if (i % 3 === 0 || i % 5 === 0) {
+    s += i;
+  }
 }
 
 console.log(s);
