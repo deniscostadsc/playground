9b9709f19f8f2fcfca1c7637cba63ca31e04af9c
Denis Costa <deniscostadsc@gmail.com>
2016-01-03 20:31:37 +0000

    Corrige lint.

--- projecteuler/001/001.c
+++ projecteuler/001/001.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main(){
+int main() {
     int i, sum = 0;
     for (i = 0; i < 1000; i++)
         if (i % 3 == 0 || i % 5 == 0)
--- projecteuler/001/001.cpp
+++ projecteuler/001/001.cpp
@@ -7,7 +7,7 @@ int sum_all_divisible_by_n(int n, int target) {
     return n * (p * (p + 1)) / 2;
 }
 
-int main(){
+int main() {
     cout << sum_all_divisible_by_n(3, 999) +
         sum_all_divisible_by_n(5, 999) -
         sum_all_divisible_by_n(15, 999) << endl;
