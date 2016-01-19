409f8822c585ff807c3900cd5a4bdff1ab68ea84
Denis Costa <deniscostadsc@gmail.com>
2016-01-19 00:40:51 +0000

    Lint me.

--- uri/1165/1165.cpp
+++ uri/1165/1165.cpp
@@ -1,25 +1,30 @@
-#include <cstdio>
+#include <iostream>
 #include <cmath>
 
-bool is_prime(unsigned long n){
+bool is_prime(long n) {
     if (n == 2) return true;
     if (n % 2 == 0 || n < 2) return false;
 
-    for (unsigned long i = 3; i <= sqrt(n); i += 2){
+    for (long i = 3; i <= sqrt(n); i += 2) {
         if (n % i == 0) return false;
     }
     return true;
 }
 
-int main(){
-    int n, i;
-    scanf("%d", &i);
-    while(i--){
-        scanf("%d", &n);
-        if (is_prime(n))
-            printf("%d eh primo\n", n);
-        else
-            printf("%d nao eh primo\n", n);
+int main() {
+    long x;
+    int n;
+
+    std::cin >> n;
+
+    while (n--) {
+        std::cin >> x;
+
+        if (is_prime(x)) {
+            std::cout << x << " eh primo" << std::endl;
+        } else {
+            std::cout << x << " nao eh primo" << std::endl;
+        }
     }
 
     return 0;
