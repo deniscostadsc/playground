0a9639da6f272d970e4b0ecf3331e7cb350501b1
Denis Costa <deniscostadsc@gmail.com>
2016-04-10 16:03:44 +0000

    Solves GCJ probelm.

---
+++ google-code-jam/2016/qualification-round/b/b.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+int main() {
+    int t, i, count, limit;
+    unsigned int j;
+    bool minus_found;
+    std::string s;
+
+    std::cin >> t;
+
+    for (i = 1; i <= t; i++) {
+        std::cin >> s;
+        count = 0;
+
+        do {
+            limit = 0;
+            minus_found = false;
+
+            for (j = 0; j < s.length(); j++) {
+                if (s.at(j) == '-') {
+                    minus_found = true;
+                }
+                if (minus_found && s.at(j) == '+') {
+                    break;
+                }
+                limit++;
+            }
+
+            if (minus_found) {
+                count++;
+                std::replace(s.begin(), s.begin() + limit, '+', '*');
+                std::replace(s.begin(), s.begin() + limit, '-', '+');
+                std::replace(s.begin(), s.begin() + limit, '*', '-');
+            }
+        } while (minus_found);
+
+        std::cout << "Case #" << i << ": " << count <<std::endl;
+    }
+
+    return 0;
+}
