bb88c14ab77df8cbafc5ad4aa54fcaa0e1c9d0e3
Denis Costa <deniscostadsc@gmail.com>
2016-04-29 06:44:48 -0300

    Corrige resposta.

--- uri/1098/1098.cpp
+++ uri/1098/1098.cpp
@@ -7,18 +7,18 @@ int main() {
     for (i = 0; i <= 20; i += 2) {
         if (i == 0 || i == 10 || i == 20) {
             printf("I=%.0f J=%.0f\n",
-                static_cast<float>(i / 10), static_cast<float>(i / 10 + 1));
+                static_cast<float>(i) / 10, static_cast<float>(i) / 10 + 1);
             printf("I=%.0f J=%.0f\n",
-                static_cast<float>(i) / 10, static_cast<float>(i / 10 + 2));
+                static_cast<float>(i) / 10, static_cast<float>(i) / 10 + 2);
             printf("I=%.0f J=%.0f\n",
-                static_cast<float>(i) / 10, static_cast<float>(i / 10 + 3));
+                static_cast<float>(i) / 10, static_cast<float>(i) / 10 + 3);
         } else {
             printf("I=%.1f J=%.1f\n",
-                static_cast<float>(i) / 10, static_cast<float>(i / 10 + 1));
+                static_cast<float>(i) / 10, static_cast<float>(i) / 10 + 1);
             printf("I=%.1f J=%.1f\n",
-                static_cast<float>(i) / 10, static_cast<float>(i / 10 + 2));
+                static_cast<float>(i) / 10, static_cast<float>(i) / 10 + 2);
             printf("I=%.1f J=%.1f\n",
-                static_cast<float>(i) / 10, static_cast<float>(i / 10 + 3));
+                static_cast<float>(i) / 10, static_cast<float>(i) / 10 + 3);
         }
     }
 
