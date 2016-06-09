7eee13fc1fa76240c9912bd5cb8d5ca9a7f6e429
Denis Costa <deniscostadsc@gmail.com>
2016-06-09 07:01:24 -0300

    Improves readbility.

--- br.spoj/contrato/contrato.c
+++ br.spoj/contrato/contrato.c
@@ -5,15 +5,15 @@ int main() {
     while ((t = getchar()) != '0') {
         getchar();
         s = 0;
+
         while ((n = getchar()) != '\n') {
             if (n != t) {
-                s += n - '0';
-                if (s != 0)
-                    printf("%c", n);
+                s += n - '0'; // converts char to int and then sum to s
+                if (s != 0) printf("%c", n);
             }
         }
-        if (s == 0)
-            printf("%d", 0);
+
+        if (s == 0) printf("%d", 0);
         printf("\n");
     }
     return 0;
