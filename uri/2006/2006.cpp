423e6e2f701193c75fbb00db4c516cab9a34b18e
Denis Costa <deniscostadsc@gmail.com>
2016-06-14 20:43:54 -0300

    Solves 2006.

---
+++ uri/2006/2006.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    int t, x, i, c = 0;
+
+    std::cin >> t;
+
+    for (i = 0; i <= 4; i++) {
+        std::cin >> x;
+        if (x == t) c++;
+    }
+
+    std::cout << c << std::endl;
+    return 0;
+}
