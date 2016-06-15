bd654a3ed66b32c90ebaddb3e3e8f49290d52e90
Denis Costa <deniscostadsc@gmail.com>
2016-06-14 21:22:30 -0300

    Solves 1828.

---
+++ uri/1828/1828.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+
+int rock_paper_scissors_lizard_spock(std::string sheldons, std::string rajs) {
+    if (sheldons == rajs) return 0;
+    if ((sheldons == "tesoura" && (rajs == "papel" || rajs == "lagarto")) ||
+        (sheldons == "papel" && (rajs == "pedra" || rajs == "Spock")) ||
+        (sheldons == "pedra" && (rajs == "lagarto" || rajs == "tesoura")) ||
+        (sheldons == "lagarto" && (rajs == "Spock" || rajs == "papel")) ||
+        (sheldons == "Spock" && (rajs == "tesoura" || rajs == "pedra"))) {
+        return 1;
+    }
+    return -1;
+}
+
+
+int main() {
+    int t, i, result;
+    std::string sheldons, rajs;
+
+    std::cin >> t;
+    for (i = 1; i <= t; i++) {
+        std::cin >> sheldons >> rajs;
+
+        result = rock_paper_scissors_lizard_spock(sheldons, rajs);
+
+        std::cout << "Caso #" << i << ": ";
+        if (result == 1) {
+            std::cout << "Bazinga!";
+        } else if (result == 0) {
+            std::cout << "De novo!";
+        } else {
+            std::cout << "Raj trapaceou!";
+        }
+        std::cout << std::endl;
+    }
+    return 0;
+}
---
+++ uri/1828/generate_in.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+TEST_CASES=25
+
+echo $TEST_CASES
+
+for sheldons in pedra papel tesoura lagarto Spock; do
+    for rajs in pedra papel tesoura lagarto Spock; do
+        echo "$sheldons $rajs"
+    done
+done
