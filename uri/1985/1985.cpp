33b7651ae47cc6d683e60b0227564d27a96e5d03
Denis Costa <deniscostadsc@gmail.com>
2016-06-17 19:30:44 -0300

    Solves 1985.

---
+++ uri/1985/1985.cpp
@@ -0,0 +1,30 @@
+#include <iomanip>
+#include <iostream>
+#include <map>
+
+int main() {
+    std::map<int, float> menu;
+    menu[1001] = 1.5;
+    menu[1002] = 2.5;
+    menu[1003] = 3.5;
+    menu[1004] = 4.5;
+    menu[1005] = 5.5;
+
+    int p, q, id = 0;
+    float sum = 0.0;
+
+    std::cin >> p;
+
+    while (p--) {
+        std::cin >> id >> q;
+        sum += menu[id] * q;
+    }
+
+    std::cout <<
+        std::setprecision(2) <<
+        std::fixed <<
+        sum <<
+        std::endl;
+
+    return 0;
+}
