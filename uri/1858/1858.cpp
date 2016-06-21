5fa5db792ae8bc9c3b335535220c0c2e4a053e21
Denis Costa <deniscostadsc@gmail.com>
2016-06-21 19:34:41 -0300

    Solves 1858.

---
+++ uri/1858/1858.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+    int n, t, i, index, lowest = 21;
+
+    std::cin >> n;
+
+    for (i = 1; i <= n; i++) {
+        std::cin >> t;
+
+        if (t < lowest) {
+            lowest = t;
+            index = i;
+        }
+    }
+
+    std::cout << index << std::endl;
+
+    return 0;
+}
