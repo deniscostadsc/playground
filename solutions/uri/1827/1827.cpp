754e8294d241d163dbea8ce57f38c5427d725575
Denis Costa <deniscostadsc@gmail.com>
2016-06-25 11:43:53 -0300

    Solves 1827.

---
+++ solutions/uri/1827/1827.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main() {
+    int n, i, j;
+
+    while (std::cin >> n) {
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < n; j++) {
+                if (n % 2 == 1 && i == n / 2 && j == n / 2) {
+                    std::cout << 4;
+                } else if ((i >= n / 3 && i < n - (n / 3)) &&
+                    (j >= n / 3 && j < n - (n / 3))) {
+                    std::cout << 1;
+                } else if (i == j) {
+                    std::cout << 2;
+                } else if (i + j == n - 1) {
+                    std::cout << 3;
+                } else {
+                    std::cout << 0;
+                }
+            }
+            std::cout << std::endl;
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
