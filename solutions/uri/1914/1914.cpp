e358c02b039b98d8056bb2840d8a86662dcc1089
Denis Costa <deniscostadsc@gmail.com>
2016-06-25 00:27:48 -0300

    Solves 1914.

---
+++ solutions/uri/1914/1914.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    int qt;
+    long first_number, second_number;
+    bool is_even, first_won;
+    std::string first_player, first_choice, second_player, second_choice;
+
+    std::cin >> qt;
+
+    while (qt--) {
+        std::cin >> first_player
+            >> first_choice
+            >> second_player
+            >> second_choice;
+        std::cin >> first_number >> second_number;
+
+        is_even = (first_number + second_number) % 2 == 0;
+        first_won = ((is_even && first_choice == "PAR") ||
+            (!is_even && first_choice == "IMPAR"));
+
+        std::cout << (first_won ? first_player : second_player) << std::endl;
+    }
+    return 0;
+}
---
+++ solutions/uri/1914/generate_in.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+TEST_CASES=250
+
+echo $TEST_CASES
+
+for ((i = 0;i < $TEST_CASES; i++)); do
+    elegible_words=$(egrep "^[A-Za-z]{4,6}$" /usr/share/dict/words)
+    word_count=$(wc -w <<< $elegible_words)
+    first_name=$(cut -d ' ' -f $(($RANDOM % $word_count + 1)) <<< $elegible_words | tr [:upper:] [:lower:])
+    second_name=$(cut -d ' ' -f $(($RANDOM % $word_count + 1)) <<< $elegible_words | tr [:upper:] [:lower:])
+
+    if [ $(($RANDOM % 2)) == 0 ]; then
+        echo "$first_name PAR $second_name IMPAR"
+    else
+        echo "$first_name IMPAR $second_name PAR"
+    fi
+    echo "$RANDOM $RANDOM"
+done
