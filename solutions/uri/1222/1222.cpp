39b36b3c05a7662a2215fc57bce89070336bee18
Denis Costa <deniscostadsc@gmail.com>
2016-09-05 19:04:49 -0300

    Fixes problem 1222.

--- solutions/uri/1222/1222.cpp
+++ solutions/uri/1222/1222.cpp
@@ -2,25 +2,24 @@
 #include <string>
 
 int main() {
-    unsigned int n, l, c, lines, characters;
+    unsigned int c, characters;
+    int n, l, lines;
     std::string word;
 
     while (std::cin >> n >> l >> c) {
-        lines = 0;
+        lines = 1;
         characters = 0;
 
         while (n--) {
             std::cin >> word;
-            if (characters + word.length() < c) {
+            if (characters + word.length() <= c) {
                 characters += word.length() + 1;
-                // if (n == 1) lines++;
             } else {
                 characters = word.length() + 1;
                 lines++;
             }
         }
 
-        // std::cout << lines << " + + "< std::endl;
         if (lines % l != 0) {
             std::cout << lines / l + 1 << std::endl;
         } else {
