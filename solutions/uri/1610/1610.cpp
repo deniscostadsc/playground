a90e0e0a211d7f288f1b1453d637340abb91ef5b
Denis Costa <deniscostadsc@gmail.com>
2016-10-09 17:52:06 -0300

    Fixes 1610.

--- solutions/uri/1610/1610.cpp
+++ solutions/uri/1610/1610.cpp
@@ -1,31 +1,48 @@
+#include <cstring>
 #include <iostream>
 #include <map>
+#include <stack>
 #include <vector>
-#include <cstring>
+
+int visited[10001];
+int scc_count;  // scc = strongly connected components
 
 std::map<int, std::vector<int> > graph;
-int visited[10001];
+std::map<int, std::vector<int> > transposed_graph;
+std::stack<int> traverse_order;
 
-bool has_loop(int i) {
-    bool result;
+void order_filler_dfs(int i) {
     std::vector<int>::iterator it;
 
     visited[i] = 1;
-    result = false;
 
     for (it = graph[i].begin(); it != graph[i].end(); it++) {
-        if (visited[*it]) {
-            return true;
-        } else {
-            result ^= has_loop(*it);
+        if (!visited[*it]) {
+            order_filler_dfs(*it);
         }
     }
 
-    return result;
+    traverse_order.push(i);
 }
+
+void scc_counter_dfs(int i) {
+    std::vector<int>::iterator it;
+
+    visited[i] = 1;
+
+    for (it = transposed_graph[i].begin();
+        it != transposed_graph[i].end(); it++) {
+        if (!visited[*it]) {
+            scc_count++;
+            scc_counter_dfs(*it);
+        }
+    }
+}
+
 
 int main() {
     int t, n, m, a, b;
+    std::map<int, std::vector<int> >::iterator it;
 
     std::cin >> t;
 
@@ -35,17 +52,32 @@ int main() {
         while (m--) {
             std::cin >> a >> b;
             graph[a].push_back(b);
+            transposed_graph[b].push_back(a);
         }
 
         memset(visited, 0, sizeof(visited));
+        for (it = graph.begin(); it != graph.end(); it++) {
+            if (!visited[it->first]) order_filler_dfs(it->first);
+        }
 
-        if (has_loop(1)) {
+        memset(visited, 0, sizeof(visited));
+        scc_count = 0;
+        while (!traverse_order.empty()) {
+            if (!visited[traverse_order.top()]) {
+                scc_counter_dfs(traverse_order.top());
+            }
+            traverse_order.pop();
+        }
+
+        if (scc_count) {
             std::cout << "SIM" << std::endl;
         } else {
             std::cout << "NAO" << std::endl;
         }
 
         graph.clear();
+        transposed_graph.clear();
     }
+
     return 0;
 }
