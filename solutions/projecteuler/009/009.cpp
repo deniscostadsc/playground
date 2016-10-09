9881645a220da6dc0b754e7d00f1de887e4417ce
Denis Costa <deniscostadsc@gmail.com>
2016-10-09 10:52:22 -0300

    Improves code legibility.

--- solutions/projecteuler/009/009.cpp
+++ solutions/projecteuler/009/009.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 #include <cmath>
 
-
+int s = 1000;
 
 int main() {
     int a, b, c;
 
-    for (a = 1; a < 500; a++) {
-        for (b = a + 1; b < 500; b++) {
-            c = sqrt(a * a + b * b);
-            if (sqrt(a * a + b * b) == c && a + b + c == 1000) {
+    for (a = 1; a < (s - 3) / 3; a++) {
+        for (b = a + 1; b < (s - 1 - a) / 2; b++) {
+            c = s - a - b;
+            if (c == sqrt(a * a + b * b)) {
                 std::cout << a * b * c << std::endl;
                 break;
             }
