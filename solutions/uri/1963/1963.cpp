5a4568fe673b4adb2f01f68ace0f6323ee38c09c
Denis Costa <deniscostadsc@gmail.com>
2016-11-18 20:37:25 -0200

    Solves 1963.

---
+++ solutions/uri/1963/1963.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+
+int main() {
+    double price1, price2;
+
+    while (std::cin >> price1 >> price2) {
+        std::cout << std::fixed << std::setprecision(2) <<
+            100 * (price2 - price1) / price1 << "%" << std::endl;
+    }
+    return 0;
+}
