09a4a4d86e33209eda4243f46a4aef79b4fe505a
Denis Costa <deniscostadsc@gmail.com>
2017-02-26 19:15:07 -0300

    Ádds unsolved problem.

---
+++ solutions/uri/1929/1929.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+bool can_form_triangle(int a, int b, int c) {
+    if (a > b + c) return false;
+    if (b > a + c) return false;
+    if (c > a + b) return false;
+    return true;
+}
+
+int main() {
+    int a, b, c, d;
+    bool result;
+
+    while (std::cin >> a >> b >> c >> d) {
+        std::cout << can_form_triangle(a, b, c) << std::endl;
+        std::cout << can_form_triangle(a, b, d) << std::endl;
+        std::cout << can_form_triangle(a, b, c) << std::endl;
+        std::cout << can_form_triangle(a, b, c) << std::endl;
+
+        result = can_form_triangle(a, b, c);
+        result &= can_form_triangle(a, b, d);
+        result &= can_form_triangle(a, c, d);
+        result &= can_form_triangle(b, c, d);
+
+        std::cout << ((result) ? ("S") : ("N")) << std::endl;
+    }
+    return 0;
+}
