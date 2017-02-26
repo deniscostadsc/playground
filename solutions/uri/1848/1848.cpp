a11f25d5222b03d6af94b806b23acf5909e4c296
Denis Costa <deniscostadsc@gmail.com>
2017-02-26 16:55:34 -0300

    Solves 1848.

---
+++ solutions/uri/1848/1848.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <map>
+
+int main() {
+    int sum = 0;
+    std::string input;
+    std::map<std::string, int> blink2int;
+
+    blink2int["---"] = 0;
+    blink2int["--*"] = 1;
+    blink2int["-*-"] = 2;
+    blink2int["-**"] = 3;
+    blink2int["*--"] = 4;
+    blink2int["*-*"] = 5;
+    blink2int["**-"] = 6;
+    blink2int["***"] = 7;
+
+    while (std::cin >> input) {
+        if (input == "caw") {
+            std::cin.ignore(10, '\n');
+            std::cout << sum << std::endl;
+            sum = 0;
+        } else {
+            sum += blink2int[input];
+        }
+    }
+
+    return 0;
+}
