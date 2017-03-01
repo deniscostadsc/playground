8ae26a1200b3d840b398b792c45358434fb05442
Denis Costa <deniscostadsc@gmail.com>
2017-03-01 11:23:31 -0300

    Solves 1550.

--- solutions/uri/1550/1550.cpp
+++ solutions/uri/1550/1550.cpp
@@ -1,5 +1,6 @@
 #include <cmath>
 #include <iostream>
+#include <utility>
 #include <map>
 #include <queue>
 
@@ -9,9 +10,11 @@ int reverse(int number) {
 
     if (number < 10) return number;
 
-    while(static_cast<int>(number / std::pow(10, digits_count)) > 0) digits_count++;
+    while (static_cast<int>(number / std::pow(10, digits_count)) > 0) {
+        digits_count++;
+    }
 
-    while(digits_count--) {
+    while (digits_count--) {
         reversed += (number % 10) * std::pow(10, digits_count);
         number /= 10;
     }
@@ -19,35 +22,33 @@ int reverse(int number) {
 }
 
 void bfs(int number, int goal) {
-    int current, reversed, levels = 0;
-    std::queue<int> q;
+    int reversed;
+    std::pair<int, int> current;
+    std::queue<std::pair<int, int> > q;
     std::map<int, bool> visited;
 
-    q.push(number);
+    q.push(std::make_pair(number, 0));
     visited[number] = true;
 
-    while(!q.empty()) {
+    while (!q.empty()) {
         current = q.front();
-        std::cout << "i+ " << current << std::endl;
-        if (current == goal) {
-            std::cout << levels << std::endl;
+        if (current.first == goal) {
+            std::cout << current.second << std::endl;
             break;
         }
         q.pop();
 
-        if (!visited[current + 1]) {
-            visited[current + 1] = true;
-            q.push(current + 1);
+        if (!visited[current.first + 1]) {
+            visited[current.first + 1] = true;
+            q.push(std::make_pair(current.first + 1, current.second + 1));
         }
 
-         reversed = reverse(current);
+         reversed = reverse(current.first);
 
         if (!visited[reversed]) {
             visited[reversed] = true;
-            q.push(reversed);
+            q.push(std::make_pair(reversed, current.second + 1));
         }
-
-        levels++;
     }
 }
 
@@ -56,7 +57,7 @@ int main() {
 
     std::cin >> t;
 
-    while(t--) {
+    while (t--) {
         std::cin >> a >> b;
         bfs(a, b);
     }
