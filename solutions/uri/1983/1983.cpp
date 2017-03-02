4848ec563a60d38d8f40768e2e595e2ff3a097f7
Denis Costa <deniscostadsc@gmail.com>
2017-03-02 19:08:40 -0300

    Solves 1983.

---
+++ solutions/uri/1983/1983.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main() {
+    int n;
+    long m, bigest_m;
+    float grade, bigest_grade;
+
+    std::cin >> n;
+    std::cin >> bigest_m >> bigest_grade;
+    n--;
+
+    while (n--) {
+        std::cin >> m >> grade;
+
+        if (grade > bigest_grade) {
+            bigest_grade = grade;
+            bigest_m = m;
+        }
+    }
+
+    if (bigest_grade < 8.0) {
+        std::cout << "Minimum note not reached" << std::endl;
+    } else {
+        std::cout << bigest_m << std::endl;
+    }
+
+    return 0;
+}
