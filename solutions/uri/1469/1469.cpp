c74620fbabefafa0c21d425dcca6a9ccde7724d3
Denis Costa <deniscostadsc@gmail.com>
2017-03-12 20:07:56 -0300

    Adds some unfinished code.

---
+++ solutions/uri/1469/1469.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+struct person {
+    int age;
+};
+
+int main() {
+    int n, m, i, age, x, y;
+
+    std::cin >> n >> m >> i;
+
+    while (n--) {
+        std::cin >> age;
+    }
+
+    while (m--) {
+        std::cin >> x >> y;
+    }
+
+    while (i--) {
+    }
+    return 0;
+}
