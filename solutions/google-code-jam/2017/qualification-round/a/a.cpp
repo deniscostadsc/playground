2dc87af004e8c12626234f087926911d744309c7
Denis Costa <deniscostadsc@gmail.com>
2017-04-16 22:58:56 -0300

    Adds a bunch of GCJ things.

---
+++ solutions/google-code-jam/2017/qualification-round/a/a.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int t, flipper_capacity;
+    std::string pancakes;
+
+    std::cin >> t;
+
+    for (int i = 0; i < t; i++) {
+        std::cin >> pancakes >> flipper_capacity;
+    }
+
+    return 0;
+}
--- solutions/google-code-jam/practice-contests/b/b.cpp
+++ solutions/google-code-jam/practice-contests/b/b.cpp
@@ -1,5 +1,27 @@
 #include <iostream>
+#include <string>
+#include <cstring>
+
+int positions[4];
+
+void count_positions(std::string entrance2exit, std::string exit2entrance) {
+    // 0 down
+    // 1 right
+    // 2 up
+    // 3 left
+    int direction = 0;
+}
 
 int main() {
+    int t;
+    std::string entrance2exit, exit2entrance;
+
+    std::cin >> t;
+
+    for (int i = 0; i < t; i++) {
+        std::cin >> entrance2exit >> exit2entrance;
+        memset(positions, 0, sizeof(positions));
+    }
+
     return 0;
 }
