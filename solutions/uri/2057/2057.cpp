866d37b97d2dda215d156db950920d1697296dd5
Denis Costa <deniscostadsc@gmail.com>
2019-03-07 15:16:34 -0300

    Solve 2057

---
+++ solutions/uri/2057/2057.cpp
@@ -0,0 +1,15 @@
+#include  <iostream>
+
+int main() {
+    int s, t, f, result;
+
+    std::cin >> s >> t >> f;
+
+    result = (s + t + f) % 24;
+
+    if (result < 0) {
+        result += 24;
+    }
+
+    printf("%d\n", result);
+}
