f25da355d208e6c2c46e1be65c20a00f8f85a918
Denis Costa <deniscostadsc@gmail.com>
2019-06-08 11:19:54 -0300

    Add 2165

---
+++ solutions/uri/2165/2165.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main () {
+    char letter;
+    int count = 0;
+
+    while (std::cin.get(letter)) {
+        if (letter != '\n') {
+            count++;
+        } else {
+            if (count <= 140) {
+                std::cout << "TWEET" << std::endl;
+            } else {
+                std::cout << "MUTE" << std::endl;
+            }
+            count = 0;
+        }
+    }
+
+    return 0;
+}
