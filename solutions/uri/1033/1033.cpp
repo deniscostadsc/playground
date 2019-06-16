f51bd860d824e935eac2def58e6db525af1ac404
Denis Costa <deniscostadsc@gmail.com>
2019-06-16 19:21:56 -0300

    Remove test code

--- solutions/uri/1033/1033.cpp
+++ solutions/uri/1033/1033.cpp
@@ -2,36 +2,34 @@
 #include <cmath>
 #include <cstring>
 
-unsigned long long memo_fib[2000];
-unsigned long long memo_calls[2000];
+unsigned long long memo_fib[263];
+unsigned long long memo_calls[263];
 
 long long fib(long long n) {
-    return round((pow((1 + sqrt(5)) / 2, n)) / sqrt(5) + 1/2);
-}
-
-long long recursive_fib(long long n) {
-    if (memo_calls[n]) return memo_fib[n];
+    if (n == 0 || memo_calls[n]) {
+        return memo_fib[n];
+    }
 
     memo_fib[n] = fib(n - 1) + fib(n - 2);
     memo_calls[n] = memo_calls[n - 1] + memo_calls[n - 2] + 1;
+
     return memo_fib[n];
 }
 
 int main() {
+    unsigned long long n;
+    int b;
+
     memset(memo_calls, 0, sizeof(memo_calls));
+    memset(memo_fib, 0, sizeof(memo_fib));
 
     memo_fib[0] = 0;
     memo_fib[1] = 1;
     memo_calls[0] = 1;
     memo_calls[1] = 1;
 
-    for (int i = 0; i < 2000; i++) {
-        std::cout << fib(i) << std::endl;
-        if (fib(i) != recursive_fib(i)) {
-            std::cout << "fudeu " << std::endl;
-            break;
-        }
-
+    while (std::cin >> n >> b && n && b) {
+        std::cout << fib(n) << std::endl;
     }
 
     return 0;
