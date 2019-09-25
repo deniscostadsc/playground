97e9e237e72a05c5a8645ca762032d352b011ccc
Denis Costa <deniscostadsc@gmail.com>
2019-09-25 13:35:29 -0300

    Solve 2160

---
+++ solutions/uri/2160/2160.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string name;
+
+    while (std::getline(std::cin, name)) {
+        std::cout << (name.size() <= 80 ? "YES" : "NO") << std::endl;
+    }
+
+    return 0;
+}
