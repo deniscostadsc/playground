dfacef7429377148828076750641d66251280372
Denis Costa <deniscostadsc@gmail.com>
2019-09-28 17:51:38 -0300

    Solve 1805

---
+++ solutions/uri/1805/1805.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    unsigned long long first, last;
+
+    while (std::cin >> first >> last) {
+        std::cout << (last - first + 1) * (first + last) / 2 << std::endl;
+    }
+
+    return 0;
+}
