eeef2e9f066d39900cc94426b1ea699ef3c876c1
Denis Costa <deniscostadsc@gmail.com>
2019-10-14 23:50:19 -0300

    Solve 2003

---
+++ solutions/uri/2003/generate_in.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+HOUR_START=5
+HOUR_END=8
+MINUTE_START=0
+MINUTE_END=59
+
+for hour in $(seq $HOUR_START $HOUR_END); do
+    for minute in $(seq -f "%02g" $MINUTE_START $MINUTE_END); do
+        echo "$hour:$minute"
+    done
+done
+echo "9:00"
