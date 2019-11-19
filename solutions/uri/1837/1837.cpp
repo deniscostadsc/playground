1c345c9c7d5cdbe0b6b281777c32661a120f93d0
Denis Costa <deniscostadsc@gmail.com>
2019-11-19 10:22:38 -0300

    Solves 1837

--- solutions/uri/1837/1837.cpp
+++ solutions/uri/1837/1837.cpp
@@ -3,20 +3,51 @@
 
 int main() {
     int a, b, r, q;
+    bool q_is_zero, negative, positive;
 
     while (std::cin >> a >> b) {
         r = a % b;
         q = a / b;
+        q_is_zero = false;
+        negative = false;
+        positive = false;
 
-        if (r < 0) {
-            if (q > 0) {
+        while (r < 0) {
+            if (!q_is_zero && q > 0) {
                 q++;
             }
-            if (q < 0) {
+
+            if (!q_is_zero && q < 0) {
                 q--;
             }
+
+            if (q_is_zero || q == 0) {
+                q_is_zero = true;
+                if (!positive && !negative) {
+                    positive = true;
+                }
+                if (positive) {
+                    q++;
+                }
+                if (positive && q >= 1000) {
+                    q = 0;
+                    positive = false;
+                    negative = true;
+                }
+                if (negative) {
+                    q--;
+                }
+                if (negative && q <= -1000) {
+                    q = 0;
+                    positive = false;
+                    negative = false;
+                }
+            }
+
             r = a - (q * b);
         }
+
+        q_is_zero = false;
 
         std::cout << q << " " << r << std::endl;
     }
