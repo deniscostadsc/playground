36247dff27810ca59f5a6ac3d582dd3bf75bc1a6
Denis Costa <deniscostadsc@gmail.com>
2020-02-14 10:49:32 -0300

    Solve 2031

---
+++ solutions/uri/2031/2031.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main() {
+    int n;
+    std::string player1_sign, player2_sign;
+
+    std::cin >> n;
+
+    while (n--) {
+        std::cin >> player1_sign;
+        std::cin >> player2_sign;
+
+        if (player1_sign == "ataque" && player2_sign == "ataque") {
+            std::cout << "Aniquilacao mutua" << std::endl;
+        } else if (player1_sign == "papel" && player2_sign == "papel") {
+            std::cout << "Ambos venceram" << std::endl;
+        } else if (player1_sign == "pedra" && player2_sign == "pedra") {
+            std::cout << "Sem ganhador" << std::endl;
+        } else if (player1_sign == "ataque" || (player1_sign == "pedra" && player2_sign == "papel")) {
+            std::cout << "Jogador 1 venceu" << std::endl;
+        } else {
+            std::cout << "Jogador 2 venceu" << std::endl;
+        }
+    }
+
+    return 0;
+}
