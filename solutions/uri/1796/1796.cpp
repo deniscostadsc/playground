b9def06a85a74651a3b827b01a50ac8a3768e375
Denis Costa <deniscostadsc@gmail.com>
2020-02-18 18:51:28 -0300

    Solve 1796

---
+++ solutions/uri/1796/1796.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main() {
+    int q, v, not_ok_count, survey_count;
+
+    while(std::cin >> q) {
+        not_ok_count = 0;
+        survey_count = 0;
+
+        while (q--) {
+            std::cin >> v;
+            if (v) {
+                not_ok_count++;
+            }
+            survey_count++;
+        }
+
+        if ((not_ok_count < survey_count / 2 && survey_count % 2 == 0) ||
+            (not_ok_count < survey_count / 2 + 1 && survey_count % 2 == 1)) {
+            std::cout << "Y";
+        } else {
+            std::cout << "N";
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
