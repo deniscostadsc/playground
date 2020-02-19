4f59a6c5c2fcc6cfbb92115dafc3cf3e6ec01baa
Denis Costa <deniscostadsc@gmail.com>
2020-02-19 10:53:07 -0300

    Solve 2140

---
+++ solutions/uri/2140/2140.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstring>
+
+int main() {
+    int n, m;
+    int bills[] = {2, 5, 10, 20, 50, 100};
+    int possible_changes[201];
+
+    memset(possible_changes, 0, sizeof(possible_changes));
+
+    for (int i = 0; i <= 5; i++) {
+        for (int j = 0; j <= 5; j++) {
+            possible_changes[bills[i] + bills[j]] = bills[i] + bills[j];
+        }
+    }
+
+    while (std::cin >> n >> m && n && m) {
+        if (m - n <= 200 && possible_changes[m - n]) {
+            std::cout << "possible" << std::endl;
+        } else {
+            std::cout << "impossible" << std::endl;
+        }
+    }
+
+    return 0;
+}
