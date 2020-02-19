3f35c4f4952ce1e4819d6495499f0f07289b6ab0
Denis Costa <deniscostadsc@gmail.com>
2020-02-19 17:02:47 -0300

    Solve 2176

---
+++ solutions/uri/2176/2176.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+
+int main() {
+    std::string s;
+
+    while (std::cin >> s) {
+        std::cout <<
+            s <<
+            (count(s.begin(), s.end(), '1') % 2 == 0 ? 0 : 1) <<
+            std::endl;
+    }
+    return 0;
+}
