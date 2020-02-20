6c39138619664fbd083606d146b7c766a3c911ee
Denis Costa <deniscostadsc@gmail.com>
2020-02-20 18:20:49 -0300

    Solve 2162

---
+++ solutions/uri/2162/2162.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+int main() {
+    int n, h, previous_h, direction;
+    bool has_pattern;
+
+    while (std::cin >> n) {
+        std::cin >> previous_h;
+        std::cin >> h;
+
+        has_pattern = true;
+
+        if (previous_h > h) {
+            direction = -1;
+        } else if (previous_h < h) {
+            direction = 1;
+        } else {
+            has_pattern = false;
+        }
+
+        n -= 2;
+        while (n--) {
+            previous_h = h;
+            std::cin >> h;
+
+            if (previous_h > h && direction == 1) {
+                direction = -1;
+            } else if (previous_h < h && direction == -1) {
+                direction = 1;
+            } else {
+                has_pattern = false;
+            }
+        }
+
+        std::cout << (has_pattern ? 1 : 0) << std::endl;
+    }
+
+    return 0;
+}
