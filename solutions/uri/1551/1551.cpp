ce7ffce4e30f619714e76a197df2a9fb0eba50cd
Denis Costa <deniscostadsc@gmail.com>
2020-02-20 10:14:16 -0300

    Solve 1551

---
+++ solutions/uri/1551/1551.cpp
@@ -0,0 +1,41 @@
+#include <cstdio>
+#include <iostream>
+#include <map>
+
+int main() {
+    int n, completeness;
+    std::string phrase, alphabet = "abcdefghijklmnopqrstuvwxyz";
+    std::map<char, int> letter_count;
+
+    std::cin >> n;
+    getchar();
+
+    while (n--) {
+        for (int i = 0; i < (int)alphabet.length(); i++) {
+            letter_count[alphabet[i]] = 0;
+        }
+
+        std::getline(std::cin, phrase);
+
+        for (int j = 0; j < (int)phrase.length(); j++) {
+            letter_count[phrase[j]]++;
+        }
+
+        completeness = 0;
+        for (int z = 0; z < (int)alphabet.length(); z++) {
+            if (letter_count[alphabet[z]] != 0) {
+                completeness++;
+            }
+        }
+
+        if (completeness == 26) {
+            std::cout << "frase completa" << std::endl;
+        } else if (completeness >= 13) {
+            std::cout << "frase quase completa" << std::endl;
+        } else {
+            std::cout << "frase mal elaborada" << std::endl;
+        }
+    }
+
+    return 0;
+}
