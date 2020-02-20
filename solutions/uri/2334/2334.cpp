acbdb0dd67b5aed098d67275d641d8683c464b0f
Denis Costa <deniscostadsc@gmail.com>
2020-02-20 14:39:59 -0300

    Solve 2334

---
+++ solutions/uri/2334/2334.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    unsigned long long p;
+
+    while (std::cin >> p && p != -1) {
+        if (p == 0) {
+            std::cout << 0 << std::endl;
+        } else {
+            std::cout << p - 1 << std::endl;
+        }
+    }
+
+    return 0;
+}
