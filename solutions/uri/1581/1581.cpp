e69f84e35749465b4b05e4f88d7535c704cca9bf
Denis Costa <deniscostadsc@gmail.com>
2020-02-20 18:57:34 -0300

    Solve 1581

---
+++ solutions/uri/1581/1581.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int main() {
+    int n, k;
+    std::string language, previous_language;
+    bool all_languages_equal;
+
+    std::cin >> n;
+
+    while (n--) {
+        std::cin >> k;
+        std::cin >> previous_language;
+
+        all_languages_equal = true;
+
+        k--;
+        while (k--) {
+            std::cin >> language;
+
+            if (previous_language != language) {
+                all_languages_equal = false;
+            }
+        }
+
+        std::cout << ( all_languages_equal ? language : "ingles") << std::endl;
+    }
+
+    return 0;
+}
