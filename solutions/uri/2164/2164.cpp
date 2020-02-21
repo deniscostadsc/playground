f8d3adbc6e3931f7de7085e30691e82f66fbe41f
Denis Costa <deniscostadsc@gmail.com>
2020-02-21 15:00:10 -0300

    Solve 2164

--- solutions/uri/2164/2164.cpp
+++ solutions/uri/2164/2164.cpp
@@ -1,15 +1,17 @@
-#include <iostream>
 #include <cmath>
+#include <iomanip>
+#include <iostream>
 
-unsigned long long fib(unsigned long long n) {
+double fib(double n) {
     return (pow((1 + sqrt(5)) / 2, n) - pow((1 - sqrt(5)) / 2, n)) / sqrt(5.0);
 }
 
 int main() {
-    unsigned long long n;
+    double n;
 
     while (std::cin >> n) {
-        std::cout << fib(n) << ".0" << std::endl;
+        std::cout << std::fixed << std::setprecision(1);
+        std::cout << fib(n) << std::endl;
     }
 
     return 0;
