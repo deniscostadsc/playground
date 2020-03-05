24b0033dc03f5c43804accbffaeb4482d986ba80
Denis Costa <deniscostadsc@gmail.com>
2020-03-05 08:28:49 -0300

    Solve 1943

---
+++ solutions/uri/1943/1943.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main() {
+    int k;
+
+    while (std::cin >> k) {
+        std::cout << "Top ";
+        if (k == 1) {
+            std::cout << 1;
+        } else if (k <= 3) {
+            std::cout << 3;
+        } else if (k <= 5) {
+            std::cout << 5;
+        } else if (k <= 10) {
+            std::cout << 10;
+        } else if (k <= 25) {
+            std::cout << 25;
+        } else if (k <= 50) {
+            std::cout << 50;
+        } else {
+            std::cout << 100;
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
