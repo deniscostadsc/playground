0ff143ab7760bfbeb7fbd20d65ec9a0663e0538b
Denis Costa <deniscostadsc@gmail.com>
2020-03-09 16:37:30 -0300

    Solve 2137

---
+++ solutions/uri/2137/2137.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main() {
+    int n;
+    std::string code;
+    std::vector<std::string> codes;
+    std::vector<std::string>::iterator it;
+
+    while (std::cin >> n) {
+        while (n--) {
+            std::cin >> code;
+            codes.push_back(code);
+        }
+        std::sort(codes.begin(), codes.end());
+
+        for (it = codes.begin(); it != codes.end(); it++) {
+            std::cout << *it << std::endl;
+        }
+        codes.clear();
+    }
+
+    return 0;
+}
