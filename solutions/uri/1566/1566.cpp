eb73ef26b40f67dae8e0e1e9059d04cd13b8b03c
Denis Costa <deniscostadsc@gmail.com>
2020-03-09 18:06:21 -0300

    Solve 1566

---
+++ solutions/uri/1566/1566.cpp
@@ -0,0 +1,34 @@
+#include <algorithm>
+#include <cstdio>
+#include <vector>
+
+int main() {
+    int nc, n, h;
+    std::vector<int> heights;
+    std::vector<int>::iterator it;
+
+    scanf("%d", &nc);
+
+    while (nc--) {
+        scanf("%d", &n);
+
+        while (n--) {
+            scanf("%d", &h);
+            heights.push_back(h);
+        }
+
+        std::sort(heights.begin(), heights.end());
+
+        it = heights.begin();
+        printf("%d", *it);
+        it++;
+        for (; it != heights.end(); it++) {
+            printf(" %d", *it);
+        }
+        printf("\n");
+
+        heights.clear();
+    }
+
+    return 0;
+}
