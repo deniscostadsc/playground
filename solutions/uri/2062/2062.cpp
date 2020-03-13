d080eb39a95a3fbedb0e00218c01ff25ab92914c
Denis Costa <deniscostadsc@gmail.com>
2020-03-13 14:58:00 -0300

    Solve 2062

---
+++ solutions/uri/2062/2062.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    int n;
+    std::string word;
+
+    while (std::cin >> n) {
+        for (int i = 0; i < n; i++) {
+            if (i > 0) {
+                std::cout << " ";
+            }
+
+            std::cin >> word;
+
+            if (word.size() == 3) {
+                if (
+                        word[0] == 'O' &&
+                        word[1] == 'B') {
+                    std::cout << "OBI";
+                } else if (
+                        word[0] == 'U' &&
+                        word[1] == 'R') {
+                    std::cout << "URI";
+                } else {
+                    std::cout << word;
+                }
+            } else {
+                std::cout << word;
+            }
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
