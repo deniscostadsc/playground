4bbde92c03dd8a9df037361fdb3d03488b6170ea
Denis Costa <deniscostadsc@gmail.com>
2020-03-27 15:29:30 -0300

    Solve 2581

---
+++ solutions/uri/2581/2581.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+#include <iostream>
+
+int main() {
+    int n;
+
+    std::cin >> n;
+
+    while (n--) {
+        std::cout << "I am Toorg!" << std::endl;
+    }
+
+    return 0;
+}
