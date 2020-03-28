794eb155fb8f1a2bbc5137b5299fa06305bc538e
Denis Costa <deniscostadsc@gmail.com>
2020-03-28 20:19:44 -0300

    Add unsolved 2161

---
+++ solutions/uri/2161/2161.cpp
@@ -0,0 +1,17 @@
+#include <iomanip>
+#include <iostream>
+#include <cmath>
+
+int main() {
+    double n;
+    double result = 6.0;
+
+    while (std::cin >> n) {
+        while (n--) {
+            result = 6.0 + 1.0 / result;
+        }
+        std::cout << result << std::endl;
+    }
+
+    return 0;
+}
