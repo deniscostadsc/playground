235a94965f896c55d122c6c8960ce5e9c4d9c29e
Denis Costa <deniscostadsc@gmail.com>
2020-03-28 17:37:29 -0300

    Solve 2253

---
+++ solutions/uri/2253/2253.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+bool is_password_valid(std::string password) {
+
+    if (!(password.size() >= 6 && password.size() <= 32)) {
+        return false;
+    }
+
+    std::string uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    std::string lowercase_letters = "abcdefghijklmnopqrstuvwxyz";
+    std::string numbers = "0123456789";
+    std::size_t found;
+    bool
+        has_uppercase = false,
+        has_lowercase = false,
+        has_number = false,
+        is_valid_char;
+
+    for (int i = 0; i < password.size(); i++) {
+        is_valid_char = false;
+
+        found = uppercase_letters.find(password[i]);
+        if (found != std::string::npos) {
+            has_uppercase = true;
+            is_valid_char = true;
+        }
+
+        found = lowercase_letters.find(password[i]);
+        if (found != std::string::npos) {
+            has_lowercase = true;
+            is_valid_char = true;
+        }
+
+        found = numbers.find(password[i]);
+        if (found != std::string::npos) {
+            has_number = true;
+            is_valid_char = true;
+        }
+
+        if (!is_valid_char) {
+            return false;
+        }
+    }
+
+    return has_uppercase && has_lowercase;
+}
+
+int main() {
+    std::string password;
+
+    while (std::getline(std::cin, password)) {
+        std::cout <<
+            (
+                is_password_valid(password) ?
+                "Senha valida." :
+                "Senha invalida."
+            ) <<
+            std::endl;
+    }
+
+    return 0;
+}
