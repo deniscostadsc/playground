a10ea45eb25a403242682b0d19a6c01b298b4613
Denis Costa <deniscostadsc@gmail.com>
2021-02-18 17:37:38 -0300

    Solve 2748

---
+++ solutions/uri/2748/2748.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    std::cout << "---------------------------------------" << std::endl;
+    std::cout << "|        Roberto                      |" << std::endl;
+    std::cout << "|                                     |" << std::endl;
+    std::cout << "|        5786                         |" << std::endl;
+    std::cout << "|                                     |" << std::endl;
+    std::cout << "|        UNIFEI                       |" << std::endl;
+    std::cout << "---------------------------------------" << std::endl;
+
+    return 0;
+}
