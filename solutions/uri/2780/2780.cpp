901a07a0c3d6fbff118c268404288d261dc9d350
Denis Costa <deniscostadsc@gmail.com>
2021-02-19 13:02:08 -0300

    Solve 2780

---
+++ solutions/uri/2780/2780.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+    int d;
+
+    while (std::cin >> d) {
+        if (d <= 800) {
+            std::cout << "1";
+        } else if (d <= 1400) {
+            std::cout << "2";
+        } else {
+            std::cout << "3";
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
