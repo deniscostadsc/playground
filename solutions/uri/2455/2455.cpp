2029212cceb73a1238eb437eef0768961c0ad6ce
Denis Costa <deniscostadsc@gmail.com>
2021-02-19 12:26:43 -0300

    Solve 2455

---
+++ solutions/uri/2455/2455.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+    int c1, c2, p1, p2;
+
+    while (std::cin >> c1 >> p1 >> c2 >> p2) {
+        if (c1 * p1 == c2 * p2) {
+            std::cout << "0";
+        } else if (c1 * p1 < c2 * p2) {
+            std::cout << "1";
+        } else {
+            std::cout << "-1";
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
