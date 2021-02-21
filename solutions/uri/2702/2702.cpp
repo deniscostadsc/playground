e82342df05221c2174c16fd9ad0a49f60ec0feef
Denis Costa <deniscostadsc@gmail.com>
2021-02-21 16:26:42 -0300

    Solve 2702

---
+++ solutions/uri/2702/2702.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+    int
+        chicken_meals, beef_meals, pasta_meals,
+        chicken_orders, beef_orders, pasta_orders,
+        result;
+
+    while (
+            std::cin >> chicken_meals >> beef_meals >> pasta_meals >>
+            chicken_orders >> beef_orders >> pasta_orders) {
+        result = (chicken_orders - chicken_meals >= 0) ? chicken_orders - chicken_meals: 0;
+        result += (beef_orders - beef_meals >= 0) ? beef_orders - beef_meals: 0;
+        result += (pasta_orders - pasta_meals >= 0) ? pasta_orders - pasta_meals: 0;
+
+        std::cout << result << std::endl;
+    }
+
+    return 0;
+}
