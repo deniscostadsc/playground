eb7298e828340bb84cf3fb78068b3fd5447cc6b6
Denis Costa <deniscostadsc@gmail.com>
2021-02-21 18:19:03 -0300

    Solve 2756

---
+++ solutions/uri/2756/2756.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    std::cout << "       A"<< std::endl;
+    std::cout << "      B B"<< std::endl;
+    std::cout << "     C   C"<< std::endl;
+    std::cout << "    D     D"<< std::endl;
+    std::cout << "   E       E"<< std::endl;
+    std::cout << "    D     D"<< std::endl;
+    std::cout << "     C   C"<< std::endl;
+    std::cout << "      B B"<< std::endl;
+    std::cout << "       A"<< std::endl;
+
+    return 0;
+}
