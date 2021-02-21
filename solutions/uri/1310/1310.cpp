0e04d4f7b5c6d9ec6a312df2329678495a174969
Denis Costa <deniscostadsc@gmail.com>
2021-02-21 19:15:34 -0300

    Solve 1310

---
+++ solutions/uri/1310/1310.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+
+int main() {
+    int n, per_day_cost, revenue, largest, largest_start;
+
+    while (std::cin >> n) {
+        std::cin >> per_day_cost;
+
+        largest = -1001;
+        largest_start = 0;
+
+        for (int i = 0; i < n; i++) {
+            std::cin >> revenue;
+
+            largest_start +=  revenue - per_day_cost;
+
+            if (largest < largest_start) {
+                largest = largest_start;
+            }
+
+            if (largest_start < 0) {
+                largest_start = 0;
+            }
+        }
+
+        std::cout << (largest >= 0? largest: 0) << std::endl;
+    }
+
+    return 0;
+}
