43f77f43b9ad5d9d43b6b11c57cf42e589423fef
Denis Costa <deniscostadsc@gmail.com>
2021-02-22 17:54:54 -0300

    Solve Output 3

---
+++ solutions/uri/2749/2749.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    std::cout << "---------------------------------------" << std::endl;
+    std::cout << "|x = 35                               |" << std::endl;
+    std::cout << "|                                     |" << std::endl;
+    std::cout << "|               x = 35                |" << std::endl;
+    std::cout << "|                                     |" << std::endl;
+    std::cout << "|                               x = 35|" << std::endl;
+    std::cout << "---------------------------------------" << std::endl;
+
+    return 0;
+}
