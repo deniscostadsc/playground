06339f2636cdc7bf1d486b819d78571afced4c53
Denis Costa <deniscostadsc@gmail.com>
2021-04-20 15:49:26 -0300

    Solve Area in python

--- solutions/uri/1012/1012.cpp
+++ solutions/uri/1012/1012.cpp
@@ -4,10 +4,12 @@
 
 int main() {
     float a, b, c;
-    scanf("%f %f %f", &a, &b, &c);
-    printf("TRIANGULO: %.3f\n", a * c / 2);
-    printf("CIRCULO: %.3f\n", pi * (c * c));
-    printf("TRAPEZIO: %.3f\n", c * ((a + b) / 2));
-    printf("QUADRADO: %.3f\n", b * b);
-    printf("RETANGULO: %.3f\n", a * b);
+
+    while (scanf("%f %f %f", &a, &b, &c) != EOF) {
+        printf("TRIANGULO: %.3f\n", a * c / 2);
+        printf("CIRCULO: %.3f\n", pi * (c * c));
+        printf("TRAPEZIO: %.3f\n", c * ((a + b) / 2));
+        printf("QUADRADO: %.3f\n", b * b);
+        printf("RETANGULO: %.3f\n", a * b);
+    }
 }
---
+++ solutions/uri/1012/1012.py
@@ -0,0 +1,12 @@
+import sys
+
+PI = 3.14159
+
+for line in sys.stdin:
+    a, b, c = map(float, line.split())
+
+    print(f"TRIANGULO: {a * c / 2:.3f}")
+    print(f"CIRCULO: {PI * (c * c):.3f}")
+    print(f"TRAPEZIO: {c * ((a + b) / 2):.3f}")
+    print(f"QUADRADO: {b * b:.3f}")
+    print(f"RETANGULO: {a * b:.3f}")
