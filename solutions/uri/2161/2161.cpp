7ac69ab71496a7d17a36e93697cf9c6922dd8eaf
Denis Costa <deniscostadsc@gmail.com>
2021-04-20 16:16:29 -0300

    Solve Square Root of 10

--- solutions/uri/2161/2161.cpp
+++ solutions/uri/2161/2161.cpp
@@ -1,16 +1,22 @@
-#include <cmath>
 #include <iomanip>
 #include <iostream>
 
+double sqrtt(int n) {
+    double result = 1 / 6;
+
+    for (int i = 0; i < n; i++) {
+        result = 1 / (6 + result);
+    }
+
+    return result + 3;
+}
+
 int main() {
-    double n;
-    double result = 6.0;
+    int n;
 
     while (std::cin >> n) {
-        while (n--) {
-            result = 6.0 + 1.0 / result;
-        }
-        std::cout << result << std::endl;
+        std::cout << std::fixed << std::setprecision(10) << sqrtt(n)
+                  << std::endl;
     }
 
     return 0;
