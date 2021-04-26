c9f7f61fddf4174d51ecb7c07768b69ef1956044
Denis Costa <deniscostadsc@gmail.com>
2021-04-26 17:31:13 -0300

    Solve CPF Input and Output

---
+++ solutions/uri/2763/2763.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+
+int main() {
+    int x, y, z, d;
+
+    while (scanf("%d.%d.%d-%d", &x, &y, &z, &d) != EOF) {
+        printf("%03d\n", x);
+        printf("%03d\n", y);
+        printf("%03d\n", z);
+        printf("%02d\n", d);
+    }
+    return 0;
+}
