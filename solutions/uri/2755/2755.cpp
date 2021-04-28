553298fe3320451f07b5af3d1e5d8e206401853a
Denis Costa <deniscostadsc@gmail.com>
2021-04-28 19:27:51 -0300

    Solve Output 9

---
+++ solutions/uri/2755/2755.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    std::cout << "\"Ro'b'er\tto\\/\"" << std::endl;
+    std::cout << "(._.) ( l: ) ( .-. ) ( :l ) (._.)" << std::endl;
+    std::cout << "(^_-) (-_-) (-_^)" << std::endl;
+    std::cout << "(\"_\") ('.')" << std::endl;
+
+    return 0;
+}
