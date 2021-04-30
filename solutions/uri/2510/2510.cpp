badea91b2909126fbddd277176881f2923923d30
Denis Costa <deniscostadsc@gmail.com>
2021-04-30 16:26:48 -0300

    Solve Batmain

---
+++ solutions/uri/2510/2510.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int n;
+    std::string name;
+
+    std::cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        std::cout << "Y" << std::endl;
+    }
+
+    return 0;
+}
