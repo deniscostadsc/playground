356fa283a4f97caeb1030feec49c765b7122a3e3
Denis Costa <deniscostadsc@gmail.com>
2021-05-15 11:42:41 -0300

    Solve Output 4

---
+++ solutions/uri/2750/2750.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main () {
+    std::cout << "---------------------------------------" << std::endl;
+    std::cout << "|  decimal  |  octal  |  Hexadecimal  |" << std::endl;
+    std::cout << "---------------------------------------" << std::endl;
+    std::cout << "|      0    |    0    |       0       |" << std::endl;
+    std::cout << "|      1    |    1    |       1       |" << std::endl;
+    std::cout << "|      2    |    2    |       2       |" << std::endl;
+    std::cout << "|      3    |    3    |       3       |" << std::endl;
+    std::cout << "|      4    |    4    |       4       |" << std::endl;
+    std::cout << "|      5    |    5    |       5       |" << std::endl;
+    std::cout << "|      6    |    6    |       6       |" << std::endl;
+    std::cout << "|      7    |    7    |       7       |" << std::endl;
+    std::cout << "|      8    |   10    |       8       |" << std::endl;
+    std::cout << "|      9    |   11    |       9       |" << std::endl;
+    std::cout << "|     10    |   12    |       A       |" << std::endl;
+    std::cout << "|     11    |   13    |       B       |" << std::endl;
+    std::cout << "|     12    |   14    |       C       |" << std::endl;
+    std::cout << "|     13    |   15    |       D       |" << std::endl;
+    std::cout << "|     14    |   16    |       E       |" << std::endl;
+    std::cout << "|     15    |   17    |       F       |" << std::endl;
+    std::cout << "---------------------------------------" << std::endl;
+
+    return 0;
+}
