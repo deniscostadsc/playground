b182f6a3b3d389eb0ec7deef17e4839c3c455e79
Denis Costa <deniscostadsc@gmail.com>
2021-05-15 11:03:54 -0300

    Format code

--- solutions/uri/1015/1015.cpp
+++ solutions/uri/1015/1015.cpp
@@ -3,7 +3,9 @@
 
 int main() {
     float x1, y1, x2, y2;
+
     scanf("%f %f", &x1, &y1);
     scanf("%f %f", &x2, &y2);
+
     printf("%.4f\n", std::sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)));
 }
--- solutions/uri/1016/1016.cpp
+++ solutions/uri/1016/1016.cpp
@@ -3,6 +3,8 @@
 
 int main() {
     int n;
+
     scanf("%d", &n);
+
     printf("%d minutos\n", 150 / 60 * n);
 }
