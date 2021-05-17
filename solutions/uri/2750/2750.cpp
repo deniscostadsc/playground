aede5d26e61d961ad4c361ac576ff4d92ce43184
Denis Costa <deniscostadsc@gmail.com>
2021-05-17 18:12:39 -0300

    Fix lint

--- solutions/uri/2750/2750.cpp
+++ solutions/uri/2750/2750.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-int main () {
+int main() {
     std::cout << "---------------------------------------" << std::endl;
     std::cout << "|  decimal  |  octal  |  Hexadecimal  |" << std::endl;
     std::cout << "---------------------------------------" << std::endl;
