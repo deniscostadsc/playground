35c6e0b5c953b5ab75ca9528b194d7f43a60c557
Denis Costa <deniscostadsc@gmail.com>
2021-05-18 11:22:50 -0300

    Improve input file and change solution to support it

--- solutions/uri/1019/1019.cpp
+++ solutions/uri/1019/1019.cpp
@@ -1,17 +1,21 @@
 #include <cstdio>
 
 int main() {
-    int t, h = 0, m = 0, s = 0;
-    scanf("%d", &t);
-    if (t >= 60 * 60) {
-        h = t / (60 * 60);
-        t %= (60 * 60);
+    int t, h = 0, m = 0;
+
+    while(scanf("%d", &t) != EOF) {
+        if (t >= 60 * 60) {
+            h = t / (60 * 60);
+            t %= (60 * 60);
+        }
+        if (t >= 60) {
+            m = t / 60;
+            t %= 60;
+        }
+
+        printf("%d:%d:%d\n", h, m, t);
+        h = 0, m = 0;
     }
-    if (t >= 60) {
-        m = t / 60;
-        t %= 60;
-    }
-    s = t;
-    printf("%d:%d:%d\n", h, m, s);
+
     return 0;
 }
