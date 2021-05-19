fe9758c21af1870cd8c387aaf7f706bccf4be3f0
Denis Costa <deniscostadsc@gmail.com>
2021-05-19 20:14:45 -0300

    Solve Age in Days in python

---
+++ solutions/uri/1020/1020.py
@@ -0,0 +1,19 @@
+import sys
+
+
+for t in sys.stdin:
+    t = int(t)
+    a = 0
+    m = 0
+
+    if t >= 365:
+        a = t // 365
+        t %= 365
+
+    if t >= 30:
+        m = t // 30
+        t %= 30
+
+    print(f"{a} ano(s)")
+    print(f"{m} mes(es)")
+    print(f"{t} dia(s)")
