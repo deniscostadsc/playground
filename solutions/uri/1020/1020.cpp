7651bfdf502c040b178ec9b74cbe72819805ef1c
Denis Costa <deniscostadsc@gmail.com>
2021-05-19 20:13:32 -0300

    Add more outputs

--- solutions/uri/1020/1020.cpp
+++ solutions/uri/1020/1020.cpp
@@ -2,18 +2,23 @@
 
 int main() {
     int t, a = 0, m = 0, d = 0;
-    scanf("%d", &t);
-    if (t >= 365) {
-        a = t / 365;
-        t %= 365;
+
+    while(scanf("%d", &t) != EOF) {
+        if (t >= 365) {
+            a = t / 365;
+            t %= 365;
+        }
+        if (t >= 30) {
+            m = t / 30;
+            t %= 30;
+        }
+
+        d = t;
+
+        printf("%d ano(s)\n", a);
+        printf("%d mes(es)\n", m);
+        printf("%d dia(s)\n", d);
     }
-    if (t >= 30) {
-        m = t / 30;
-        t %= 30;
-    }
-    d = t;
-    printf("%d ano(s)\n", a);
-    printf("%d mes(es)\n", m);
-    printf("%d dia(s)\n", d);
+
     return 0;
 }
