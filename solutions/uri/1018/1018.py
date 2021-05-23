f5670cd749b2c0f3691cdeac2909a175bdb464b3
Denis Costa <deniscostadsc@gmail.com>
2021-05-22 21:38:33 -0300

    Solve Banknotes in python

---
+++ solutions/uri/1018/1018.py
@@ -0,0 +1,11 @@
+bills = [100, 50, 20, 10, 5, 2, 1]
+
+n = int(input())
+
+print(n)
+
+for bill in bills:
+    print(f"{n // bill} nota(s) de R$ {bill},00")
+
+    if n >= bill:
+        n -= bill * (n // bill)
