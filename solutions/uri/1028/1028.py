707897dce2221a41292353eb127cc4d6b05bec4f
Denis Costa <deniscostadsc@gmail.com>
2021-05-26 15:40:51 -0300

    Solve Collectable Cards in python

---
+++ solutions/uri/1028/1028.py
@@ -0,0 +1,13 @@
+import sys
+
+def gcd(a, b):
+    while b > 0:
+        a, b = b, a % b
+
+    return a
+
+n = int(input())
+
+for line in range(n):
+    a, b = map(int, input().split())
+    print(gcd(a, b))
