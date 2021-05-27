8a0ee09eec5f24152babfff3882a38ca749b3cc9
Denis Costa <deniscostadsc@gmail.com>
2021-05-27 12:47:35 -0300

    Solve Fibonacci, How Many Calls? in python

---
+++ solutions/uri/1029/1029.py
@@ -0,0 +1,28 @@
+from functools import lru_cache
+
+fibs = [0] * 40
+calls = [0] * 40
+
+
+def fibonacci(n):
+    global fibs, calls
+
+    if n <= 1:
+        fibs[n] = n
+        calls[n] = 0
+    else:
+        if fibs[n] != 0:
+            return fibs[n]
+        fibs[n] = fibonacci(n - 1) + fibonacci(n - 2)
+        calls[n] = calls[n - 1] + calls[n - 2] + 2
+
+    return fibs[n]
+
+
+i = int(input())
+
+for _ in range(i):
+    n = int(input())
+    fibonacci_result = fibonacci(n)
+    calls_count = calls[n]
+    print(f"fib({n}) = {calls_count} calls = {fibonacci_result}")
