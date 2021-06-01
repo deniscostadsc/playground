318b33c29f1ede36f317097b396d29d8e4c1ecc4
Denis Costa <deniscostadsc@gmail.com>
2021-06-01 20:06:53 -0300

    Format code

--- solutions/uri/1010/1010.cpp
+++ solutions/uri/1010/1010.cpp
@@ -4,8 +4,10 @@
 int main() {
     int a, b;
     float c, s = 0.0;
+
     while (scanf("%d %d %f", &a, &b, &c) != EOF) {
         s += c * b;
     }
+
     printf("VALOR A PAGAR: R$ %.2f\n", s);
 }
