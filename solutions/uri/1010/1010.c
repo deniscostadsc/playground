d88ad4e76c026a8a2a959cacefd456d04ea22601
Denis Costa <deniscostadsc@gmail.com>
2021-06-01 20:06:27 -0300

    Solve Simple Calculate in c

---
+++ solutions/uri/1010/1010.c
@@ -0,0 +1,13 @@
+#include <math.h>
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    float c, s = 0.0;
+
+    while (scanf("%d %d %f", &a, &b, &c) != EOF) {
+        s += c * b;
+    }
+
+    printf("VALOR A PAGAR: R$ %.2f\n", s);
+}
