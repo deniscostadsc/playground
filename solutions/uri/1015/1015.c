bb7e7b91b9d422d5fe2bb0483d0413c2e01c2679
Denis Costa <deniscostadsc@gmail.com>
2021-06-01 17:13:45 -0300

    Solve Distance Between Two Points in c

---
+++ solutions/uri/1015/1015.c
@@ -0,0 +1,11 @@
+#include <math.h>
+#include <stdio.h>
+
+int main() {
+    float x1, y1, x2, y2;
+
+    scanf("%f %f", &x1, &y1);
+    scanf("%f %f", &x2, &y2);
+
+    printf("%.4f\n", sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)));
+}
