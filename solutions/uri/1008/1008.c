78b22cbbcab88e21cb8cc31ee01e8114314071ae
Denis Costa <deniscostadsc@gmail.com>
2021-06-01 11:15:53 -0300

    Solve Salary in c

---
+++ solutions/uri/1008/1008.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    float c;
+
+    scanf("%d", &a);
+    scanf("%d", &b);
+    scanf("%f", &c);
+
+    printf("NUMBER = %d\n", a);
+    printf("SALARY = U$ %.2f\n", c * b);
+
+    return 0;
+}
