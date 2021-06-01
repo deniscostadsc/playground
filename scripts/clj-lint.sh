434c9452032c324cb3126dfc0990c7ce73ff0d0c
Denis Costa <deniscostadsc@gmail.com>
2021-06-01 10:15:15 -0300

    Remove debug code

--- scripts/clj-lint.sh
+++ scripts/clj-lint.sh
@@ -2,12 +2,10 @@
 
 set -euo pipefail
 
-pwd
-
 EXIT_STATUS=0
 
 while IFS= read -r -d '' file; do
     clj-kondo --lint "$file" || EXIT_STATUS=1
-done <   <(find . -name '*.clj' -print0)
+done < <(find . -name '*.clj' -print0)
 
 exit $EXIT_STATUS
