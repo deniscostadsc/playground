644a657309ef6447acdcd9e8102ee68efdb51324
Denis Costa <deniscostadsc@gmail.com>
2021-06-01 20:02:37 -0300

    Solve Salary with Bonus in c

---
+++ solutions/uri/1009/1009.c
@@ -0,0 +1,15 @@
+#include <math.h>
+#include <stdio.h>
+
+int main() {
+    char a[300];
+    float b, c;
+
+    scanf("%s", a);
+    scanf("%f", &b);
+    scanf("%f", &c);
+
+    printf("TOTAL = R$ %.2f\n", b + round((c * 0.15) * 100) / 100);
+
+    return 0;
+}
