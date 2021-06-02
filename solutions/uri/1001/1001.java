18d71fe046c10a19bf92f82ed8ff67659ae2d8f3
Denis Costa <deniscostadsc@gmail.com>
2021-06-02 13:16:12 -0300

    Solve Extremely Basic in java

---
+++ solutions/uri/1001/1001.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        InputStreamReader ir = new InputStreamReader(System.in);
+        BufferedReader in = new BufferedReader(ir);
+
+        int a, b, x;
+
+        a = Integer.parseInt(in.readLine());
+        b = Integer.parseInt(in.readLine());
+
+        x = a + b;
+
+        System.out.printf("X = %d\n", x);
+    }
+}
