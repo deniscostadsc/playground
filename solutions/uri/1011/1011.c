15616782b0e059ff081fae614b9826b176493414
Denis Costa <deniscostadsc@gmail.com>
2021-06-02 13:06:36 -0300

    Solve Sphere in c

---
+++ solutions/uri/1011/1011.c
@@ -0,0 +1,12 @@
+#include <math.h>
+#include <stdio.h>
+
+#define pi 3.14159
+
+int main() {
+    float r;
+
+    while (scanf("%f", &r) != EOF) {
+        printf("VOLUME = %.3f\n", 4.0 / 3.0 * pi * pow(r, 3));
+    }
+}
