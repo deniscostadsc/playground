5afc75aa8d0377d600b5c27a49e4810f15d37abc
Denis Costa <deniscostadsc@gmail.com>
2021-06-02 14:38:59 -0300

    Solve Extremely Basic in kotlin

---
+++ solutions/uri/1001/1001.kt
@@ -0,0 +1,10 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    val sc = Scanner(System.`in`);
+
+    val a: String = sc.next();
+    val b: String = sc.next();
+
+    println("X = " + (a.toInt() + b.toInt()));
+}
