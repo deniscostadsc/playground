ed8985e2d37f6a28a23ff2cd1efffd7b136eeed1
Denis Costa <deniscostadsc@gmail.com>
2021-06-03 16:24:50 -0300

    Imporove languages script

--- scripts/languages.sh
+++ scripts/languages.sh
@@ -7,7 +7,9 @@ pushd "$(dirname "$0")/.." > /dev/null
 FOLDERS=$(find . -name 'problem.txt' | sed 's/problem.txt//g' | sort)
 
 for folder in $FOLDERS; do
-    solutions="$(find -E "$folder" -regex '.*(c|clj|cpp|cs|dart|go|hs|java|js|kt|lua|ml|pas|php|py|r|rb|rs|scala|sql)')"
+    solutions="$(\
+        find -E "$folder" -regex '.*(c|clj|cpp|cs|dart|go|hs|java|js|kt|lua|ml|pas|php|py|r|rb|rs|scala|sql)' |\
+        grep -Ev 'drop-table.sql|schema.sql')"
     echo "$(echo "$solutions" | wc -w) $folder"
 done | sort -nr
 
