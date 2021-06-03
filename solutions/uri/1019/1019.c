f846021291546acc3f6899d00c6ca60003baa371
Denis Costa <deniscostadsc@gmail.com>
2021-06-03 11:12:04 -0300

    Solve Time Conversion in c

---
+++ solutions/uri/1019/1019.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int t, h = 0, m = 0;
+
+    while (scanf("%d", &t) != EOF) {
+        if (t >= 60 * 60) {
+            h = t / (60 * 60);
+            t %= (60 * 60);
+        }
+        if (t >= 60) {
+            m = t / 60;
+            t %= 60;
+        }
+
+        printf("%d:%d:%d\n", h, m, t);
+        h = 0, m = 0;
+    }
+
+    return 0;
+}
