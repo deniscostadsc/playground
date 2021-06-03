b1d5b2c9b0169d893cd69ecfc5402bb4560292a4
Denis Costa <deniscostadsc@gmail.com>
2021-06-03 16:32:47 -0300

    Solve Positive Numbers in c

---
+++ solutions/uri/1060/1060.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    float n;
+    int c = 0;
+    while (scanf("%f", &n) != EOF) {
+        if (n > 0) {
+            c++;
+        }
+    }
+    printf("%d valores positivos\n", c);
+    return 0;
+}
