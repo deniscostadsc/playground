a603af5280511d2c0af42bdbb3800bffacfd7e9a
Denis Costa <deniscostadsc@gmail.com>
2021-06-04 14:45:06 -0300

    Solve Bhaskara's Formula in c

---
+++ solutions/uri/1036/1036.c
@@ -0,0 +1,18 @@
+#include <math.h>
+#include <stdio.h>
+
+int main() {
+    double a, b, c, delta;
+
+    scanf("%lf %lf %lf", &a, &b, &c);
+
+    delta = (b * b) - 4 * a * c;
+    if (delta >= 0 && a != 0) {
+        printf("R1 = %.5lf\n", ((b * -1) + sqrt(delta)) / (2 * a));
+        printf("R2 = %.5lf\n", ((b * -1) - sqrt(delta)) / (2 * a));
+    } else {
+        printf("Impossivel calcular\n");
+    }
+
+    return 0;
+}
