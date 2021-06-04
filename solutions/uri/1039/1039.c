8144596da658cd3854610f51094b7b3407c0d4b4
Denis Costa <deniscostadsc@gmail.com>
2021-06-04 20:33:05 -0300

    Solve Fire Flowers in c

--- solutions/uri/1039/1039.c
+++ solutions/uri/1039/1039.c
@@ -5,7 +5,8 @@ int main() {
     double r1, x1, y1, r2, x2, y2;
     double distance;
 
-    while (scanf("%lf %lf %lf %lf %lf %lf", &r1, &x1, &y1, &r2, &x2, &y2) != EOF) {
+    while (scanf("%lf %lf %lf %lf %lf %lf", &r1, &x1, &y1, &r2, &x2, &y2)
+           != EOF) {
         distance = sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
 
         if (r1 >= distance + r2) {
