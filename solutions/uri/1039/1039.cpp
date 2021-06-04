e1c43d8a7a64f48314a8c1303d8c9722d4f80c0d
Denis Costa <deniscostadsc@gmail.com>
2021-06-04 20:31:41 -0300

    Format code

--- solutions/uri/1039/1039.cpp
+++ solutions/uri/1039/1039.cpp
@@ -6,7 +6,7 @@ int main() {
     double distance;
 
     while (std::cin >> r1 >> x1 >> y1 >> r2 >> x2 >> y2) {
-        distance = sqrt(((x1 - x2) * (x1 - x2)) + ((y1 - y2) * (y1 - y2)));
+        distance = sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
 
         if (r1 >= distance + r2) {
             std::cout << "RICO" << std::endl;
