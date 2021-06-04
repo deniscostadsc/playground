ee8ca2123f3bd0343c9f0d50c9717a24beefed66
Denis Costa <deniscostadsc@gmail.com>
2021-06-04 08:24:26 -0300

    Solve Mirror Sequence in python

---
+++ solutions/uri/2157/2157.py
@@ -0,0 +1,15 @@
+import sys
+from itertools import chain
+
+line_number = int(input())
+
+for line in sys.stdin:
+    start, end = map(int, line.split())
+    print(
+        "".join(
+            chain(
+                map(str, range(start, end + 1)),
+                (x[::-1] for x in map(str, range(end, start - 1, -1))),
+            )
+        )
+    )
