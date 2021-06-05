a2226dd55e1bdf7d0afb5750307f538a2c2cdc86
Denis Costa <deniscostadsc@gmail.com>
2021-06-05 20:40:52 -0300

    Solve Snack in c

---
+++ solutions/uri/1038/1038.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int n, q;
+
+    scanf("%d %d", &n, &q);
+
+    if (n == 1) {
+        printf("Total: R$ %.2f\n", 4.00 * q);
+    } else if (n == 2) {
+        printf("Total: R$ %.2f\n", 4.50 * q);
+    } else if (n == 3) {
+        printf("Total: R$ %.2f\n", 5.00 * q);
+    } else if (n == 4) {
+        printf("Total: R$ %.2f\n", 2.00 * q);
+    } else {
+        printf("Total: R$ %.2f\n", 1.50 * q);
+    }
+
+    return 0;
+}
