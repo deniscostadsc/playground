cf3b60b9ed352b433b18179230887b3288d69ea8
Denis Costa <deniscostadsc@gmail.com>
2021-06-08 18:11:36 -0300

    Solve Simple Sum in javascript

---
+++ solutions/uri/1003/1003.js
@@ -0,0 +1,9 @@
+const input = require('fs').readFileSync('/dev/stdin', 'utf8')
+
+const sum = input
+  .trim()
+  .split('\n')
+  .map(x => parseInt(x))
+  .reduce((acc, value) => acc + value)
+
+console.log(`SOMA = ${sum}`)
