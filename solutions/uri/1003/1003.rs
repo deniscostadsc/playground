e234fbb4e336a6e221ea2ee0ac77ab0177b0c6bc
Denis Costa <deniscostadsc@gmail.com>
2021-06-08 18:35:46 -0300

    Solve Simple Sum in rust

---
+++ solutions/uri/1003/1003.rs
@@ -0,0 +1,14 @@
+use std::io;
+
+fn main() {
+    let mut input_a = String::new();
+    let mut input_b = String::new();
+
+    if let Err(_e) = io::stdin().read_line(&mut input_a){}
+    if let Err(_e) = io::stdin().read_line(&mut input_b){}
+
+    let a: i32 = input_a.trim().parse().unwrap();
+    let b: i32 = input_b.trim().parse().unwrap();
+
+    println!("SOMA = {}", a + b);
+}
