b35db69da72f054e830d471740077f0f1462de67
Denis Costa <deniscostadsc@gmail.com>
2021-06-09 14:55:54 -0300

    Solve Bean in c++

---
+++ solutions/uri/2791/2791.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+    int a, b, c, d;
+
+    while (std::cin >> a >> b >> c >> d) {
+        if (a == 1) {
+            std::cout << 1 << std::endl;
+        } else if (b == 1) {
+            std::cout << 2 << std::endl;
+        } else if (c == 1) {
+            std::cout << 3 << std::endl;
+        } else {
+            std::cout << 4 << std::endl;
+        }
+    }
+
+    return 0;
+}
