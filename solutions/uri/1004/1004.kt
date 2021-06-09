3400ded1a71b26fa3322248df4533ad155189fbe
Denis Costa <deniscostadsc@gmail.com>
2021-06-09 16:13:22 -0300

    Solve Simple Product in kotlin

---
+++ solutions/uri/1004/1004.kt
@@ -0,0 +1,10 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    val sc = Scanner(System.`in`);
+
+    val a: String = sc.next();
+    val b: String = sc.next();
+
+    println("PROD = " + (a.toInt() * b.toInt()));
+}
