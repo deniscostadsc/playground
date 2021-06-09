4cd02fee2b30fc4204d9350b39a893ae2cec65a6
Denis Costa <deniscostadsc@gmail.com>
2021-06-09 16:02:55 -0300

    Solve Simple Product in java

---
+++ solutions/uri/1004/1004.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        InputStreamReader ir = new InputStreamReader(System.in);
+        BufferedReader in = new BufferedReader(ir);
+
+        int a, b;
+
+        a = Integer.parseInt(in.readLine());
+        b = Integer.parseInt(in.readLine());
+
+        System.out.printf("PROD = %d\n", a * b);
+    }
+}
