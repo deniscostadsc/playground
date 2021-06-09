0876fe153ff52646cd058ecd3b87f0846578f610
Denis Costa <deniscostadsc@gmail.com>
2021-06-08 21:03:34 -0300

    Solve Area of a Circle in java

---
+++ solutions/uri/1002/1002.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        InputStreamReader ir = new InputStreamReader(System.in);
+        BufferedReader in = new BufferedReader(ir);
+
+        double n;
+
+        n = Double.parseDouble(in.readLine());
+
+        System.out.printf("A=%.4f\n", n * n * 3.14159);
+    }
+}
