c2746ea2cef3f1580752a54709956be737b5baae
Denis Costa <deniscostadsc@gmail.com>
2021-06-09 15:49:45 -0300

    Solve Simple Product in c#

---
+++ solutions/uri/1004/1004.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+class Solution {
+    static void Main() {
+        int a, b;
+        string val;
+
+        val = Console.ReadLine();
+        a = Convert.ToInt32(val);
+        val = Console.ReadLine();
+        b = Convert.ToInt32(val);
+
+        Console.Write("PROD = {0}{1}", a * b, Environment.NewLine);
+    }
+}
