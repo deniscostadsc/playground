45c37b9ec75a584c74ef5a34595b2cc8a22fd4fe
Denis Costa <deniscostadsc@gmail.com>
2021-06-10 05:09:04 -0300

    Solve Trinomial Triangle in c++

---
+++ solutions/uri/1795/1795.cpp
@@ -0,0 +1,29 @@
+#include <cstdio>
+#include <cstring>
+
+unsigned long long memo[1000];
+
+unsigned long long calculate_sum(unsigned long long line) {
+    if (memo[line] != 0) {
+        return memo[line];
+    }
+
+    if (line == 0) {
+        memo[line] = 1;
+    } else {
+        memo[line] = calculate_sum(line - 1) * 3;
+    }
+
+    return memo[line];
+}
+
+int main() {
+    unsigned long long line;
+    memset(memo, 0, sizeof(memo));
+
+    while(scanf("%llu", &line) != EOF) {
+        printf("%llu\n", calculate_sum(line));
+    }
+
+    return 0;
+}
