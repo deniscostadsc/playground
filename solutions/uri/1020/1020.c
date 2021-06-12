aaab81c1bbe221302ab0931f0700265932ecc18d
Denis Costa <deniscostadsc@gmail.com>
2021-06-12 13:27:05 -0300

    Solve Age in Days in c

---
+++ solutions/uri/1020/1020.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int t, a = 0, m = 0, d = 0;
+
+    while (scanf("%d", &t) != EOF) {
+        if (t >= 365) {
+            a = t / 365;
+            t %= 365;
+        }
+        if (t >= 30) {
+            m = t / 30;
+            t %= 30;
+        }
+
+        d = t;
+
+        printf("%d ano(s)\n", a);
+        printf("%d mes(es)\n", m);
+        printf("%d dia(s)\n", d);
+    }
+
+    return 0;
+}
