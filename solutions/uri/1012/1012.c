783d3c796b23f5cb44cf4ed225377328fa85f7c4
Denis Costa <deniscostadsc@gmail.com>
2021-06-12 13:23:55 -0300

    Solve Area in c

---
+++ solutions/uri/1012/1012.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define pi 3.14159
+
+int main() {
+    float a, b, c;
+
+    while (scanf("%f %f %f", &a, &b, &c) != EOF) {
+        printf("TRIANGULO: %.3f\n", a * c / 2);
+        printf("CIRCULO: %.3f\n", pi * (c * c));
+        printf("TRAPEZIO: %.3f\n", c * ((a + b) / 2));
+        printf("QUADRADO: %.3f\n", b * b);
+        printf("RETANGULO: %.3f\n", a * b);
+    }
+}
