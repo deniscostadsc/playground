03698a736137c2876e6fcd79ab2f367f18b86c9c
Denis Costa <deniscostadsc@gmail.com>
2021-06-12 13:37:54 -0300

    Add wrong solution

---
+++ solutions/uri/1021/1021.c
@@ -0,0 +1,38 @@
+#include <math.h>
+#include <stdio.h>
+
+int main() {
+    double n;
+    int d[] = {10000, 5000, 2000, 1000, 500, 200, 100, 50, 25, 10, 5, 1};
+    int total_value, t = 0, c, i;
+
+    while (scanf("%lf", &n) != EOF) {
+        total_value = (int)(ceil((n * 100)));
+        printf("NOTAS:\n");
+
+        t = 0;
+
+        for (i = 0; i < 12; i++) {
+            c = 0;
+
+            while (total_value >= d[t]) {
+                total_value -= d[t];
+                c++;
+            }
+
+            if (d[t] == 100) {
+                printf("MOEDAS:\n");
+            }
+
+            if (d[t] >= 200) {
+                printf("%d nota(s) de R$ %d.00\n", c, d[t] / 100);
+            } else {
+                printf("%d moeda(s) de R$ %.2f\n", c, d[t] / 100.0);
+            }
+
+            t++;
+        }
+    }
+
+    return 0;
+}
