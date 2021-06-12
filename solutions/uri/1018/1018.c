e1b180d337fb837dbde5fe2b3b1f77c2c7fc3855
Denis Costa <deniscostadsc@gmail.com>
2021-06-12 13:24:57 -0300

    Solve Banknotes in c

---
+++ solutions/uri/1018/1018.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int d[] = {100, 50, 20, 10, 5, 2, 1};
+    int n, t = 0;
+
+    scanf("%d", &n);
+    printf("%d\n", n);
+    do {
+        printf("%d nota(s) de R$ %d,00\n", n / d[t], d[t]);
+        if (n >= d[t]) {
+            n -= d[t] * (n / d[t]);
+        }
+    } while (d[t++] != 1);
+
+    return 0;
+}
