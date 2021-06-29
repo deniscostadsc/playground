91bbf13cda514629246405c0190da6e89f575606
Denis Costa <deniscostadsc@gmail.com>
2021-06-29 10:36:27 -0300

    Format code

--- solutions/uri/1001/1001.hs
+++ solutions/uri/1001/1001.hs
@@ -1,5 +1,5 @@
 main :: IO ()
 main = do
-        a <- readLn :: IO Int
-        b <- readLn :: IO Int
-        putStrLn $ id ("X = " ++ show(a + b))
+    a <- readLn :: IO Int
+    b <- readLn :: IO Int
+    putStrLn $ id ("X = " ++ show(a + b))
--- solutions/uri/1004/1004.hs
+++ solutions/uri/1004/1004.hs
@@ -1,5 +1,5 @@
 main :: IO ()
 main = do
-        a <- readLn :: IO Int
-        b <- readLn :: IO Int
-        putStrLn $ id ("PROD = " ++ show(a * b))
+    a <- readLn :: IO Int
+    b <- readLn :: IO Int
+    putStrLn $ id ("PROD = " ++ show(a * b))
