b117729d3c40e9a09c201b59d2a670fe074fa3f9
Denis Costa <deniscostadsc@gmail.com>
2021-06-30 10:02:53 -0300

    Solve Difference in dart

---
+++ solutions/uri/1007/1107.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+void main() {
+  int a = int.parse(stdin.readLineSync() ?? '');
+  int b = int.parse(stdin.readLineSync() ?? '');
+  int c = int.parse(stdin.readLineSync() ?? '');
+  int d = int.parse(stdin.readLineSync() ?? '');
+
+  int diff = a * b - c * d;
+
+  print("DIFERENCA = $diff");
+}
