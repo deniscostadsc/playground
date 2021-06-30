fe24f8e572ea7dde63f0dbf10240d64c31d4176b
Denis Costa <deniscostadsc@gmail.com>
2021-06-30 10:17:44 -0300

    Solve Average 2 in dart

---
+++ solutions/uri/1006/1006.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+void main() {
+  double a = double.parse(stdin.readLineSync() ?? '');
+  double b = double.parse(stdin.readLineSync() ?? '');
+  double c = double.parse(stdin.readLineSync() ?? '');
+
+  double media = (a * 2.0 + b * 3.0 + c * 5.0) / 10.0;
+
+  print("MEDIA = ${media.toStringAsFixed(1)}");
+}
