3ed1023d3cc5b06200ab4862f165edf0fa11275b
Denis Costa <deniscostadsc@gmail.com>
2021-07-01 13:54:38 -0300

    Solve Average 2 in java

---
+++ solutions/uri/1006/1006.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        InputStreamReader ir = new InputStreamReader(System.in);
+        BufferedReader in = new BufferedReader(ir);
+
+        float a, b, c;
+
+        a = Float.parseFloat(in.readLine());
+        b = Float.parseFloat(in.readLine());
+        c = Float.parseFloat(in.readLine());
+
+        System.out.printf("MEDIA = %.1f\n", (a * 2.0 + b * 3.0 + c * 5.0) / 10.0);
+    }
+}
