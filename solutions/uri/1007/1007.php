17ee5cab7fde0ced386e1110e42c4fd6ee28c1ca
Denis Costa <deniscostadsc@gmail.com>
2021-07-05 09:19:59 -0300

    Solve Difference in php

---
+++ solutions/uri/1007/1007.php
@@ -0,0 +1,9 @@
+<?php
+$a = trim(fgets(STDIN));
+$b = trim(fgets(STDIN));
+$c = trim(fgets(STDIN));
+$d = trim(fgets(STDIN));
+$difference = $a * $b - $c * $d;
+
+echo "DIFERENCA = " . $difference . "\n";
+?>
