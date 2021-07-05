337bd17d8b4d26c55bb842c88d7e7b0e6d0d000c
Denis Costa <deniscostadsc@gmail.com>
2021-07-04 22:19:58 -0300

    Fix typo

--- solutions/uri/1031/1031.cpp
+++ solutions/uri/1031/1031.cpp
@@ -17,7 +17,7 @@ int main() {
         Why need '- 1' and it's 12 not 13?
 
         Because classic josephus function assume that sequence starts at 1,
-        but shoots at gap-th person. This version of problem allways shut 1
+        but shoots at gap-th person. This version of problem allways shot 1
         off first, and then goes to gap-th number.
         */
         while (josephus(n - 1, m) != 12) {
