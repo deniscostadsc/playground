88b058d92141fa38da8b0fe337cfe27210fc46ae
Denis Costa <deniscostadsc@gmail.com>
2021-07-10 10:53:20 -0300

    Solve Salary in java

---
+++ solutions/uri/1008/1008.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        InputStreamReader ir = new InputStreamReader(System.in);
+        BufferedReader in = new BufferedReader(ir);
+
+        int a, b;
+        float c;
+
+        a = Integer.parseInt(in.readLine());
+        b = Integer.parseInt(in.readLine());
+        c = Float.parseFloat(in.readLine());
+
+        System.out.printf("NUMBER = %d\n", a);
+        System.out.printf("SALARY = U$ %.2f\n", b * c);
+    }
+}
