cd5ab6668d09c0fec66a3ea27e5f5c74427df1ba
Denis Costa <deniscostadsc@gmail.com>
2021-07-22 21:51:10 -0300

    Add wrong solution

---
+++ solutions/uri/1010/1010.clj
@@ -0,0 +1,14 @@
+(ns main
+  (:require [clojure.string :as str]))
+
+(defn main []
+  (loop [line (read-line)
+         result 0.0]
+    (if (= ":done" line)
+      result
+      (let [[_ b c] (str/split line #" ")
+            b' (Double/parseDouble b)
+            c' (Double/parseDouble c)]
+        (recur (read-line) (+ result (* b' c')))))))
+
+(printf "VALOR A PAGAR: R$ %.2f%n" (main))
