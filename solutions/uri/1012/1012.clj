11d43e4df28f03e178a0685267b397b1a0ebe672
Denis Costa <deniscostadsc@gmail.com>
2021-07-31 20:17:03 -0300

    Solve Area in clojure

---
+++ solutions/uri/1012/1012.clj
@@ -0,0 +1,20 @@
+(ns main
+  (:require [clojure.string :as str]))
+
+(def pi 3.14159)
+
+(defn main []
+  (loop [line (read-line)]
+    (when line
+      (let [[a b c] (str/split line #" ")
+            a' (Double/parseDouble a)
+            b' (Double/parseDouble b)
+            c' (Double/parseDouble c)]
+        (printf "TRIANGULO: %.3f%n" (/ (* a' c') 2))
+        (printf "CIRCULO: %.3f%n" (* pi (* c' c')))
+        (printf "TRAPEZIO: %.3f%n" (* (/ (+ a' b') 2) c'))
+        (printf "QUADRADO: %.3f%n" (* b' b'))
+        (printf "RETANGULO: %.3f%n" (* a' b')))
+      (recur (read-line)))))
+
+(main)
