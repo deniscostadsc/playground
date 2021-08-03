ce1f2b42a80d66b3c78ffbcd682f0b4546f75ffd
Denis Costa <deniscostadsc@gmail.com>
2021-08-03 18:26:44 -0300

    Solve Simple Calculate in dart

---
+++ solutions/uri/1010/1010.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+void main() {
+  int b;
+  double c, result = 0.0;
+  String line;
+
+
+  while ((line = stdin.readLineSync()) != null) {
+    var numbers = line.split(' ');
+    b = int.parse(numbers[1]);
+    c = double.parse(numbers[2]);
+
+    result += c * b;
+  }
+
+  print("VALOR A PAGAR: R\$ ${result.toStringAsFixed(2)}");
+}
