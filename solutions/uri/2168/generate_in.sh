c65bd65cd15056b50eb07707a77fce72b698218d
Denis Costa <deniscostadsc@gmail.com>
2021-08-11 15:22:08 -0300

    Fix lint

--- solutions/uri/2168/generate_in.sh
+++ solutions/uri/2168/generate_in.sh
@@ -2,19 +2,22 @@
 
 set -euo pipefail
 
+
 TEST_CASES=10
 MAX_N=30
 
+# shellcheck disable=SC2167
 for _ in $(seq $TEST_CASES); do
-    n=$((RANDOM % $MAX_N + 1))
+    n=$((RANDOM % MAX_N + 1))
 
     echo "$n"
 
+    # shellcheck disable=SC2165
     for _ in $(seq $((n + 1))); do
         for j in $(seq $((n + 1))); do
             echo -n "$((RANDOM % 2))"
 
-            if [ $j -ne $((n + 1)) ]; then
+            if [ "$j" -ne $((n + 1)) ]; then
                 echo -n " "
             else
                 echo
