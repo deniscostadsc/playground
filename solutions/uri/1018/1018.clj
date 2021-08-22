b04a30aaecaac9d5728e064cf79ddff0b12426bc
Denis Costa <deniscostadsc@gmail.com>
2021-08-22 14:36:43 -0300

    Solve Banknotes in clojure

---
+++ solutions/uri/1018/1018.clj
@@ -0,0 +1,20 @@
+(ns main)
+
+(defn main []
+  (let [bills '(100 50 20 10 5 2 1)
+        value (-> (read-line)
+                  (Integer/parseInt))]
+    (println value)
+    (loop [[bill & remaining-bills] bills
+           v value]
+      (printf "%d nota(s) de R$ %d,00%n" (quot v bill) bill)
+      (when (seq remaining-bills)
+        (recur
+         remaining-bills
+         (if (>= v bill)
+           (->> (quot v bill)
+                (* bill)
+                (- v))
+           v))))))
+
+(main)
