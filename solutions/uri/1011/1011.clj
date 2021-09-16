c936417a20caaf3ab227d8252f822171c9e654cf
Denis Costa <deniscostadsc@gmail.com>
2021-09-16 19:18:30 -0300

    Refactor 1011

--- solutions/uri/1011/1011.clj
+++ solutions/uri/1011/1011.clj
@@ -6,7 +6,10 @@
   (loop [line (read-line)]
     (when line
        (let [radius (Double/parseDouble line)]
-         (printf "VOLUME = %.3f%n" (*(*(/ 4.0 3.0) pi) (Math/pow radius 3))))
+         (printf "VOLUME = %.3f%n"
+                 (-> (/ 4.0 3.0)
+                     (* pi)
+                     (* (Math/pow radius 3)))))
        (recur (read-line)))))
 
 (main)
