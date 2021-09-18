5acd1689def48d35d5d2e0547d602b5f786543c2
Denis Costa <deniscostadsc@gmail.com>
2021-09-18 14:04:09 -0300

    Solve Simple Calculate in php

---
+++ solutions/uri/1010/1010.php
@@ -0,0 +1,12 @@
+<?php
+$s = 0.0;
+
+while($line = fgets(STDIN)){
+    $arr = explode(" ", $line);
+    $b = $arr[1];
+    $c = $arr[2];
+    $s += $b * $c;
+}
+
+echo "VALOR A PAGAR: R$ " . number_format($s, 2, '.', '') . "\n";
+?>
