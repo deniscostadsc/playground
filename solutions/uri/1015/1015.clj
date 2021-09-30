2c88d99bd56d0e9bde57a33872e737c6beaac77e
Denis Costa <deniscostadsc@gmail.com>
2021-09-30 09:39:22 -0300

    Refactor code

--- solutions/uri/1015/1015.clj
+++ solutions/uri/1015/1015.clj
@@ -1,10 +1,20 @@
 (ns main
   (:require [clojure.string :as str]))
 
+(defn str->double
+  [sequence]
+  (map #(Double/parseDouble %) sequence))
+
+(defn read-line->double []
+  (-> (read-line)
+    (str/split #" ")
+    (str->double)))
+
 (defn main []
-  (let [[x1 y1] (-> (read-line) (str/split #" "))
-        [x2 y2] (-> (read-line) (str/split #" "))
-        [x1' y1' x2' y2'] (map #(Double/parseDouble %) [x1 y1 x2 y2])]
-    (printf "%.4f%n" (Math/sqrt (+ (Math/pow (- x2' x1') 2) (Math/pow (- y2' y1') 2))))))
+  (let [[x1 y1] (read-line->double)
+        [x2 y2] (read-line->double)]
+    (printf
+      "%.4f%n"
+      (Math/sqrt (+ (Math/pow (- x2 x1) 2) (Math/pow (- y2 y1) 2))))))
 
 (main)
