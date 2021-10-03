854ca7be22be9f771c0e23d99a279035e163f404
Denis Costa <deniscostadsc@gmail.com>
2021-10-03 17:44:59 -0300

    Solve Simple Calculate in java

---
+++ solutions/uri/1010/1010.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        double a, b, c, s = 0.0;
+
+        while (sc.hasNextDouble()) {
+            a = sc.nextDouble();
+            b = sc.nextDouble();
+            c = sc.nextDouble();
+
+            s += b * c;
+        }
+
+        System.out.printf("VALOR A PAGAR: R$ %.2f\n", s);
+
+        sc.close();
+    }
+}
