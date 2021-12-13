5c000543ce943619ea89b2443395a2ee10c49ee0
Denis Costa <deniscostadsc@gmail.com>
2021-12-13 18:20:08 -0300

    Refactor python version of Simple Calculate

--- solutions/beecrowd/1010/1010.py
+++ solutions/beecrowd/1010/1010.py
@@ -3,8 +3,8 @@ import sys
 s = 0.0
 
 for line in sys.stdin:
-    a, b, c = line.split()
-    a, b, c = int(a), int(b), float(c)
+    _, b, c = line.split()
+    b, c = int(b), float(c)
     s += c * b
 
 print(f'VALOR A PAGAR: R$ {s:.2f}')
