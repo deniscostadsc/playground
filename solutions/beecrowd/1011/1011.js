898ffaa93ea4a1f75f1ca834bf22283cbeb0bac2
Denis Costa <deniscostadsc@gmail.com>
2021-12-15 09:36:34 -0300

    Solve Sphere in javascript

---
+++ solutions/beecrowd/1011/1011.js
@@ -0,0 +1,13 @@
+const readline = require('readline')
+
+const PI = 3.14159
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+})
+
+rl.on('line', function (line) {
+  console.log(`VOLUME = ${(4.0 / 3.0 * PI * Math.pow(parseInt(line), 3)).toFixed(3)}`)
+})
