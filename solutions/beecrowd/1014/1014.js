27335e5637950c44eaf7de07ab611fb9542eed36
Denis Costa <deniscostadsc@gmail.com>
2021-12-18 14:14:10 -0300

    Solve Consumption in javascript

---
+++ solutions/beecrowd/1014/1014.js
@@ -0,0 +1,23 @@
+const readline = require('readline')
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  terminal: false
+})
+
+let evenLine = false
+let previousLine
+
+(() => {
+  rl.on('line', (line) => {
+    const n = parseFloat(line)
+
+    if (evenLine) {
+      console.log(`${(previousLine / n).toFixed(3)} km/l`)
+    } else {
+      previousLine = n
+    }
+
+    evenLine = !evenLine
+  })
+})()
