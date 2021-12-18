b3e9fa986e36278f76b6296419f3d821fc6dcf62
Denis Costa <deniscostadsc@gmail.com>
2021-12-18 13:34:46 -0300

    Solve The Greatest in javascript

---
+++ solutions/beecrowd/1013/1013.js
@@ -0,0 +1,18 @@
+const readline = require('readline')
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+});
+
+(() => {
+  rl.on('line', (line) => {
+    const [a, b, c] = line.split(' ').map(x => parseInt(x))
+
+    let m = Math.floor((a + b + Math.abs(a - b)) / 2)
+    m = Math.floor((m + c + Math.abs(m - c)) / 2)
+
+    console.log(`${m} eh o maior`)
+  })
+})()
