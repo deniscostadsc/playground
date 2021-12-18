65de28a7dc83d40e7885705ef82ca590aae65be2
Denis Costa <deniscostadsc@gmail.com>
2021-12-18 13:36:51 -0300

    Refactor

--- solutions/beecrowd/1010/1010.py
+++ solutions/beecrowd/1010/1010.py
@@ -3,8 +3,8 @@ import sys
 s = 0.0
 
 for line in sys.stdin:
-    _, b, c = line.split()
-    b, c = int(b), float(c)
-    s += c * b
+    _, a, b = line.split()
+    a, b = int(a), float(b)
+    s += a * b
 
 print(f'VALOR A PAGAR: R$ {s:.2f}')
