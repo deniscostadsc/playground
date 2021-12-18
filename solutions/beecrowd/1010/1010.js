10c3a597446a45738bcc9b1e50683943c1393a5e
Denis Costa <deniscostadsc@gmail.com>
2021-12-18 13:29:52 -0300

    Solve Simple Calculate in javascript

---
+++ solutions/beecrowd/1010/1010.js
@@ -0,0 +1,21 @@
+const readline = require('readline')
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+});
+
+(() => {
+  let s = 0.0
+
+  rl.on('line', (line) => {
+    const [, a, b] = line.split(' ')
+
+    s += parseInt(a) * parseFloat(b)
+  })
+
+  rl.on('close', () => {
+    console.log(`VALOR A PAGAR: R$ ${s.toFixed(2)}`)
+  })
+})()
