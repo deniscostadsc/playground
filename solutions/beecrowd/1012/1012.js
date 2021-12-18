1b21816290cf69c4e77ca7da9d8bee9061ff7075
Denis Costa <deniscostadsc@gmail.com>
2021-12-18 13:33:55 -0300

    Solve Area in javascript

---
+++ solutions/beecrowd/1012/1012.js
@@ -0,0 +1,21 @@
+const readline = require('readline')
+
+const PI = 3.14159
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+});
+
+(() => {
+  rl.on('line', (line) => {
+    const [a, b, c] = line.split(' ').map(x => parseFloat(x))
+
+    console.log(`TRIANGULO: ${(a * c / 2).toFixed(3)}`)
+    console.log(`CIRCULO: ${(PI * c * c).toFixed(3)}`)
+    console.log(`TRAPEZIO: ${(c * ((a + b) / 2)).toFixed(3)}`)
+    console.log(`QUADRADO: ${(b * b).toFixed(3)}`)
+    console.log(`RETANGULO: ${(a * b).toFixed(3)}`)
+  })
+})()
