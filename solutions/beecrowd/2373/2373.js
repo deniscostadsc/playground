11fa8e2b708571940451f005e1f55af0b6e5764a
Denis Costa <deniscostadsc@gmail.com>
2021-12-19 18:36:42 -0300

    Solve Garçom in javascript

---
+++ solutions/beecrowd/2373/2373.js
@@ -0,0 +1,28 @@
+const readline = require('readline')
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  terminal: false
+});
+
+(() => {
+  let totalBrokenglasses = 0
+  let firstLine = true
+
+  rl.on('line', (line) => {
+    if (firstLine) {
+      firstLine = false
+      return
+    }
+
+    const [l, c] = line.split(' ').map(x => parseInt(x))
+
+    if (l > c) {
+      totalBrokenglasses += c
+    }
+  })
+
+  rl.on('close', () => {
+    console.log(`${totalBrokenglasses}`)
+  })
+})()
