d8562e3b5da99ce45aeab3f7047dc8a1cc7201a5
Denis Costa <deniscostadsc@gmail.com>
2022-03-29 18:09:16 -0300

    Move variable declaration to the top of method

--- solutions/beecrowd/1010/1010.cs
+++ solutions/beecrowd/1010/1010.cs
@@ -5,9 +5,10 @@ class Solution {
         int b;
         double c, s = 0.0;
         string line;
+        string[] numbers;
 
         while ((line = Console.ReadLine()) != null) {
-            string[] numbers = line.Split(' ');
+            numbers = line.Split(' ');
             b = Convert.ToInt16(numbers[1]);
             c = Convert.ToDouble(numbers[2]);
             s += c * b;
