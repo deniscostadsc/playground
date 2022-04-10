62fb9fe0e5660919cd19bba9b073d84a8ad52511
Denis Costa <deniscostadsc@gmail.com>
2022-04-10 20:22:52 -0300

    Solve Triangle in python

---
+++ solutions/beecrowd/1929/1929.py
@@ -0,0 +1,23 @@
+import sys
+
+def can_form_trinagle(a, b, c):
+    if a >= b + c:
+        return False
+    if b >= a + c:
+        return False
+    if c >= a + b:
+        return False
+    return True
+
+for line in sys.stdin:
+    a, b, c, d = map(int, line.split())
+
+    result = can_form_trinagle(a, b, c)
+    result |= can_form_trinagle(a, b, d)
+    result |= can_form_trinagle(a, c, d)
+    result |= can_form_trinagle(b, c, d)
+
+    if result:
+        print("S")
+    else:
+        print("N")
---
+++ solutions/beecrowd/1929/generate_in.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+TESTCASE=150
+MAX_N=20
+
+for i in $(seq $TESTCASE); do
+    echo -n "$((RANDOM % MAX_N + 1)) $((RANDOM % MAX_N + 1)) "
+    echo "$((RANDOM % MAX_N + 1)) $((RANDOM % MAX_N + 1))"
+done
+
+MAX_N=40
+
+for i in $(seq $TESTCASE); do
+    echo -n "$((RANDOM % MAX_N + 1)) $((RANDOM % MAX_N + 1)) "
+    echo "$((RANDOM % MAX_N + 1)) $((RANDOM % MAX_N + 1))"
+done
