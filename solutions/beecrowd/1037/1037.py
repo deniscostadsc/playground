c779f9668bfb00b14919bdad2853942adca11d12
Denis Costa <deniscostadsc@gmail.com>
2022-04-10 18:02:50 -0300

    Solve Interval in python

---
+++ solutions/beecrowd/1037/1037.py
@@ -0,0 +1,15 @@
+import sys
+
+for line in sys.stdin:
+    n = float(line)
+
+    if n >= 0.0 and n <= 25.0:
+        print("Intervalo [0,25]")
+    elif n > 25.0 and n <= 50.0:
+        print("Intervalo (25,50]")
+    elif n > 50.0 and n <= 75.0:
+        print("Intervalo (50,75]")
+    elif n > 75.0 and n <= 100:
+        print("Intervalo (75,100]")
+    else:
+        print("Fora de intervalo")
