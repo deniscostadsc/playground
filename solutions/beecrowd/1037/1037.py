ce7f6c77efdcce09ef277e8195b2262532b3112a
Denis Costa <deniscostadsc@gmail.com>
2022-04-10 18:08:31 -0300

    Fix lint

--- solutions/beecrowd/1037/1037.py
+++ solutions/beecrowd/1037/1037.py
@@ -4,12 +4,12 @@ for line in sys.stdin:
     n = float(line)
 
     if n >= 0.0 and n <= 25.0:
-        print("Intervalo [0,25]")
+        print('Intervalo [0,25]')
     elif n > 25.0 and n <= 50.0:
-        print("Intervalo (25,50]")
+        print('Intervalo (25,50]')
     elif n > 50.0 and n <= 75.0:
-        print("Intervalo (50,75]")
+        print('Intervalo (50,75]')
     elif n > 75.0 and n <= 100:
-        print("Intervalo (75,100]")
+        print('Intervalo (75,100]')
     else:
-        print("Fora de intervalo")
+        print('Fora de intervalo')
