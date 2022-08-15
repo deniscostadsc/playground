515f1c979bd3d006694caf2103d0194a296fcda3
Denis Costa <deniscostadsc@gmail.com>
2022-08-15 18:52:00 -0300

    Solve Sphere in dart

---
+++ solutions/beecrowd/1011/1011.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+import "dart:math";
+
+void main() {
+  double r, pi = 3.14159;
+  var line;
+
+  while ((line = stdin.readLineSync()) != null) {
+    r = double.parse(line);
+    print("VOLUME = ${(4.0 / 3.0 * pi * pow(r, 3)).toStringAsFixed(3)}");
+  }
+}
