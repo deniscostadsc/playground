a944ce53485143048ebf7168126151b980293d40
Denis Costa <deniscostadsc@gmail.com>
2022-08-20 18:19:08 -0300

    Update python lint

--- solutions/project-euler/016/016.py
+++ solutions/project-euler/016/016.py
@@ -1,1 +1,1 @@
-print(sum(map(int, str(2 ** 1000))))
+print(sum(map(int, str(2**1000))))
--- solutions/project-euler/048/048.py
+++ solutions/project-euler/048/048.py
@@ -1,1 +1,1 @@
-print(str(sum(x ** x for x in range(1, 1000)))[-10:])
+print(str(sum(x**x for x in range(1, 1000)))[-10:])
