1da1356163129de0d650fe603cd24570e2e058c1
Denis Costa <deniscostadsc@gmail.com>
2022-08-23 21:38:46 -0300

    Fix lint

--- solutions/beecrowd/2746/schema.sql
+++ solutions/beecrowd/2746/schema.sql
@@ -10,13 +10,13 @@ CREATE TABLE virus (
 
 
 INSERT INTO virus(id, name)
-VALUES 
+VALUES
       (1, 'H1RT'),
       (2, 'H7H1'),
       (3, 'HUN8'),
       (4, 'XH1HX'),
       (5, 'XXXX');
 
-  
+
   /*  Execute this query to drop the tables */
   -- DROP TABLE virus; --
