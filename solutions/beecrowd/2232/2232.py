c30614745616f83f8b86dd71b677b912b30d6035
Denis Costa <deniscostadsc@gmail.com>
2022-08-30 12:16:12 -0300

    Refactor

--- solutions/beecrowd/2232/2232.py
+++ solutions/beecrowd/2232/2232.py
@@ -5,10 +5,9 @@ def sum_triangle_until_row(n):
     if n in previous_results:
         return previous_results[n]
 
-    if n == 1:
-        result = 2 ** (n - 1)
-    else:
-        result = 2 ** (n - 1) + sum_triangle_until_row(n - 1)
+    result = 2 ** (n - 1)
+    if n != 1:
+        result += sum_triangle_until_row(n - 1)
 
     previous_results[n] = result
 
