d5035639a5f4c211fbb5b0440c05dc502ba4c0e0
Denis Costa <deniscostadsc@gmail.com>
2022-09-05 11:42:24 -0300

    Refactor

--- solutions/beecrowd/1063/1063.py
+++ solutions/beecrowd/1063/1063.py
@@ -14,14 +14,14 @@ def main():
 
         while current_order_index < len(current_order) or aux_stack:
             if (
-                desired_order
+                desired_order_index < len(desired_order)
                 and aux_stack
                 and desired_order[desired_order_index] == aux_stack[-1]
             ):
                 aux_stack.pop()
                 desired_order_index += 1
                 print('R', end='')
-            elif current_order_index >= len(current_order) and aux_stack:
+            elif current_order_index >= len(current_order):
                 break
             else:
                 aux_stack.append(current_order[current_order_index])
