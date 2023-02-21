823c9aa92dc502cd0f0409bc8c1a5486feab74da
Denis Costa <deniscostadsc@gmail.com>
2023-02-21 08:23:16 -0300

    Fix sql CI runtime

--- scripts/run-problems.sh
+++ scripts/run-problems.sh
@@ -8,23 +8,17 @@ DOCKER_RUN_PREFIX=$3
 
 for language in $LANGUAGES; do
     if [ "$language" == "sql" ]; then
-        :
-        # TODO: ensure to pass variables to container using docker-compose
-        #FOLDERS="$FOLDERS" \
-        #USER="$(id -u):$(id -g)" \
-        #docker-compose \
-        #    -f .docker/sql-docker-compose.yml \
-        #    --log-level ERROR \
-        #    up \
-        #    --build \
-        #    --quiet-pull \
-        #    --abort-on-container-exit \
-        #    --exit-code-from database-client
+        FOLDERS="$FOLDERS" \
+        USER="$(id -u):$(id -g)" \
+        docker-compose \
+            -f .docker/sql-docker-compose.yml \
+            --log-level ERROR \
+            up \
+            --build \
+            --quiet-pull \
+            --abort-on-container-exit \
+            --exit-code-from database-client
     else
         $DOCKER_RUN_PREFIX -e FOLDERS="$FOLDERS" "$language"
-
-        # TODO: avoid creating many containers that works when something fails within the container
-        #$DOCKER_RUN_PREFIX --name "${language}-container" -e FOLDERS="$FOLDERS" "$language"
-        #docker rm $(docker stop $(docker ps -a -q --filter="name=${language}-container" --format="{{.ID}}")) > /dev/null
     fi
 done
