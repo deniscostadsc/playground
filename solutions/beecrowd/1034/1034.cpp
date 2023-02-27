24e916ab75c5e65627c00636fc06ea6a0f3a2798
Denis Costa <deniscostadsc@gmail.com>
2023-02-27 09:56:30 -0300

    Refactor Ice Statues Festival

--- solutions/beecrowd/1034/1034.cpp
+++ solutions/beecrowd/1034/1034.cpp
@@ -10,13 +10,15 @@ std::int32_t minimum_blocks(std::int32_t m, std::int32_t n) {
     std::int32_t i, j, result[m + 1];
 
     result[0] = 0;
-    for (i = 1; i <= m; i++) {
-        result[i] = std::numeric_limits< std::int32_t >::max();
-    }
 
     for (j = 0; j < n; j++) {
         for (i = blocks[j]; i <= m; i++) {
-            result[i] = std::min(result[i], result[i - blocks[j]] + 1);
+            if (j == 0) {
+                result[i] = std::numeric_limits< std::int32_t >::max();
+            }
+            if (blocks[j] <= i) {
+                result[i] = std::min(result[i], result[i - blocks[j]] + 1);
+            }
         }
     }
 
@@ -30,8 +32,6 @@ int main() {
 
     while (t--) {
         std::cin >> n >> m;
-
-        memset(blocks, 0, sizeof(blocks));
 
         for (i = 0; i < n; i++) {
             std::cin >> blocks[i];
