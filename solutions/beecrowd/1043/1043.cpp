7a52173a5408b4c5c50b6bf239c2a89478af9d4f
Denis Costa <deniscostadsc@gmail.com>
2023-03-02 19:12:51 -0300

    Improve input

--- solutions/beecrowd/1043/1043.cpp
+++ solutions/beecrowd/1043/1043.cpp
@@ -4,14 +4,12 @@
 int main() {
     float a, b, c;
 
-    std::cin >> a;
-    std::cin >> b;
-    std::cin >> c;
-
-    if (a < b + c && b < a + c && c < a + b) {
-        printf("Perimetro = %.1f\n", a + b + c);
-    } else {
-        printf("Area = %.1f\n", c * (a + b) / 2);
+    while (std::cin >> a >> b >> c) {
+        if (a < b + c && b < a + c && c < a + b) {
+            printf("Perimetro = %.1f\n", a + b + c);
+        } else {
+            printf("Area = %.1f\n", c * (a + b) / 2);
+        }
     }
 
     return 0;
---
+++ solutions/beecrowd/1043/generate_in.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+
+for i in $(seq 1 7); do
+    for j in $(seq 1 7); do
+        for k in $(seq 1 7); do
+            echo "${i}.0 ${j}.0 ${k}.0"
+        done
+    done
+done
