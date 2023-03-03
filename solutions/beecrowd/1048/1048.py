5bf9b40ba24842d042b4a29d6fbba1caf451bb02
Denis Costa <deniscostadsc@gmail.com>
2023-03-03 09:49:59 -0300

    Fix lint

--- solutions/beecrowd/1048/1048.py
+++ solutions/beecrowd/1048/1048.py
@@ -4,22 +4,32 @@ for line in sys.stdin:
     salary = float(line)
 
     if salary <= 400.00:
-        print(f'Novo salario: {salary * 1.15:.2f}\n'
-              f'Reajuste ganho: {salary * 1.15 - salary:.2f}\n'
-              'Em percentual: 15 %')
+        print(
+            f'Novo salario: {salary * 1.15:.2f}\n'
+            f'Reajuste ganho: {salary * 1.15 - salary:.2f}\n'
+            'Em percentual: 15 %'
+        )
     elif salary <= 800.00:
-        print(f'Novo salario: {salary * 1.12:.2f}\n'
-              f'Reajuste ganho: {salary * 1.12 - salary:.2f}\n'
-              'Em percentual: 12 %')
+        print(
+            f'Novo salario: {salary * 1.12:.2f}\n'
+            f'Reajuste ganho: {salary * 1.12 - salary:.2f}\n'
+            'Em percentual: 12 %'
+        )
     elif salary <= 1200.00:
-        print(f'Novo salario: {salary * 1.1:.2f}\n'
-              f'Reajuste ganho: {salary * 1.1 - salary:.2f}\n'
-              'Em percentual: 10 %')
+        print(
+            f'Novo salario: {salary * 1.1:.2f}\n'
+            f'Reajuste ganho: {salary * 1.1 - salary:.2f}\n'
+            'Em percentual: 10 %'
+        )
     elif salary <= 2000.00:
-        print(f'Novo salario: {salary * 1.07:.2f}\n'
-              f'Reajuste ganho: {salary * 1.07 - salary:.2f}\n'
-              'Em percentual: 7 %')
+        print(
+            f'Novo salario: {salary * 1.07:.2f}\n'
+            f'Reajuste ganho: {salary * 1.07 - salary:.2f}\n'
+            'Em percentual: 7 %'
+        )
     else:
-        print(f'Novo salario: {salary * 1.04:.2f}\n'
-              f'Reajuste ganho: {salary * 1.04 - salary:.2f}\n'
-              'Em percentual: 4 %')
+        print(
+            f'Novo salario: {salary * 1.04:.2f}\n'
+            f'Reajuste ganho: {salary * 1.04 - salary:.2f}\n'
+            'Em percentual: 4 %'
+        )
