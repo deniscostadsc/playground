c4d9acba6e1dcc5da64df05bdfd55e069e7f8ab4
Denis Costa <deniscostadsc@gmail.com>
2023-03-07 09:51:26 -0300

    Improve input

--- solutions/beecrowd/1049/1049.cpp
+++ solutions/beecrowd/1049/1049.cpp
@@ -5,36 +5,34 @@
 int main() {
     std::string a, b, c;
 
-    std::cin >> a;
-    std::cin >> b;
-    std::cin >> c;
-
-    if (a == "vertebrado") {
-        if (b == "ave") {
-            if (c == "carnivoro") {
-                printf("aguia\n");
+    while (std::cin >> a && std::cin >> b && std::cin >> c) {
+        if (a == "vertebrado") {
+            if (b == "ave") {
+                if (c == "carnivoro") {
+                    printf("aguia\n");
+                } else {
+                    printf("pomba\n");
+                }
             } else {
-                printf("pomba\n");
+                if (c == "onivoro") {
+                    printf("homem\n");
+                } else {
+                    printf("vaca\n");
+                }
             }
         } else {
-            if (c == "onivoro") {
-                printf("homem\n");
+            if (b == "inseto") {
+                if (c == "hematofago") {
+                    printf("pulga\n");
+                } else {
+                    printf("lagarta\n");
+                }
             } else {
-                printf("vaca\n");
-            }
-        }
-    } else {
-        if (b == "inseto") {
-            if (c == "hematofago") {
-                printf("pulga\n");
-            } else {
-                printf("lagarta\n");
-            }
-        } else {
-            if (c == "hematofago") {
-                printf("sanguessuga\n");
-            } else {
-                printf("minhoca\n");
+                if (c == "hematofago") {
+                    printf("sanguessuga\n");
+                } else {
+                    printf("minhoca\n");
+                }
             }
         }
     }
