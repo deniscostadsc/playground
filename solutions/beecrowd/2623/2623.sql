6d6616a03604c9ded0ab59bf2692f8caacca8495
Denis Costa <deniscostadsc@gmail.com>
2023-05-10 21:52:14 -0300

    Fix lint

--- solutions/beecrowd/2623/2623.sql
+++ solutions/beecrowd/2623/2623.sql
@@ -2,5 +2,5 @@ SELECT products.name, categories.name
 FROM products
 JOIN categories ON products.id_categories = categories.id
 WHERE products.amount > 100
-AND categories.id in (1, 2, 3, 6, 9)
+AND categories.id IN (1, 2, 3, 6, 9)
 ORDER BY categories.id ASC;
