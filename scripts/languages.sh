fc3203fb110d9320fc1e0d5de6a02873b9e5ef5b
Denis Costa <deniscostadsc@gmail.com>
2023-09-27 17:56:03 -0300

    Fix find for language couting

--- scripts/languages.sh
+++ scripts/languages.sh
@@ -9,8 +9,26 @@ FOLDERS=$(find . -name 'problem.md' | sed 's/problem.md//g' | sort)
 for folder in $FOLDERS; do
     solutions="$(\
         find "$folder" \
-            -regex \
-            '.*\(c\|clj\|cpp\|cs\|dart\|go\|hs\|java\|js\|kt\|lua\|ml\|pas\|php\|py\|r\|rb\|rs\|scala\|sql\)' |\
+            -name '*.c' -o \
+            -name '*.clj' -o \
+            -name '*.cpp' -o \
+            -name '*.cs' -o \
+            -name '*.dart' -o \
+            -name '*.go' -o \
+            -name '*.hs' -o \
+            -name '*.java' -o \
+            -name '*.js' -o \
+            -name '*.kt' -o \
+            -name '*.lua' -o \
+            -name '*.ml' -o \
+            -name '*.pas' -o \
+            -name '*.php' -o \
+            -name '*.py' -o \
+            -name '*.r' -o \
+            -name '*.rb' -o \
+            -name '*.rs' -o \
+            -name '*.scala' -o \
+            -name '*.sql' | \
         grep -Ev 'drop-table.sql|schema.sql')"
     echo "$(echo "$solutions" | wc -w | sed 's/ //g') $folder"
 done | sort -nr
