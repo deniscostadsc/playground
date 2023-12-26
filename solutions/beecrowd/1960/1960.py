b21564233a54b513eac9d64fe6a3c170235cf7fd
Denis Costa <deniscostadsc@gmail.com>
2023-12-26 12:59:34 -0300

    Fix lint

--- solutions/beecrowd/1960/1960.py
+++ solutions/beecrowd/1960/1960.py
@@ -1,40 +1,40 @@
 import sys
 
 romans = {
-    0: "",
-    1: "I",
-    2: "II",
-    3: "III",
-    4: "IV",
-    5: "V",
-    6: "VI",
-    7: "VII",
-    8: "VIII",
-    9: "IX",
-    10: "X",
-    20: "XX",
-    30: "XXX",
-    40: "XL",
-    50: "L",
-    60: "LX",
-    70: "LXX",
-    80: "LXXX",
-    90: "XC",
-    100: "C",
-    200: "CC",
-    300: "CCC",
-    400: "CD",
-    500: "D",
-    600: "DC",
-    700: "DCC",
-    800: "DCCC",
-    900: "CM",
-    1000: "M",
+    0: '',
+    1: 'I',
+    2: 'II',
+    3: 'III',
+    4: 'IV',
+    5: 'V',
+    6: 'VI',
+    7: 'VII',
+    8: 'VIII',
+    9: 'IX',
+    10: 'X',
+    20: 'XX',
+    30: 'XXX',
+    40: 'XL',
+    50: 'L',
+    60: 'LX',
+    70: 'LXX',
+    80: 'LXXX',
+    90: 'XC',
+    100: 'C',
+    200: 'CC',
+    300: 'CCC',
+    400: 'CD',
+    500: 'D',
+    600: 'DC',
+    700: 'DCC',
+    800: 'DCCC',
+    900: 'CM',
+    1000: 'M',
 }
 
 for number in sys.stdin:
     number = int(number)
-    roman_result = ""
+    roman_result = ''
     multiplier = 1
 
     while number:
@@ -45,4 +45,3 @@ for number in sys.stdin:
         multiplier *= 10
 
     print(roman_result)
-
