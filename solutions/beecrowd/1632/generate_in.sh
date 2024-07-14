935a7e64870d9dd65a7ccc04ca42e62b1cd66561
Denis Costa <deniscostadsc@gmail.com>
2024-07-14 11:26:17 -0300

    Fix lint

--- solutions/beecrowd/1632/generate_in.sh
+++ solutions/beecrowd/1632/generate_in.sh
@@ -6,11 +6,11 @@ alphabet=(a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I 
 
 tests=4000
 
-echo $tests
+echo "${tests}"
 
-for _ in $(seq $tests); do
+for _ in $(seq "${tests}"); do
     n=$((RANDOM % 16 + 1))
-    for _ in $(seq $n); do
+    for _ in $(seq "${n}"); do
         echo -n "${alphabet[$((RANDOM % 52))]}"
     done
     echo ""
