07f1786cd13af3d5ae322482a7cecfd8c34155d0
Denis Costa <deniscostadsc@gmail.com>
2024-07-19 01:44:16 -0300

    Solve Conta de Água in python

--- solutions/beecrowd/2369/2369.py
+++ solutions/beecrowd/2369/2369.py
@@ -1,7 +1,7 @@
 import sys
 
-MAX_RANGE_1 = 9
-MAX_RANGE_2 = 29
+MAX_RANGE_1 = 10
+MAX_RANGE_2 = 30
 MAX_RANGE_3 = 100
 
 
@@ -10,23 +10,19 @@ for line in sys.stdin:
     total_bill = 0
 
     if n <= MAX_RANGE_1:
-        # print('if')
         total_bill = 7
     elif n <= MAX_RANGE_2:
         total_bill = 7 + (n - MAX_RANGE_1) * 1
-        # print(f'total_bill {total_bill}')
-        # print(f'n {n}')
-        # print(f'n - MAX_RANGE_1 {n - MAX_RANGE_1}')
     elif n <= MAX_RANGE_3:
         total_bill = (
-            7 + ((n - MAX_RANGE_1) * 1) + ((n - MAX_RANGE_1 - MAX_RANGE_2) * 2)
+            7 + ((MAX_RANGE_2 - MAX_RANGE_1) * 1) + ((n - MAX_RANGE_2) * 2)
         )
     else:
         total_bill = (
             7
-            + ((n - MAX_RANGE_1) * 1)
-            + ((n - MAX_RANGE_1 - MAX_RANGE_2) * 2)
-            + ((n - MAX_RANGE_1 - MAX_RANGE_2 - MAX_RANGE_3) * 5)
+            + ((MAX_RANGE_2 - MAX_RANGE_1) * 1)
+            + ((MAX_RANGE_3 - MAX_RANGE_2) * 2)
+            + ((n - MAX_RANGE_3) * 5)
         )
 
     print(total_bill)
