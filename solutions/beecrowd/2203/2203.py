dd74a5307e6f070cd22de5fa7d16527790ba9682
Denis Costa <deniscostadsc@gmail.com>
2024-07-22 18:18:10 -0300

    Solve Crowstorm in python

--- solutions/beecrowd/2203/2203.py
+++ solutions/beecrowd/2203/2203.py
@@ -3,8 +3,8 @@ from math import sqrt
 
 for line in sys.stdin:
     xf, yf, xi, yi, vi, r1, r2 = map(int, line.split())
-    distance = sqrt(pow(xi - xf, 2) + pow(yi - yf, 2))
-    if r1 + r2 > distance + 1.5 * vi:
+    distance = sqrt(pow(xi - xf, 2) + pow(yi - yf, 2)) + 1.5 * vi
+    if distance <= r1 + r2:
         print('Y')
     else:
         print('N')
