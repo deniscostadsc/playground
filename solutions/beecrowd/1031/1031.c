23f683c096ff4be598e36c0116c34d010339cd81
Denis Costa <deniscostadsc@gmail.com>
2024-07-23 17:11:08 -0300

    Solve Power Crisis in c

---
+++ solutions/beecrowd/1031/1031.c
@@ -0,0 +1,31 @@
+#include <stdint.h>
+#include <stdio.h>
+
+int16_t josephus(int16_t n, int16_t k) {
+    if (n == 1) {
+        return 1;
+    }
+    return ((josephus(n - 1, k) + k - 1) % n) + 1;
+}
+
+int main() {
+    int16_t n, m;
+
+    while (scanf("%d", &n) && n) {
+        m = 1;
+
+        /*
+        Why need '- 1' and it's 12 not 13?
+
+        Classic josephus function assume that sequence starts at 1, but shoots
+        at gap-th person. This version of problem allways shot 1 off first, and
+        then goes to gap-th number.
+        */
+        while (josephus(n - 1, m) != 12) {
+            m++;
+        }
+        printf("%d\n", m);
+    }
+
+    return 0;
+}
