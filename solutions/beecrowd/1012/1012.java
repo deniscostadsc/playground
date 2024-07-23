b76fda5ef39bd5a823d93c40397a995110af4ce9
Denis Costa <deniscostadsc@gmail.com>
2024-07-23 08:53:45 -0300

    Solve Area in java

---
+++ solutions/beecrowd/1012/1012.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner input = new Scanner (System.in);
+
+        float a, b, c;
+        double pi = 3.14159;
+
+        while (input.hasNextDouble()) {
+            a = input.nextFloat();
+            b = input.nextFloat();
+            c = input.nextFloat();
+
+            System.out.printf("TRIANGULO: %.3f\n", a * c / 2);
+            System.out.printf("CIRCULO: %.3f\n", pi * (c * c));
+            System.out.printf("TRAPEZIO: %.3f\n", c * ((a + b) / 2));
+            System.out.printf("QUADRADO: %.3f\n", b * b);
+            System.out.printf("RETANGULO: %.3f\n", a * b);
+        }
+    }
+}
