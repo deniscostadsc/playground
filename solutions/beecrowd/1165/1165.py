b96234ba7b5a39534608b8fe839335272f950030
Denis Costa <deniscostadsc@gmail.com>
2024-07-23 10:50:21 -0300

    Solve Prime Number in python

---
+++ solutions/beecrowd/1165/1165.py
@@ -0,0 +1,19 @@
+import math
+
+def is_prime(n):
+    if n == 2:
+        return True
+    elif n % 2 == 0 or n < 2:
+        return False
+    else:
+        for divisor in range(3, int(math.sqrt(n)) + 1, 2):
+            if n % divisor == 0:
+                return False
+    return True
+
+
+n = int(input())
+
+for _ in range(n):
+    x = int(input())
+    print(f"{x} eh primo" if is_prime(x) else f"{x} nao eh primo")
