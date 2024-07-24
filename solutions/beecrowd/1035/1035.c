edfeccbb213553c7e1ee8c34d0d513bd02cee29a
Denis Costa <deniscostadsc@gmail.com>
2024-07-24 10:08:23 -0300

    Solve Selection Test 1 in c

---
+++ solutions/beecrowd/1035/1035.c
@@ -0,0 +1,16 @@
+#include <stdint.h>
+#include <stdio.h>
+
+int main() {
+    int16_t a, b, c, d;
+
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+
+    if (b > c && d > a && c + d > a + b && c > 0 && d > 0 && a % 2 == 0) {
+        printf("Valores aceitos\n");
+    } else {
+        printf("Valores nao aceitos\n");
+    }
+
+    return 0;
+}
