8b57ac3dff684f3cd51bf9cce9bfcab1a3648d90
Denis Costa <deniscostadsc@gmail.com>
2024-07-29 16:04:37 -0300

    Solve Coordinates of a Point in python

---
+++ solutions/beecrowd/1041/1041.py
@@ -0,0 +1,20 @@
+import sys
+
+for line in sys.stdin:
+    x, y = map(float, line.split())
+    if not x and not y:
+        print("Origem")
+    elif not x and y:
+        print("Eixo Y")
+    elif x and not y:
+        print("Eixo X")
+    elif x > 0.0:
+        if y > 0.0:
+            print("Q1")
+        else:
+            print("Q4")
+    else:
+        if y > 0.0:
+            print("Q2")
+        else:
+            print("Q3")
