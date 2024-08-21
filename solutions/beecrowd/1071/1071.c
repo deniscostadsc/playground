4007b1fb42e64ee773e928b80dd743b10ca300d7
Denis Costa <deniscostadsc@gmail.com>
2024-08-21 19:30:46 -0300

    Solve Sum of Consecutive Odd Numbers I in c

---
+++ solutions/beecrowd/1071/1071.c
@@ -0,0 +1,29 @@
+#include <stdint.h>
+#include <stdio.h>
+
+int main() {
+    int64_t x, y, s;
+
+    while (scanf("%lld", &x) != EOF && scanf("%lld", &y) != EOF) {
+
+        if (x > y) {
+            int64_t temp = x;
+            x = y;
+            y = temp;
+        }
+
+        x++;
+        if (x % 2 == 0) {
+            x++;
+        }
+
+        s = 0;
+        for (int64_t i = x; i < y; i += 2) {
+            s += i;
+        }
+
+        printf("%d\n", s);
+    }
+
+    return 0;
+}
