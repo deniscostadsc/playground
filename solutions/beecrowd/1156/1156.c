448d3ba29334cf59d79a7487351a29e64edee92a
Denis Costa <deniscostadsc@gmail.com>
2024-08-22 16:44:36 -0300

    Solve S Sequence II in c

---
+++ solutions/beecrowd/1156/1156.c
@@ -0,0 +1,16 @@
+#include <stdint.h>
+#include <stdio.h>
+
+int main() {
+    double s = 1.0, x = 2.0;
+    int16_t i;
+
+    for (i = 3; i <= 39; i += 2) {
+        s += (double)(i / x);
+        x *= 2;
+    }
+
+    printf("%.2lf\n", s);
+
+    return 0;
+}
