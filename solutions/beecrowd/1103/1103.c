e96a9e9eed0c768cb76be72b2bbd0dd7b029c94e
Denis Costa <deniscostadsc@gmail.com>
2024-08-22 16:52:08 -0300

    Solve Alarm Clock in c

---
+++ solutions/beecrowd/1103/1103.c
@@ -0,0 +1,34 @@
+#include <stdint.h>
+#include <stdio.h>
+
+int main() {
+    int32_t hi, hf, mi, mf, m;
+
+    while (1) {
+        scanf("%d %d %d %d", &hi, &mi, &hf, &mf);
+
+        if (hi == 0 && hf == 0 && mi == 0 && mf == 0) {
+            break;
+        }
+
+        if (hi <= hf) {
+            m = (hf - hi) * 60;
+        } else {
+            m = (24 - (hi - hf)) * 60;
+        }
+
+        if (mi <= mf) {
+            m += mf - mi;
+        } else {
+            m += 60 - 60 - mi + mf;
+        }
+
+        if (hi == hf && mi > mf) {
+            m = 23 * 60 + (60 - mi + mf);
+        }
+
+        printf("%d\n", m);
+    }
+
+    return 0;
+}
