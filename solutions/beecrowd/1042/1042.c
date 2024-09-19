4c02a8a5835510136d5d292ab3f644821ca2aef7
Denis Costa <deniscostadsc@gmail.com>
2024-09-19 20:45:23 -0300

    Solve Simple Sort in c

---
+++ solutions/beecrowd/1042/1042.c
@@ -0,0 +1,37 @@
+#include <stdint.h>
+#include <stdio.h>
+
+int main() {
+    int16_t a, b, c, g, m, s, x;
+
+    scanf("%d %d %d", &a, &b, &c);
+
+    g = a;
+    m = b;
+    s = c;
+
+    if (g < m) {
+        x = g;
+        g = m;
+        m = x;
+    }
+    if (m < s) {
+        x = m;
+        m = s;
+        s = x;
+    }
+    if (g < m) {
+        x = g;
+        g = m;
+        m = x;
+    }
+
+    printf("%d\n", s);
+    printf("%d\n", m);
+    printf("%d\n\n", g);
+    printf("%d\n", a);
+    printf("%d\n", b);
+    printf("%d\n", c);
+
+    return 0;
+}
