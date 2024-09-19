6edcb1283cead6da2461567660dfe81ef7e695af
Denis Costa <deniscostadsc@gmail.com>
2024-09-19 20:48:58 -0300

    Solve Triangle Types in c

---
+++ solutions/beecrowd/1045/1045.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+    float a, b, c, x;
+    while (scanf("%f %f %f", &a, &b, &c) != EOF) {
+        if (a < b) {
+            x = a;
+            a = b;
+            b = x;
+        }
+        if (b < c) {
+            x = b;
+            b = c;
+            c = x;
+        }
+        if (a < b) {
+            x = a;
+            a = b;
+            b = x;
+        }
+
+        if (a >= b + c) {
+            printf("NAO FORMA TRIANGULO\n");
+            continue;
+        } else if (a * a == b * b + c * c) {
+            printf("TRIANGULO RETANGULO\n");
+        } else if (a * a > b * b + c * c) {
+            printf("TRIANGULO OBTUSANGULO\n");
+        } else if (a * a < b * b + c * c) {
+            printf("TRIANGULO ACUTANGULO\n");
+        }
+
+        if (a == b && b == c) {
+            printf("TRIANGULO EQUILATERO\n");
+        } else if (a == b || b == c) {
+            printf("TRIANGULO ISOSCELES\n");
+        }
+    }
+
+    return 0;
+}
