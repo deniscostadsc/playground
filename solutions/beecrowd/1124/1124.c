a8898e75beb950e82d5a28f0ae676384ffd4458d
Denis Costa <deniscostadsc@gmail.com>
2024-09-20 10:37:56 -0300

    Fix lint

--- solutions/beecrowd/1124/1124.c
+++ solutions/beecrowd/1124/1124.c
@@ -6,8 +6,10 @@ int main() {
     int32_t width, length, r1, r2, x_r1, x_r2, y_r1, y_r2,
         distance_betweeen_circle_centers;
 
-    while (scanf("%d %d %d %d", &width, &length, &r1, &r2) && width && length && r1 && r2) {
-        if (width < r1 * 2 || width < r2 * 2 || length < r1 * 2 || length < r2 * 2) {
+    while (scanf("%d %d %d %d", &width, &length, &r1, &r2) && width && length
+           && r1 && r2) {
+        if (width < r1 * 2 || width < r2 * 2 || length < r1 * 2
+            || length < r2 * 2) {
             puts("N");
             continue;
         }
