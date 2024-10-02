679edef234b946fd208d099f269668183c6afaa1
Denis Costa <deniscostadsc@gmail.com>
2024-10-02 04:33:02 -0300

    Improve inout/output files

--- solutions/beecrowd/1061/1061.cpp
+++ solutions/beecrowd/1061/1061.cpp
@@ -1,38 +1,38 @@
 #include <cstdint>
-#include <cstdio>
+#include <iostream>
 
 int main() {
     std::int32_t d, dd, h, hh, m, mm, s, ss;
 
-    scanf("Dia %d", &d);
-    scanf("%d : %d : %d\n", &h, &m, &s);
-    scanf("Dia %d", &dd);
-    scanf("%d : %d : %d", &hh, &mm, &ss);
+    while (scanf("Dia %d\n", &d) != EOF) {
+        scanf("%d : %d : %d\n", &h, &m, &s);
+        scanf("Dia %d\n", &dd);
+        scanf("%d : %d : %d\n", &hh, &mm, &ss);
 
-    s = ss - s;
-    m = mm - m;
-    h = hh - h;
-    d = dd - d;
+        s = ss - s;
+        m = mm - m;
+        h = hh - h;
+        d = dd - d;
 
-    if (s < 0) {
-        s += 60;
-        m--;
+        if (s < 0) {
+            s += 60;
+            m--;
+        }
+
+        if (m < 0) {
+            m += 60;
+            h--;
+        }
+
+        if (h < 0) {
+            h += 24;
+            d--;
+        }
+
+        printf("%d dia(s)\n", d);
+        printf("%d hora(s)\n", h);
+        printf("%d minuto(s)\n", m);
+        printf("%d segundo(s)\n", s);
     }
-
-    if (m < 0) {
-        m += 60;
-        h--;
-    }
-
-    if (h < 0) {
-        h += 24;
-        d--;
-    }
-
-    printf("%d dia(s)\n", d);
-    printf("%d hora(s)\n", h);
-    printf("%d minuto(s)\n", m);
-    printf("%d segundo(s)\n", s);
-
     return 0;
 }
---
+++ solutions/beecrowd/1061/generate_in.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+TESTCASES=500
+CURRENT_PATH=$(dirname -- "${BASH_SOURCE[0]}")
+
+for i in $(seq "${TESTCASES}"); do
+    first_day=$((RANDOM % 15 + 1))
+    second_day=$((RANDOM % 15 + 15))
+    while [[ ${second_day} -lt ${first_day} ]]; do
+        second_day=$((RANDOM % 15 + 15))
+    done
+    second_hours=$((RANDOM % 12))
+    second_minutes=$((RANDOM % 60))
+    second_seconds=$((RANDOM % 60))
+    if [[ ${second_day} -eq ${first_day} ]]; then
+        first_hours=$((RANDOM % 6))
+        while [[ ${second_hours} -lt ${first_hours} ]]; do
+            second_hours=$((RANDOM % 12))
+        done
+        first_minutes=$((RANDOM % 30))
+        while [[ ${second_minutes} -lt ${first_minutes} ]]; do
+            second_minutes=$((RANDOM % 60))
+        done
+        first_seconds=$((RANDOM % 30))
+        while [[ ${second_seconds} -lt ${first_seconds} ]]; do
+            second_seconds=$((RANDOM % 60))
+        done
+    else
+        first_hours=$((RANDOM % 12))
+        first_minutes=$((RANDOM % 60))
+        first_seconds=$((RANDOM % 60))
+    fi
+    # echo "Dia ${first_day}" > "${CURRENT_PATH}/in-$(printf "%03d" "${i}")".txt
+    # echo "${first_hours} : ${first_minutes} : ${first_seconds}" >> "${CURRENT_PATH}/in-$(printf "%03d" "${i}")".txt
+    # echo "Dia ${second_day}" >> "${CURRENT_PATH}/in-$(printf "%03d" "${i}")".txt
+    # echo "${second_hours} : ${second_minutes} : ${second_seconds}" >> "${CURRENT_PATH}/in-$(printf "%03d" "${i}")".txt
+
+    {
+    echo "Dia ${first_day}"
+    echo "${first_hours} : ${first_minutes} : ${first_seconds}"
+    echo "Dia ${second_day}"
+    echo "${second_hours} : ${second_minutes} : ${second_seconds}"
+    } > "${CURRENT_PATH}/in-$(printf "%03d" "${i}")".txt
+done
+
+
