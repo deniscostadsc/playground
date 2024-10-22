22243322af36c8dd4b36538ecf3c483d5edca185
Denis Costa <deniscostadsc@gmail.com>
2024-10-22 18:23:08 -0300

    Solve Flavious Josephus Legend in clojure

---
+++ solutions/beecrowd/1030/1030.clj
@@ -0,0 +1,26 @@
+(ns main
+  (:require [clojure.string :as str]))
+
+(defn josephus [n k]
+  (loop [current-n 2
+         result 1]
+    (if
+     (> current-n n)
+      result
+      (recur (inc current-n)
+             (-> result
+                 (+ k)
+                 dec
+                 (mod current-n)
+                 (inc))))))
+
+(defn main []
+  (loop [nc (Integer/parseInt (read-line))
+         index 1]
+    (let [[n k] (->> (str/split (read-line) #" ")
+                     (map #(Integer/parseInt %)))]
+      (printf "Case %d: %d%n" index (josephus n k))
+      (when (< index nc)
+        (recur nc (inc index))))))
+
+(main)
