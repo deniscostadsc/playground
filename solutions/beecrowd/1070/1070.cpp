566282355c999f730d3a33bb6569ee50f4a99992
Denis Costa <deniscostadsc@gmail.com>
2024-11-12 17:55:38 -0300

    Improve input/output files

--- solutions/beecrowd/1070/1070.cpp
+++ solutions/beecrowd/1070/1070.cpp
@@ -4,13 +4,13 @@
 int main() {
     std::int16_t n;
 
-    std::cin >> n;
-
-    if (n % 2 == 0) {
-        n++;
-    }
-    for (std::int16_t i = 0; i <= 5; i++, n += 2) {
-        std::cout << n << std::endl;
+    while (std::cin >> n) {
+        if (n % 2 == 0) {
+            n++;
+        }
+        for (std::int16_t i = 0; i <= 5; i++, n += 2) {
+            std::cout << n << std::endl;
+        }
     }
 
     return 0;
