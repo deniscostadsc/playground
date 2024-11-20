8ba3ea7879ab77dda63e8ef733c23dd16b82f1b1
Denis Costa <deniscostadsc@gmail.com>
2024-11-20 12:43:23 -0300

    Solve Building Houses in python

---
+++ solutions/beecrowd/1541/1541.py
@@ -0,0 +1,9 @@
+import math
+import sys
+
+for line in sys.stdin:
+    if line.startswith('0'):
+        break
+
+    a, b, c = map(int, line.split())
+    print(f"{math.floor(math.sqrt((100 * a * b) / c))}")
