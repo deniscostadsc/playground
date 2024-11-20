9abade22780ff6251b2738b3ddbaf6dc43c524f1
Denis Costa <deniscostadsc@gmail.com>
2024-11-20 12:37:00 -0300

    Solve Array 123 in python

---
+++ solutions/beecrowd/1534/1534.py
@@ -0,0 +1,15 @@
+import sys
+
+for line in sys.stdin:
+    n = int(line)
+    for i in range(n):
+        for j in range(n):
+            if i == j and i + j ==  n - 1:
+                print(2, end="")
+            elif i == j:
+                print(1, end="")
+            elif i + j == n - 1:
+                print(2, end="")
+            else:
+                print(3, end="")
+        print()
