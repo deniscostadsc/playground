e298ba2b4819e228784bfa65a51b2a3b0854958a
Denis Costa <deniscostadsc@gmail.com>
2024-11-20 19:09:10 -0300

    Solve Month in python

---
+++ solutions/beecrowd/1052/1052.py
@@ -0,0 +1,19 @@
+import sys
+
+months = {
+    1: 'January',
+    2: 'February',
+    3: 'March',
+    4: 'April',
+    5: 'May',
+    6: 'June',
+    7: 'July',
+    8: 'August',
+    9: 'September',
+    10: 'October',
+    11: 'November',
+    12: 'December',
+}
+
+for line in sys.stdin:
+    print(months[int(line)])
