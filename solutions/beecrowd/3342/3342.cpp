c6735776030d883e0990375217917d72f5000154
Denis Costa <deniscostadsc@gmail.com>
2024-11-21 11:53:56 -0300

    Solve Keanu in clojure

---
+++ solutions/beecrowd/3342/3342.cpp
@@ -0,0 +1,19 @@
+#include <cstdint>
+#include <iostream>
+
+int main() {
+    int16_t n;
+    while (std::cin >> n) {
+        int16_t black_squares = n * n / 2;
+        int16_t white_squares = black_squares;
+
+        if (!(n % 2 == 0)) {
+            white_squares++;
+        }
+
+        std::cout << white_squares << " casas brancas e ";
+        std::cout << black_squares << " casas pretas" << std::endl;
+    }
+
+    return 0;
+}
