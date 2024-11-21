10efe65fb0d6f043d64efd992beb49248428fdda
Denis Costa <deniscostadsc@gmail.com>
2024-11-21 10:44:03 -0300

    Solve Month in clojure

---
+++ solutions/beecrowd/1052/1052.clj
@@ -0,0 +1,24 @@
+(ns main)
+
+(defn print-month [index]
+  (let [months {1 "January"
+                2 "February",
+                3 "March",
+                4 "April",
+                5 "May",
+                6 "June",
+                7 "July",
+                8 "August",
+                9 "September"
+                10 "October",
+                11 "November",
+                12 "December"}]
+    (println (get months index))))
+
+(defn main []
+  (loop [line (read-line)]
+    (when line
+      (print-month (Integer/parseInt line))
+      (recur (read-line)))))
+
+(main)
