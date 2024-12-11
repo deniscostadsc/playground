1a8f5def4514c3bb54afafd3d613041803bb6a70
Denis Costa <deniscostadsc@gmail.com>
2024-12-11 11:34:20 -0300

    Solve Divisors I in python

---
+++ solutions/beecrowd/1157/1157.py
@@ -0,0 +1,20 @@
+import sys
+import math
+import heapq
+
+for line in sys.stdin:
+    number = int(line)
+    divisors = []
+    found_divisors = set()
+
+    for possible_divisor in range(1, int(math.sqrt(number)) + 1):
+        if number % possible_divisor == 0:
+            found_divisors.add(possible_divisor)
+            heapq.heappush(divisors, possible_divisor)
+
+            if number // possible_divisor not in found_divisors:
+                heapq.heappush(divisors, number // possible_divisor)
+                found_divisors.add(number // possible_divisor)
+
+    while divisors:
+        print(heapq.heappop(divisors))
