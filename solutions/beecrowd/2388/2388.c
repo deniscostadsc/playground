f8c6cd0c92e1f135189d94e9a2f9ea86d32104fa
Denis Costa <deniscostadsc@gmail.com>
2024-12-18 12:54:01 -0300

    Solve Tacógrafo in c

---
+++ solutions/beecrowd/2388/2388.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdint.h>
+
+int main() {
+    int32_t distance;
+    int16_t n, t, v;
+
+    while (scanf("%d", &n) != EOF) {
+        distance = 0;
+        for (int16_t i = 0; i < n; i++) {
+            scanf("%d %d", &t, &v);
+            distance += t * v;
+        }
+        printf("%d\n", distance);
+    }
+    return 0;
+}
