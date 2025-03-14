1604db4df8931ac1c8c6440cd7418933798d957a
Denis Costa <deniscostadsc@gmail.com>
2025-03-14 16:59:00 -0300

    Solve Exceeding Z in c

---
+++ solutions/beecrowd/1150/1150.c
@@ -0,0 +1,20 @@
+#include <stdint.h>
+#include <stdio.h>
+
+int main() {
+    int16_t x, s = 0, z, n = 0;
+
+    scanf("%d", &x);
+
+    while (scanf("%d", &z) && z <= x) {
+    }
+
+    for (int16_t i = x; s <= z; i++) {
+        s += x++;
+        n++;
+    }
+
+    printf("%d\n", n);
+
+    return 0;
+}
