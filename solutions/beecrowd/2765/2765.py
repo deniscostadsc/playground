5f38387fb07a2e6122ed02249023d35f49893b8e
Denis Costa <deniscostadsc@gmail.com>
2025-03-28 12:36:03 -0300

    Add comment

--- solutions/beecrowd/2765/2765.py
+++ solutions/beecrowd/2765/2765.py
@@ -1,6 +1,7 @@
 import sys
 
 for sentence in sys.stdin:
+    # we can't use strip because we ned to keep trailing/leading space
     splited_sentence = sentence.replace('\n', '').split(',')
     print(splited_sentence[0])
     print(','.join(splited_sentence[1:]))
