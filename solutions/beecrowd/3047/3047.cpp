515265735b0ef21407dfa8e7b21c123d2c7be8d8
Denis Costa <deniscostadsc@gmail.com>
2025-04-02 19:02:05 -0300

    Solve The age of Mrs. Monica in c++

---
+++ solutions/beecrowd/3047/3047.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    std::int16_t monica_age, first_son, second_son, monica_minus_children;
+
+    while (std::cin >> monica_age) {
+        std::cin >> first_son;
+        std::cin >> second_son;
+        monica_minus_children = monica_age - first_son - second_son;
+
+        std::cout << std::max({first_son, second_son, monica_minus_children}) << std::endl;
+    }
+
+    return 0;
+}
