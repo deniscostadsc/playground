8944d2864e869711c631b08dd3aa924cf0fab3b8
Denis Costa <deniscostadsc@gmail.com>
2025-04-03 16:53:45 -0300

    Solve Chocolate Factory in c++

---
+++ solutions/beecrowd/1573/1573.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <iostream>
+
+int main() {
+    int16_t a, b, c;
+
+    while (std::cin >> a >> b >> c) {
+        if (!a) {
+            break;
+        }
+
+        std::cout << static_cast< int16_t >(std::pow(a * b * c, 1.0 / 3.0))
+                  << std::endl;
+    }
+    return 0;
+}
