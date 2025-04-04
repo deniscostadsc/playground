5d9325316ec1af2928182e5c9a1e003bfda93046
Denis Costa <deniscostadsc@gmail.com>
2025-04-04 11:01:52 -0300

    Solve Output 6 in c++

---
+++ solutions/beecrowd/2752/2752.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    std::cout << "<AMO FAZER EXERCICIO NO URI>" << std::endl;
+    std::cout << "<    AMO FAZER EXERCICIO NO URI>" << std::endl;
+    std::cout << "<AMO FAZER EXERCICIO >" << std::endl;
+    std::cout << "<AMO FAZER EXERCICIO NO URI>" << std::endl;
+    std::cout << "<AMO FAZER EXERCICIO NO URI    >" << std::endl;
+    std::cout << "<AMO FAZER EXERCICIO NO URI>" << std::endl;
+    std::cout << "<          AMO FAZER EXERCICIO >" << std::endl;
+    std::cout << "<AMO FAZER EXERCICIO           >" << std::endl;
+
+    return 0;
+}
