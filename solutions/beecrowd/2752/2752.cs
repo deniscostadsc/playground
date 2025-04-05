ec8f2ad6768c72c00c00607a14f647f956290be0
Denis Costa <deniscostadsc@gmail.com>
2025-04-05 07:16:55 -0300

    Solve Output 6 in c#

---
+++ solutions/beecrowd/2752/2752.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Solution {
+    static void Main() {
+        Console.WriteLine("<AMO FAZER EXERCICIO NO URI>");
+        Console.WriteLine("<    AMO FAZER EXERCICIO NO URI>");
+        Console.WriteLine("<AMO FAZER EXERCICIO >");
+        Console.WriteLine("<AMO FAZER EXERCICIO NO URI>");
+        Console.WriteLine("<AMO FAZER EXERCICIO NO URI    >");
+        Console.WriteLine("<AMO FAZER EXERCICIO NO URI>");
+        Console.WriteLine("<          AMO FAZER EXERCICIO >");
+        Console.WriteLine("<AMO FAZER EXERCICIO           >");
+    }
+}
