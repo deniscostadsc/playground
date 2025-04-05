babc78774f7dbdda48dd586a37a984b4c23f7598
Denis Costa <deniscostadsc@gmail.com>
2025-04-05 08:05:05 -0300

    Solve Output 6 in go

---
+++ solutions/beecrowd/2752/2752.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("<AMO FAZER EXERCICIO NO URI>")
+	fmt.Println("<    AMO FAZER EXERCICIO NO URI>")
+	fmt.Println("<AMO FAZER EXERCICIO >")
+	fmt.Println("<AMO FAZER EXERCICIO NO URI>")
+	fmt.Println("<AMO FAZER EXERCICIO NO URI    >")
+	fmt.Println("<AMO FAZER EXERCICIO NO URI>")
+	fmt.Println("<          AMO FAZER EXERCICIO >")
+	fmt.Println("<AMO FAZER EXERCICIO           >")
+}
