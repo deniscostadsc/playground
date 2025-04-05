9dbc54cee38dd8c10336784a5a420d5861853bfb
Denis Costa <deniscostadsc@gmail.com>
2025-04-05 07:09:35 -0300

    Solve Output 6 in c

---
+++ solutions/beecrowd/2752/2752.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    puts("<AMO FAZER EXERCICIO NO URI>");
+    puts("<    AMO FAZER EXERCICIO NO URI>");
+    puts("<AMO FAZER EXERCICIO >");
+    puts("<AMO FAZER EXERCICIO NO URI>");
+    puts("<AMO FAZER EXERCICIO NO URI    >");
+    puts("<AMO FAZER EXERCICIO NO URI>");
+    puts("<          AMO FAZER EXERCICIO >");
+    puts("<AMO FAZER EXERCICIO           >");
+
+    return 0;
+}
