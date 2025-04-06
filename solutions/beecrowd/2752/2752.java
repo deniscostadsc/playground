fcfdbe601fd71f8ffaf413a79a4ce236f9f7713d
Denis Costa <deniscostadsc@gmail.com>
2025-04-05 22:12:34 -0300

    Solve Output 6 in java

---
+++ solutions/beecrowd/2752/2752.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String args[]) {
+        System.out.println("<AMO FAZER EXERCICIO NO URI>");
+        System.out.println("<    AMO FAZER EXERCICIO NO URI>");
+        System.out.println("<AMO FAZER EXERCICIO >");
+        System.out.println("<AMO FAZER EXERCICIO NO URI>");
+        System.out.println("<AMO FAZER EXERCICIO NO URI    >");
+        System.out.println("<AMO FAZER EXERCICIO NO URI>");
+        System.out.println("<          AMO FAZER EXERCICIO >");
+        System.out.println("<AMO FAZER EXERCICIO           >");
+    }
+}
