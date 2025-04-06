0dad6889eea862db89ce08e5c43fdad371a3db2d
Denis Costa <deniscostadsc@gmail.com>
2025-04-06 11:37:14 -0300

    Solve Output 6 in scala

---
+++ solutions/beecrowd/2752/2752.scala
@@ -0,0 +1,12 @@
+object Main {
+    def main(args: Array[String]) {
+        println("<AMO FAZER EXERCICIO NO URI>")
+        println("<    AMO FAZER EXERCICIO NO URI>")
+        println("<AMO FAZER EXERCICIO >")
+        println("<AMO FAZER EXERCICIO NO URI>")
+        println("<AMO FAZER EXERCICIO NO URI    >")
+        println("<AMO FAZER EXERCICIO NO URI>")
+        println("<          AMO FAZER EXERCICIO >")
+        println("<AMO FAZER EXERCICIO           >")
+    }
+}
