8fe7296a3255250a3904432b5e98e9154fd0e8ba
Denis Costa <deniscostadsc@gmail.com>
2025-04-06 11:31:41 -0300

    Solve Output 6 in rust

---
+++ solutions/beecrowd/2752/2752.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("<AMO FAZER EXERCICIO NO URI>");
+    println!("<    AMO FAZER EXERCICIO NO URI>");
+    println!("<AMO FAZER EXERCICIO >");
+    println!("<AMO FAZER EXERCICIO NO URI>");
+    println!("<AMO FAZER EXERCICIO NO URI    >");
+    println!("<AMO FAZER EXERCICIO NO URI>");
+    println!("<          AMO FAZER EXERCICIO >");
+    println!("<AMO FAZER EXERCICIO           >");
+}
