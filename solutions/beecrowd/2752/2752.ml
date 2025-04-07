7a51244ecc09462c0d99bfb37d68ad51d650eb25
Denis Costa <deniscostadsc@gmail.com>
2025-04-07 11:22:57 -0300

    Solve Output 6 in ocaml

---
+++ solutions/beecrowd/2752/2752.ml
@@ -0,0 +1,8 @@
+print_string "<AMO FAZER EXERCICIO NO URI>\n";
+print_string "<    AMO FAZER EXERCICIO NO URI>\n";
+print_string "<AMO FAZER EXERCICIO >\n";
+print_string "<AMO FAZER EXERCICIO NO URI>\n";
+print_string "<AMO FAZER EXERCICIO NO URI    >\n";
+print_string "<AMO FAZER EXERCICIO NO URI>\n";
+print_string "<          AMO FAZER EXERCICIO >\n";
+print_string "<AMO FAZER EXERCICIO           >\n";
