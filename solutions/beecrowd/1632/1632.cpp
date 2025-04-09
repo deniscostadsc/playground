70c4f86bebc6ee306a103ea7bcf509f31121c5ad
Denis Costa <deniscostadsc@gmail.com>
2025-04-09 09:23:17 -0300

    Solve Variations in c++

---
+++ solutions/beecrowd/1632/1632.cpp
@@ -0,0 +1,30 @@
+#include <cstdint>
+#include <iostream>
+#include <set>
+#include <string>
+
+int main() {
+    int16_t t;
+    int32_t variations;
+    std::string line;
+    std::set< char > letters_with_three_options
+        = {'a', 'e', 'i', 'o', 's', 'A', 'E', 'I', 'O', 'S'};
+
+    std::cin >> t;
+    for (int16_t i = 0; i < t; i++) {
+        std::cin >> line;
+        variations = 1;
+
+        for (char &c : line) {
+            if (letters_with_three_options.contains(c)) {
+                variations *= 3;
+            } else {
+                variations *= 2;
+            }
+        }
+
+        std::cout << variations << std::endl;
+    }
+
+    return 0;
+}
