b7d0011653835a771047fb01978647a549c39e1b
Denis Costa <deniscostadsc@gmail.com>
2025-06-14 18:52:31 -0300

    Improve input file

--- solutions/beecrowd/2203/generate_in.sh
+++ solutions/beecrowd/2203/generate_in.sh
@@ -2,13 +2,13 @@
 
 set -euo pipefail
 
-for i in $(seq 0 19 100); do
-    for j in $(seq 0 19 100); do
-        for k in $(seq 0 19 100); do
-            for l in $(seq 0 19 100); do
-                for m in $(seq 0 19 100); do
-                    for n in $(seq 0 19 100); do
-                        for o in $(seq 0 19 100); do
+for i in $(seq 0 5); do
+    for j in $(seq 0 5); do
+        for k in $(seq 0 5); do
+            for l in $(seq 0 5); do
+                for m in $(seq 0 5); do
+                    for n in $(seq 0 5); do
+                        for o in $(seq 0 5); do
                             echo "${i} ${j} ${k} ${l} ${m} ${n} ${o}"
                         done
                     done
