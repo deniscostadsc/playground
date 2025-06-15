10f25009aecf01c116bab61837add28e903912de
Denis Costa <deniscostadsc@gmail.com>
2025-06-15 11:54:01 -0300

    Solve Output 7 in java

---
+++ solutions/beecrowd/2753/2753.java
@@ -0,0 +1,9 @@
+import java.lang.Character;
+
+public class Main {
+    public static void main(String args[]) {
+        for  (int number = 97; number <= 122; number++) {
+            System.out.printf("%d e %c\n", number, (char)number);
+        }
+    }
+}
