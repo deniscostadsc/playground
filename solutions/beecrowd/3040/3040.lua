478a2bd6d06f557104859a91fadd84e1ae6460ee
Denis Costa <deniscostadsc@gmail.com>
2025-06-15 13:21:08 -0300

    Solve The Christmas Tree in lua

---
+++ solutions/beecrowd/3040/3040.lua
@@ -0,0 +1,20 @@
+function isTreeValid (height, diameter, branches)
+    return height >= 200 and height <= 300 and diameter >= 50 and branches >= 150
+end
+
+local n = tonumber(io.read("*n"))
+
+for _=0,n do
+    local line = io.read()
+    for height, diameter, branches in string.gmatch(line, "(%w+) (%w+) (%w+)") do
+        print(
+            isTreeValid(
+                tonumber(height),
+                tonumber(diameter),
+                tonumber(branches)
+            )
+            and "Sim"
+            or "Nao"
+        )
+    end
+end
