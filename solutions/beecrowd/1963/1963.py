c5c7bacdfd17ae4f9632242c05c9d6995b47f712
Denis Costa <deniscostadsc@gmail.com>
2025-08-23 20:10:48 -0300

    Debug

--- solutions/beecrowd/1963/1963.py
+++ solutions/beecrowd/1963/1963.py
@@ -2,4 +2,4 @@ import sys
 
 for line in sys.stdin:
     price1, price2 = map(float, line.split())
-    print(f"{100 * (price2 - price1) / price1:.2f}%")
+    print(f'{100 * (price2 - price1) / price1:.2f}%')
