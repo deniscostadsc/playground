3d2949012aa33898ff3dd79f45b621d324ecda42
Denis Costa <deniscostadsc@gmail.com>
2025-08-24 16:05:28 -0300

    Update github-action scripts

--- solutions/beecrowd/1116/1116.py
+++ solutions/beecrowd/1116/1116.py
@@ -3,6 +3,6 @@ n = input()
 for i in range(int(n)):
     a, b = map(float, input().split())
     if b == 0:
-        print("divisao impossivel")
+        print('divisao impossivel')
     else:
         print(a / b)
