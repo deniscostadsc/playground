70ef2e3518938b639d4f9cba75dea5e1270a58dd
Denis Costa <deniscostadsc@gmail.com>
2025-08-24 01:29:34 -0300

    Fix lint

--- scripts/lint/lint-sql.sh
+++ scripts/lint/lint-sql.sh
@@ -4,16 +4,16 @@ set -euo pipefail
 
 echo "sql"
 
-if [ "${LINT_FIX:-}" = "1" ]; then
+if [[ "${LINT_FIX:-}" = "1" ]]; then
     find . -name '*.sql' -exec sqlformat --indent_width=4 -k upper -o {} {} \;
 else
-    for file in $(find . -name '*.sql'); do
-        sqlformat --indent_width=4 -k upper "$file" > "${file}.formatted" && \
-        if ! diff -q "$file" "${file}.formatted" >/dev/null; then
-            echo "SQL formatting issues found in $file"
-            rm "${file}.formatted"
-            exit 1
-        fi
+    while IFS= read -r -d '' file; do
+        sqlformat --indent_width=4 -k upper "$file" >"${file}.formatted" &&
+            if ! diff -q "$file" "${file}.formatted" >/dev/null; then
+                echo "SQL formatting issues found in $file"
+                rm "${file}.formatted"
+                exit 1
+            fi
         rm "${file}.formatted"
-    done
+    done < <(find . -name '*.sql' -print0)
 fi
