8296597764f832137505867ac659ae38a52fab46
Denis Costa <deniscostadsc@gmail.com>
2025-09-01 16:28:23 -0300

    Solve Output 8 in c++

---
+++ solutions/beecrowd/2754/2754.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    std::cout << "234.345000 - 45.698000" << std::endl;
+    std::cout << "234 - 46" << std::endl;
+    std::cout << "234.3 - 45.7" << std::endl;
+    std::cout << "234.34 - 45.70" << std::endl;
+    std::cout << "234.345 - 45.698" << std::endl;
+    std::cout << "2.343450e+02 - 4.569800e+01" << std::endl;
+    std::cout << "2.343450E+02 - 4.569800E+01" << std::endl;
+    std::cout << "234.345 - 45.698" << std::endl;
+    std::cout << "234.345 - 45.698" << std::endl;
+
+    return 0;
+}
