685fbe26fc31a4c4b92cad44cbbec2a929d9569c
Denis Costa <deniscostadsc@gmail.com>
2025-09-01 12:10:05 -0300

    Solve How much earn a Doctor? in sql

--- solutions/beecrowd/2994/2994.sql
+++ solutions/beecrowd/2994/2994.sql
@@ -1,5 +1,8 @@
-SELECT doctor.name, ( ( 1 * 150 ) + 1 ) + ( ( 2 * 150 ) + 15 ) FROM attendances
+SELECT
+    doctors.name,
+    ROUND(SUM(attendances.hours * 150 * (1 + work_shifts.bonus / 100)), 1) as salary
+FROM attendances
 JOIN doctors ON doctors.id = attendances.id_doctor
 JOIN work_shifts ON work_shifts.id = attendances.id_work_shift
-GROUP BY attendances.id
-ORDER BY doctors.name;
+GROUP BY doctors.name
+ORDER BY salary DESC;
