135c40e4b8468d9334b5319f0dfcc41458ef23d8
Denis Costa <deniscostadsc@gmail.com>
2025-09-01 16:51:43 -0300

    Solve Output 8 in java

---
+++ solutions/beecrowd/2754/2754.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("234.345000 - 45.698000");
+        System.out.println("234 - 46");
+        System.out.println("234.3 - 45.7");
+        System.out.println("234.34 - 45.70");
+        System.out.println("234.345 - 45.698");
+        System.out.println("2.343450e+02 - 4.569800e+01");
+        System.out.println("2.343450E+02 - 4.569800E+01");
+        System.out.println("234.345 - 45.698");
+        System.out.println("234.345 - 45.698");
+    }
+}
