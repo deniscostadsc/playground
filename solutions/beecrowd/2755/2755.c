4018dbc6f6decd79e196143c961e5cc038a28c37
Denis Costa <deniscostadsc@gmail.com>
2025-09-14 18:31:03 -0300

    Solve Output 9 in c

---
+++ solutions/beecrowd/2755/2755.c
@@ -0,0 +1,11 @@
+#include <stdint.h>
+#include <stdio.h>
+
+int main() {
+    printf("\"Ro'b'er\tto\\/\"\n");
+    printf("(._.) ( l: ) ( .-. ) ( :l ) (._.)\n");
+    printf("(^_-) (-_-) (-_^)\n");
+    printf("(\"_\") ('.')\n");
+
+    return 0;
+}
