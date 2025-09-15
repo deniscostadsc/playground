17055420fb0d2032aacad6ac099734ddc5484481
Denis Costa <deniscostadsc@gmail.com>
2025-09-15 08:13:00 -0300

    Solve Output 9 in c#

---
+++ solutions/beecrowd/2755/2755.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Solution {
+    static void Main() {
+        Console.WriteLine("\"Ro'b'er\tto\\/\"");
+        Console.WriteLine("(._.) ( l: ) ( .-. ) ( :l ) (._.)");
+        Console.WriteLine("(^_-) (-_-) (-_^)");
+        Console.WriteLine("(\"_\") ('.')");
+    }
+}
