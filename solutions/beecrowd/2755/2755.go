37e888d574b53e4483a02bb59aa132fd34a3fe7e
Denis Costa <deniscostadsc@gmail.com>
2025-09-15 09:31:03 -0300

    Solve Output 9 in go

---
+++ solutions/beecrowd/2755/2755.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("\"Ro'b'er\tto\\/\"")
+	fmt.Println("(._.) ( l: ) ( .-. ) ( :l ) (._.)")
+	fmt.Println("(^_-) (-_-) (-_^)")
+	fmt.Println("(\"_\") ('.')")
+}
