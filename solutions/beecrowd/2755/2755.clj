3c08f573a1849d0e4d6052e014a463ad663a42a4
Denis Costa <deniscostadsc@gmail.com>
2025-09-16 08:13:20 -0300

    Solve Output 9 in clojure

---
+++ solutions/beecrowd/2755/2755.clj
@@ -0,0 +1,9 @@
+(ns main)
+
+(defn main []
+  (println "\"Ro'b'er\tto\\/\"")
+  (println "(._.) ( l: ) ( .-. ) ( :l ) (._.)")
+  (println "(^_-) (-_-) (-_^)")
+  (println "(\"_\") ('.')"))
+
+(main)
