b695b2b3f6269091eac4fc35147489e044edfde0
Denis Costa <deniscostadsc@gmail.com>
2025-09-20 17:59:49 -0300

    Solve School Floor in dart

---
+++ solutions/beecrowd/2786/2786.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+void main() {
+  int l, c;
+  var l_str, c_str;
+
+  while (
+    (l_str = stdin.readLineSync()) != null &&
+    (c_str = stdin.readLineSync()) != null
+  ) {
+    l = int.parse(l_str);
+    c = int.parse(c_str);
+
+    print('${(l * c) + ((l - 1) * (c - 1))}\n${((l - 1) * 2) + ((c - 1) * 2)}');
+  }
+}
