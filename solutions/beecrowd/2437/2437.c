11f2870d1be691153d8791f70e34978b61e3d598
Denis Costa <deniscostadsc@gmail.com>
2025-09-26 14:26:54 -0300

    Solve Distância de Manhattan in c

---
+++ solutions/beecrowd/2437/2437.c
@@ -0,0 +1,13 @@
+#include <stdint.h>
+#include <stdio.h>
+#include<stdlib.h>
+
+int main() {
+    int16_t x_m, y_m, x_r, y_r;
+
+    while (scanf("%d %d %d %d", &x_m, &y_m, &x_r, &y_r) != EOF) {
+        printf("%d\n", abs(x_r - x_m) + abs(y_r - y_m));
+    }
+
+    return 0;
+}
