d7cbd462879bf898c43b1e981f72df725545e94b
Denis Costa <deniscostadsc@gmail.com>
2025-10-04 17:00:28 -0300

    Rename variable

--- solutions/beecrowd/1005/1005.exs
+++ solutions/beecrowd/1005/1005.exs
@@ -6,5 +6,5 @@ b = IO.gets("")
 |> String.trim()
 |> String.to_float()
 
-media = (a * 3.5 + b * 7.5) / 11.0
-:io.format("MEDIA = ~.5f~n", [media])
+average = (a * 3.5 + b * 7.5) / 11.0
+:io.format("MEDIA = ~.5f~n", [average])
