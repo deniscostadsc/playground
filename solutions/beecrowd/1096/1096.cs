484d38a37edb06dc10d6962085e9cde760d01b40
Denis Costa <deniscostadsc@gmail.com>
2025-10-05 10:20:42 -0300

    Solve Sequence IJ 2 in c#

---
+++ solutions/beecrowd/1096/1096.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Solution {
+    static void Main() {
+        for (int i = 1; i <= 9; i += 2) {
+            Console.WriteLine("I={0} J=7", i);
+            Console.WriteLine("I={0} J=6", i);
+            Console.WriteLine("I={0} J=5", i);
+        }
+    }
+}
