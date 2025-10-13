ea9ee1e8aaeec4a1826a5056b32be5ed79fed608
Denis Costa <deniscostadsc@gmail.com>
2025-10-13 09:24:58 -0300

    Solve Sequence IJ 2 in java

---
+++ solutions/beecrowd/1096/1096.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int number = 1; number <= 9; number += 2) {
+            System.out.printf("I=%d J=7\n", number);
+            System.out.printf("I=%d J=6\n", number);
+            System.out.printf("I=%d J=5\n", number);
+        }
+    }
+}
