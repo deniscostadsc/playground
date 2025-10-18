74cc0195ffed985ef63c6d717e04f746150881a0
Denis Costa <deniscostadsc@gmail.com>
2025-10-18 20:39:06 -0300

    Add typescript template

---
+++ templates/template.ts
@@ -0,0 +1,12 @@
+process.stdin.resume()
+process.stdin.setEncoding('utf8')
+
+let input = ''
+
+process.stdin.on('data', (chunk: string) => {
+  input += chunk
+})
+
+process.stdin.on('end', () => {
+  const lines = input.trim().split('\n')
+})
