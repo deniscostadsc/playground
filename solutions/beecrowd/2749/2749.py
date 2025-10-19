0637311fd8d827e802034a741db6306ca950af43
Denis Costa <deniscostadsc@gmail.com>
2025-10-19 13:18:04 -0300

    Fix lint

--- solutions/beecrowd/2749/2749.py
+++ solutions/beecrowd/2749/2749.py
@@ -2,10 +2,10 @@ import sys
 
 output = sys.stdout.write
 
-output("---------------------------------------\n")
-output("|x = 35                               |\n")
-output("|                                     |\n")
-output("|               x = 35                |\n")
-output("|                                     |\n")
-output("|                               x = 35|\n")
-output("---------------------------------------\n")
+output('---------------------------------------\n')
+output('|x = 35                               |\n')
+output('|                                     |\n')
+output('|               x = 35                |\n')
+output('|                                     |\n')
+output('|                               x = 35|\n')
+output('---------------------------------------\n')
