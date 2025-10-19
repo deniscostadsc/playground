b5f80b20af4054930c83cfeb133ae0f0002ceb0f
Denis Costa <deniscostadsc@gmail.com>
2025-10-19 12:24:52 -0300

    Solve Output 3 in python

---
+++ solutions/beecrowd/2749/2749.py
@@ -0,0 +1,11 @@
+import sys
+
+output = sys.stdout.write
+
+output("---------------------------------------\n")
+output("|x = 35                               |\n")
+output("|                                     |\n")
+output("|               x = 35                |\n")
+output("|                                     |\n")
+output("|                               x = 35|\n")
+output("---------------------------------------\n")
