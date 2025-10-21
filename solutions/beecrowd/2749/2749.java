bbd864634adcc9167203c403c6bd43f537f85068
Denis Costa <deniscostadsc@gmail.com>
2025-10-20 21:32:11 -0300

    Solve Output 3 in java

---
+++ solutions/beecrowd/2749/2749.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------");
+        System.out.println("|x = 35                               |");
+        System.out.println("|                                     |");
+        System.out.println("|               x = 35                |");
+        System.out.println("|                                     |");
+        System.out.println("|                               x = 35|");
+        System.out.println("---------------------------------------");
+    }
+}
