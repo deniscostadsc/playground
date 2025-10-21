a17bff77bb54163e653e4acc1fc74cd1ca3f4c23
Denis Costa <deniscostadsc@gmail.com>
2025-10-21 15:55:41 -0300

    Solve Output 3 in c#

---
+++ solutions/beecrowd/2749/2749.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Solution {
+    static void Main() {
+        Console.WriteLine("---------------------------------------");
+        Console.WriteLine("|x = 35                               |");
+        Console.WriteLine("|                                     |");
+        Console.WriteLine("|               x = 35                |");
+        Console.WriteLine("|                                     |");
+        Console.WriteLine("|                               x = 35|");
+        Console.WriteLine("---------------------------------------");
+    }
+}
