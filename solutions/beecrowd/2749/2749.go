644ee34660f94a83cec13302483b9e0a6a05de42
Denis Costa <deniscostadsc@gmail.com>
2025-10-25 16:15:04 -0300

    Solve Output 3 in go

---
+++ solutions/beecrowd/2749/2749.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("---------------------------------------")
+    fmt.Println("|x = 35                               |")
+    fmt.Println("|                                     |")
+    fmt.Println("|               x = 35                |")
+    fmt.Println("|                                     |")
+    fmt.Println("|                               x = 35|")
+    fmt.Println("---------------------------------------")
+}
