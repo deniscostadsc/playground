f73aca533aa4a846df4b647058ea8d347b641398
Denis Costa <deniscostadsc@gmail.com>
2025-10-26 10:38:58 -0300

    Fix lint

--- solutions/beecrowd/2749/2749.go
+++ solutions/beecrowd/2749/2749.go
@@ -4,10 +4,10 @@ import "fmt"
 
 func main() {
 	fmt.Println("---------------------------------------")
-    fmt.Println("|x = 35                               |")
-    fmt.Println("|                                     |")
-    fmt.Println("|               x = 35                |")
-    fmt.Println("|                                     |")
-    fmt.Println("|                               x = 35|")
-    fmt.Println("---------------------------------------")
+	fmt.Println("|x = 35                               |")
+	fmt.Println("|                                     |")
+	fmt.Println("|               x = 35                |")
+	fmt.Println("|                                     |")
+	fmt.Println("|                               x = 35|")
+	fmt.Println("---------------------------------------")
 }
