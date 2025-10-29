54e46be0c12c12d45575aa0b375d4848c15faace
Denis Costa <deniscostadsc@gmail.com>
2025-10-29 17:31:34 -0300

    Solve Digits in c++

---
+++ solutions/beecrowd/2867/2867.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <cstdint>
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0);
+
+    int16_t c, n, m;
+
+    std::cin >> c;
+    for (int64_t i = 0; i < c; i++) {
+        std::cin >> n >> m;
+        std::cout << static_cast<int>(std::floor(m * std::log10(n))) + 1 << '\n';
+    }
+
+    return 0;
+}
