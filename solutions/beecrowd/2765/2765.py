2d9495065437fc1144f52ba4e18728552bfe4a3a
Denis Costa <deniscostadsc@gmail.com>
2025-10-30 08:10:01 -0300

    Fix typo

--- solutions/beecrowd/2765/2765.py
+++ solutions/beecrowd/2765/2765.py
@@ -1,7 +1,7 @@
 import sys
 
 for sentence in sys.stdin:
-    # we can't use strip because we ned to keep trailing/leading space
+    # we can't use strip because we need to keep trailing/leading space
     splited_sentence = sentence.replace('\n', '').split(',')
     print(splited_sentence[0])
     print(','.join(splited_sentence[1:]))
