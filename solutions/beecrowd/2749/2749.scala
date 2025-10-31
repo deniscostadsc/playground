68f64f1f5fe4587a65b4d40cf661e1120a6031d3
Denis Costa <deniscostadsc@gmail.com>
2025-10-31 07:21:38 -0300

    Solve Output 3 in scala

---
+++ solutions/beecrowd/2749/2749.scala
@@ -0,0 +1,11 @@
+object Main {
+    def main(args: Array[String]) {
+        println("---------------------------------------")
+        println("|x = 35                               |")
+        println("|                                     |")
+        println("|               x = 35                |")
+        println("|                                     |")
+        println("|                               x = 35|")
+        println("---------------------------------------")
+    }
+}
