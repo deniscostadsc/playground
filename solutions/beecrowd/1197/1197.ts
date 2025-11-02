4f5a607a2bf086f6ffb1ef1d684892fbd8a8b6a2
Denis Costa <deniscostadsc@gmail.com>
2025-11-02 07:55:17 -0300

    Solve Back to High School Physics in typescript

---
+++ solutions/beecrowd/1197/1197.ts
@@ -0,0 +1,17 @@
+process.stdin.resume()
+process.stdin.setEncoding('utf8')
+
+let input = ''
+
+process.stdin.on('data', (chunk: string) => {
+  input += chunk
+})
+
+process.stdin.on('end', () => {
+  const lines = input.trim().split('\n')
+
+  lines.forEach(line => {
+    const [v, t] = line.trim().split(' ').map(Number);
+    console.log(v * t * 2 + 0); // + 0 to avoid -0 weird behavior
+  })
+})
