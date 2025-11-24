83d4bd89343d5e55985f1c233a63318da629ceb9
Denis Costa <deniscostadsc@gmail.com>
2025-11-24 19:49:52 -0300

    Fix lint

--- utils/benchmarks/py/run.sh
+++ utils/benchmarks/py/run.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 
 pushd "$(dirname "${0}")" >/dev/null
 
-time python test_print_input.py < ../in.txt > test_print_input.txt
-time python test_sys.py < ../in.txt > test_sys.txt
+time python test_print_input.py <../in.txt >test_print_input.txt
+time python test_sys.py <../in.txt >test_sys.txt
 
 rm -rf test_print_input.txt test_sys.txt
 
