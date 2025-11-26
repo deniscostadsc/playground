980e8b49803fed0a5a0b3495970f8336344c30ab
Denis Costa <deniscostadsc@gmail.com>
2025-11-26 18:49:04 -0300

    Add kotlin template

---
+++ templates/template.kt
@@ -0,0 +1,10 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    val sc = Scanner(System.`in`);
+
+    while (sc.hasNextInt()) {
+        val n = sc.nextInt()
+        println(n)
+    }
+}
