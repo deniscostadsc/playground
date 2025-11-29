6b65e8334501f426b5063f31a9c87affee7bc1f8
Denis Costa <deniscostadsc@gmail.com>
2025-11-29 08:30:11 -0300

    Solve 2581 - I am Toorg! in c

---
+++ solutions/beecrowd/2581/2581.c
@@ -0,0 +1,14 @@
+#include <stdint.h>
+#include <stdio.h>
+
+int main() {
+    int16_t n;
+
+    scanf("%d", &n);
+
+    for (int16_t i = 0; i < n; i++) {
+        puts("I am Toorg!");
+    }
+
+    return 0;
+}
