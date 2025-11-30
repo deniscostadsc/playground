403cbfebe72c7f580b9a943775076080a14f84aa
Denis Costa <deniscostadsc@gmail.com>
2025-11-30 09:49:18 -0300

    Improve add-missing task to add missing solution in more than one folder

--- scripts/makefile/add-missing.sh
+++ scripts/makefile/add-missing.sh
@@ -5,33 +5,35 @@ set -euo pipefail
 # shellcheck disable=SC1091
 source "$(dirname "$0")/../utils/environments.sh"
 
-FOLDER=${1}
+FOLDERS=${1}
 ENVIRONMENTS=${2}
 
-# make sure we have one, and only one, trailing slash
-FOLDER="$(sed 's/\/*$//g' <<<"${FOLDER}")"
-FOLDER="${FOLDER}/"
+for folder in ${FOLDERS}; do
+    # make sure we have one, and only one, trailing slash
+    folder="$(sed 's/\/*$//g' <<<"${folder}")"
+    folder="${folder}/"
 
-if [[ ! -f "${FOLDER}/problem.md" ]]; then
-    echo "This folder is not a solution folder"
-    exit 1
-fi
+    if [[ ! -f "${folder}/problem.md" ]]; then
+        echo "This folder is not a solution folder"
+        exit 1
+    fi
 
-if [[ -f "${FOLDER}/schema.sql" ]]; then
-    echo "This folder has a sql problem"
-    exit 1
-fi
-
-missing_solutions_languages=$(get_missing_solutions_languages "${FOLDER}")
-
-for extension in ${missing_solutions_languages}; do
-    if [[ ! " ${ENVIRONMENTS} " == *" ${extension} "* ]]; then
+    if [[ -f "${folder}/schema.sql" ]]; then
+        echo "This folder has a sql problem"
         continue
     fi
 
-    if [[ -f templates/template.${extension} ]] && [[ ! -f "${FOLDER}$(basename "${FOLDER}").${extension}" ]]; then
-        cp "templates/template.${extension}" "${FOLDER}$(basename "${FOLDER}").${extension}"
-    else
-        touch "${FOLDER}$(basename "${FOLDER}").${extension}"
-    fi
+    missing_solutions_languages=$(get_missing_solutions_languages "${folder}")
+
+    for extension in ${missing_solutions_languages}; do
+        if [[ ! " ${ENVIRONMENTS} " == *" ${extension} "* ]]; then
+            continue
+        fi
+
+        if [[ -f templates/template.${extension} ]] && [[ ! -f "${folder}$(basename "${folder}").${extension}" ]]; then
+            cp "templates/template.${extension}" "${folder}$(basename "${folder}").${extension}"
+        else
+            touch "${folder}$(basename "${folder}").${extension}"
+        fi
+    done
 done
