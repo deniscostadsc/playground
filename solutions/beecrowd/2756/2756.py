2b60ccc5d0cfda62675ae7ade2cdea72ae700160
Denis Costa <deniscostadsc@gmail.com>
2025-12-01 20:30:26 -0300

    Fix lint

--- solutions/beecrowd/2756/2756.py
+++ solutions/beecrowd/2756/2756.py
@@ -2,12 +2,12 @@ import sys
 
 output = sys.stdout.write
 
-output("       A\n")
-output("      B B\n")
-output("     C   C\n")
-output("    D     D\n")
-output("   E       E\n")
-output("    D     D\n")
-output("     C   C\n")
-output("      B B\n")
-output("       A\n")
+output('       A\n')
+output('      B B\n')
+output('     C   C\n')
+output('    D     D\n')
+output('   E       E\n')
+output('    D     D\n')
+output('     C   C\n')
+output('      B B\n')
+output('       A\n')
