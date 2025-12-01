2b71744010c64f74464aee9a3ed0db0e3f23d7fd
Denis Costa <deniscostadsc@gmail.com>
2025-12-01 20:27:14 -0300

    Solve 2756 - Output 10 in python

---
+++ solutions/beecrowd/2756/2756.py
@@ -0,0 +1,13 @@
+import sys
+
+output = sys.stdout.write
+
+output("       A\n")
+output("      B B\n")
+output("     C   C\n")
+output("    D     D\n")
+output("   E       E\n")
+output("    D     D\n")
+output("     C   C\n")
+output("      B B\n")
+output("       A\n")
