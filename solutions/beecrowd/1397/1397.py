3ef4d0c792beeb78174b21703095a46524fb7d64
Denis Costa <deniscostadsc@gmail.com>
2025-12-25 11:06:25 -0300

    Solve 1397 - Game of The Greatest in python

---
+++ solutions/beecrowd/1397/1397.py
@@ -0,0 +1,23 @@
+import sys
+
+input = sys.stdin.readline
+output = sys.stdout.write
+
+while line := input().strip():
+    n = int(line)
+
+    if n == 0:
+        break
+
+    a_count, b_count = 0, 0
+
+    for _ in range(n):
+        a, b = map(int, input().split())
+
+        if a > b:
+            a_count += 1
+
+        if b > a:
+            b_count += 1
+
+    output(f'{a_count} {b_count}\n')
