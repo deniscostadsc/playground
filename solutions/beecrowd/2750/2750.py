463843ead0898abe29b303fa50a5dc2481338495
Denis Costa <deniscostadsc@gmail.com>
2025-12-27 09:36:11 -0300

    Solve 2750 - Output 4 in python

---
+++ solutions/beecrowd/2750/2750.py
@@ -0,0 +1,24 @@
+import sys
+
+output = sys.stdout.write
+
+output('---------------------------------------\n')
+output('|  decimal  |  octal  |  Hexadecimal  |\n')
+output('---------------------------------------\n')
+output('|      0    |    0    |       0       |\n')
+output('|      1    |    1    |       1       |\n')
+output('|      2    |    2    |       2       |\n')
+output('|      3    |    3    |       3       |\n')
+output('|      4    |    4    |       4       |\n')
+output('|      5    |    5    |       5       |\n')
+output('|      6    |    6    |       6       |\n')
+output('|      7    |    7    |       7       |\n')
+output('|      8    |   10    |       8       |\n')
+output('|      9    |   11    |       9       |\n')
+output('|     10    |   12    |       A       |\n')
+output('|     11    |   13    |       B       |\n')
+output('|     12    |   14    |       C       |\n')
+output('|     13    |   15    |       D       |\n')
+output('|     14    |   16    |       E       |\n')
+output('|     15    |   17    |       F       |\n')
+output('---------------------------------------\n')
