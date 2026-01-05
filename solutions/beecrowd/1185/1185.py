cf8a0e2a072298c0f20f383d77485f4a2ec91d9f
Denis Costa <deniscostadsc@gmail.com>
2026-01-05 07:54:09 -0300

    Solve 1185 - Above the Secundary Diagonal in python

---
+++ solutions/beecrowd/1185/1185.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+output = sys.stdout.write
+
+o = input().strip()
+s = 0.0
+x = 12
+
+for _ in range(12):
+    x -= 1
+    for j in range(12):
+        n = float(input())
+        if j < x:
+            s += n
+
+if o == 'S':
+    output(f'{s:.1f}\n')
+else:
+    output(f'{s / 66:.1f}\n')
