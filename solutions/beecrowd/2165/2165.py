25b96eb1e064f7571ba6eb56b0d823e6be98cddb
Denis Costa <deniscostadsc@gmail.com>
2026-01-06 07:57:14 -0300

    Allow program to read more than 1 lines

--- solutions/beecrowd/2165/2165.py
+++ solutions/beecrowd/2165/2165.py
@@ -3,9 +3,8 @@ import sys
 input = sys.stdin.readline
 output = sys.stdout.write
 
-sentence = input()
-
-if len(sentence) <= 141:
-    output('TWEET\n')
-else:
-    output('MUTE\n')
+while sentence := input():
+    if len(sentence) <= 141:
+        output('TWEET\n')
+    else:
+        output('MUTE\n')
