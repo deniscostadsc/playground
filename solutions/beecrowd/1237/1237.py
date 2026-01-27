84f91fe81a8a67495e97c56f35ccd509c8b1d83f
Denis Costa <deniscostadsc@gmail.com>
2026-01-27 12:42:20 -0300

    Solve 1237 - Compare Substring in python

---
+++ solutions/beecrowd/1237/1237.py
@@ -0,0 +1,29 @@
+import sys
+
+input = sys.stdin.readline
+output = sys.stdout.write
+
+
+def print_table(table):
+    for row in table:
+        for item in row:
+            output(f'{item} ')
+        output('\n')
+
+
+while first_line := input().strip():
+    second_line = input().strip()
+    lcs = 0
+    lcs_table = [
+        [0] * (len(second_line) + 1) for _ in range(len(first_line) + 1)
+    ]
+
+    for first_line_index, first_line_letter in enumerate(first_line, 1):
+        for second_line_index, second_line_letter in enumerate(second_line, 1):
+            if first_line_letter == second_line_letter:
+                lcs_table[first_line_index][second_line_index] = (
+                    1 + lcs_table[first_line_index - 1][second_line_index - 1]
+                )
+                lcs = max(lcs, lcs_table[first_line_index][second_line_index])
+
+    output(f'{lcs}\n')
