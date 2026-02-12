1718c98527f1ce7ce4ffc788e55fc179dcf422d8
Denis Costa <deniscostadsc@gmail.com>
2026-02-12 11:20:17 -0300

    Improve input/output files

--- solutions/beecrowd/2060/2060.cpp
+++ solutions/beecrowd/2060/2060.cpp
@@ -5,31 +5,31 @@ int main() {
     std::int16_t n, l, multiple_of_2, multiple_of_3, multiple_of_4,
         multiple_of_5;
 
-    std::cin >> n;
+    while (std::cin >> n) {
+        multiple_of_2 = 0, multiple_of_3 = 0, multiple_of_4 = 0, multiple_of_5 = 0;
 
-    multiple_of_2 = 0, multiple_of_3 = 0, multiple_of_4 = 0, multiple_of_5 = 0;
+        while (n--) {
+            std::cin >> l;
 
-    while (n--) {
-        std::cin >> l;
+            if (l % 2 == 0) {
+                multiple_of_2++;
+            }
+            if (l % 3 == 0) {
+                multiple_of_3++;
+            }
+            if (l % 4 == 0) {
+                multiple_of_4++;
+            }
+            if (l % 5 == 0) {
+                multiple_of_5++;
+            }
+        }
 
-        if (l % 2 == 0) {
-            multiple_of_2++;
-        }
-        if (l % 3 == 0) {
-            multiple_of_3++;
-        }
-        if (l % 4 == 0) {
-            multiple_of_4++;
-        }
-        if (l % 5 == 0) {
-            multiple_of_5++;
-        }
+        std::cout << multiple_of_2 << " Multiplo(s) de 2" << std::endl;
+        std::cout << multiple_of_3 << " Multiplo(s) de 3" << std::endl;
+        std::cout << multiple_of_4 << " Multiplo(s) de 4" << std::endl;
+        std::cout << multiple_of_5 << " Multiplo(s) de 5" << std::endl;
     }
-
-    std::cout << multiple_of_2 << " Multiplo(s) de 2" << std::endl;
-    std::cout << multiple_of_3 << " Multiplo(s) de 3" << std::endl;
-    std::cout << multiple_of_4 << " Multiplo(s) de 4" << std::endl;
-    std::cout << multiple_of_5 << " Multiplo(s) de 5" << std::endl;
 
     return 0;
 }
