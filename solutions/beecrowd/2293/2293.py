296c30f56540779492ec1d5edee42a6854a8bc97
Denis Costa <deniscostadsc@gmail.com>
2026-04-02 07:54:12 -0300

    Improve input/output files

--- solutions/beecrowd/2293/2293.py
+++ solutions/beecrowd/2293/2293.py
@@ -1,13 +1,19 @@
-n, m = map(int, input().split())
-cells = []
-max_worms = 0
+import sys
 
-for _ in range(n):
-    cells_line = list(map(int, input().split()))
-    max_worms = max(max_worms, sum(cells_line))
-    cells.append(cells_line)
+input = sys.stdin.readline
+output = sys.stdout.write
 
-for cells_column in zip(*cells):
-    max_worms = max(max_worms, sum(cells_column))
+while line := input().strip():
+    n, m = map(int, line.split())
+    cells = []
+    max_worms = 0
 
-print(max_worms)
+    for _ in range(n):
+        cells_line = list(map(int, input().split()))
+        max_worms = max(max_worms, sum(cells_line))
+        cells.append(cells_line)
+
+    for cells_column in zip(*cells):
+        max_worms = max(max_worms, sum(cells_column))
+
+    print(max_worms)
