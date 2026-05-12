0281b59f3b899da68104fe8b22881a070a4271c5
Denis Costa <deniscostadsc@gmail.com>
2026-05-11 21:25:21 -0300

    Fix Ocaml build

--- .docker/ml.Dockerfile
+++ .docker/ml.Dockerfile
@@ -1,16 +1,7 @@
-FROM ubuntu:24.04
+FROM --platform=linux/amd64 ocaml/opam:ubuntu-16.04_ocaml-4.01.0
 
-ENV OCAML_VERSION 4.05.0
-
-RUN apt update && apt upgrade -y && apt install -y --force-yes opam
-
-RUN opam init --disable-sandboxing
-RUN opam update
-
-RUN opam install -y ocaml=${OCAML_VERSION}
-RUN opam switch create ${OCAML_VERSION}
-
-RUN mkdir code
+RUN sudo mkdir -p /code /home/opam/.opam/log/
+RUN chmod 777 -R /home/opam/
 WORKDIR /code
 
 CMD /code/scripts/docker/run.sh
--- solutions/beecrowd/1805/1805.ml
+++ solutions/beecrowd/1805/1805.ml
@@ -1,8 +1,17 @@
+let split_on_char sep s =
+  let rec loop i =
+    try
+      let j = String.index_from s i sep in
+      String.sub s i (j - i) :: loop (j + 1)
+    with Not_found -> [String.sub s i (String.length s - i)]
+  in
+  loop 0
+
 let () =
   try
     while true do
       let line = input_line stdin in
-      match String.split_on_char ' ' line with
+      match split_on_char ' ' line with
       | [f; l] ->
           let first = int_of_string f in
           let last = int_of_string l in
